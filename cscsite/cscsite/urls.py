f324105b121f7ad4f7dd70d65dfd8f8075ba166e
Dmitry Groshev <lambdadmitry@gmail.com>
2014-01-12 20:09:51 +0400

    simple static page using CBV

--- cscsite/cscsite/urls.py
+++ cscsite/cscsite/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'cscsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^events/', include('events.urls')),
+    url(r'^contacts/', TemplateView.as_view(template_name="contacts.html")),
     url(r'^admin/', include(admin.site.urls)),
 )
