174024a84a879fe701ab87feda547de2390dff35
Dmitry Groshev <lambdadmitry@gmail.com>
2014-01-18 22:53:56 +0400

    fix serving media in dev mode

--- cscsite/cscsite/urls.py
+++ cscsite/cscsite/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 from django.contrib import admin
@@ -11,4 +13,4 @@ urlpatterns = patterns('',
     url(r'^news/', include('news.urls')),
     url(r'^contacts/', TemplateView.as_view(template_name="contacts.html")),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
