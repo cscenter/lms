ce091c6e6138827472b78e429ca8e464a215c192
Dmitry Groshev <lambdadmitry@gmail.com>
2014-01-19 18:31:05 +0400

    new index page

--- cscsite/cscsite/urls.py
+++ cscsite/cscsite/urls.py
@@ -4,12 +4,16 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 from django.contrib import admin
+
+from index.views import IndexView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cscsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(regex=r'^$', view=IndexView.as_view(), name='index'),
     url(r'^news/', include('news.urls')),
     url(r'^contacts/', TemplateView.as_view(template_name="contacts.html")),
     url(r'^admin/', include(admin.site.urls)),
