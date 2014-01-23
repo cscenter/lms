afa9c870d2e1ac51dc979ca20c4efa186f079d22
Dmitry Groshev <lambdadmitry@gmail.com>
2014-01-23 23:39:24 +0400

    rework navigation

--- cscsite/core/templatetags/navigation.py
+++ cscsite/core/templatetags/navigation.py
@@ -1,27 +1,39 @@
 # Taken from blog.scur.pl/2012/09/highlighting-current-active-page-django/
 # It's a shame this didn't make it to a library yet.
 
+import logging
 from django import template
 from django.core import urlresolvers
 
 register = template.Library()
+logger = logging.getLogger(__name__)
+
+URL_NAMES = {
+    'index': {},
+
+    'syllabus': {'parent': 'about'},
+    'orgs': {'parent': 'about'},
+    'profs': {'parent': 'about'},
+    'alumni': {'parent': 'about'},
+
+    'news_list': {},
+    'news_detail': {'alias': 'news_list'},
+
+    'contacts': {}
+}
 
 @register.simple_tag(takes_context=True)
 def current(context, url_name, return_value='current', **kwargs):
-    matches = current_url_equals(context, url_name, **kwargs)
-    return return_value if matches else ''
-
-def current_url_equals(context, url_name, **kwargs):
-    resolved = False
-    try:
-        resolved = urlresolvers.resolve(context.get('request').path)
-    except:
-        pass
-    matches = resolved and resolved.url_name == url_name
-    if matches and kwargs:
-        for key in kwargs:
-            kwarg = kwargs.get(key)
-            resolved_kwarg = resolved.kwargs.get(key)
-            if not resolved_kwarg or kwarg != resolved_kwarg:
-                return False
-    return matches
+    current_url_name = context['request'].resolver_match.url_name
+    matched_simple = url_name == current_url_name
+    if not current_url_name in URL_NAMES:
+        logger.warning("can't find url {0} in URL_NAMES".format(url_name))
+        return return_value if matched_simple else ''
+    else:
+        url_info = URL_NAMES[current_url_name]
+        matched_alias = ('alias' in url_info and
+                         url_info['alias'] == url_name)
+        matched_parent = ('parent' in url_info and
+                          url_info['parent'] == url_name)
+        matched = matched_simple or matched_alias or matched_parent
+        return return_value if matched else ''
