a8d5b6d89b19688bafff1651094b8bb032086a08
Dmitry Groshev <lambdadmitry@gmail.com>
2014-02-03 18:02:31 +0400

    move url info to config

--- cscsite/core/templatetags/navigation.py
+++ cscsite/core/templatetags/navigation.py
@@ -3,31 +3,18 @@
 
 import logging
 from django import template
+from django.conf import settings
 from django.core import urlresolvers
 
 register = template.Library()
 logger = logging.getLogger(__name__)
 
-URL_NAMES = {
-    'index': {},
-
-    'syllabus': {'parent': 'about'},
-    'orgs': {'parent': 'about'},
-    'profs': {'parent': 'about'},
-    'alumni': {'parent': 'about'},
-
-    'news_list': {},
-    'news_detail': {'alias': 'news_list'},
-
-    'contacts': {}
-}
-
 @register.simple_tag(takes_context=True)
 def current(context, url_name, return_value='current', **kwargs):
     current_url_name = context['request'].resolver_match.url_name
     matched_simple = url_name == current_url_name
-    if not current_url_name in URL_NAMES:
-        logger.warning("can't find url {0} in URL_NAMES".format(url_name))
+    if not current_url_name in settings.MENU_URL_NAMES:
+        logger.warning("can't find url {0} in MENU_URL_NAMES".format(url_name))
         return return_value if matched_simple else ''
     else:
         url_info = URL_NAMES[current_url_name]
