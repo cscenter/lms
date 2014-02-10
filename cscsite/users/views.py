d758a9d4eb3b70941f86622e531769dee0e217be
Dmitry Groshev <lambdadmitry@gmail.com>
2014-02-10 20:09:28 +0400

    Better login form

--- cscsite/users/views.py
+++ cscsite/users/views.py
@@ -5,19 +5,31 @@ from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.generic import FormView, View
+from django.utils.translation import ugettext_lazy as _
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Field, Layout, Submit
+from crispy_forms.layout import Field, Layout, Submit, Hidden
+from crispy_forms.bootstrap import FormActions
+import floppyforms as forms
 
 class LoginForm(AuthenticationForm):
+    username = forms.CharField(required=True,
+                               label=_("Username"),
+                               widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
+    password = forms.CharField(widget=forms.PasswordInput,
+                               label=_("Password"))
+
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_tag = False
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-xs-2'
+        self.helper.field_class = 'col-xs-7'
         self.helper.layout = Layout(
-            Field('username', placeholder="username"),
-            Field('password', placeholder="password"),
-            Submit('submit', 'Submit'))
+            'username',
+            'password',
+            FormActions(Submit('submit', _("Submit"))),
+            Hidden('next', 'next'))
 
 # TODO: rename all XXXView to just XXX?
 class LoginView(FormView):
