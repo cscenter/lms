f51f1b91b325d6f038b07780157e24e7548ece5e
Sergei Lebedev <superbobry@gmail.com>
2014-04-20 01:45:49 +0400

    Moved 'Ubereditor*' stuff to 'core' app
      and fixed 'ImportError' on Travis

--- cscsite/library/admin.py
+++ cscsite/library/admin.py
@@ -1,11 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.db import models
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-from pagedown.widgets import AdminPagedownWidget
 
-from core.admin import meta, urlize
+from core.admin import UbereditorMixin, meta, urlize
 from .models import Book, Borrow
 
 
@@ -14,16 +12,13 @@ class BorrowInline(admin.TabularInline):
     extra = 1
 
 
-class BookAdmin(admin.ModelAdmin):
+class BookAdmin(UbereditorMixin, admin.ModelAdmin):
     list_display = ["author", "title", "read_by_with_links"]
     list_display_links = ["author", "title"]
     list_filter = ["tags"]
     list_select_related = True
 
     inlines = [BorrowInline]
-    formfield_overrides = {
-        models.TextField: {'widget': AdminPagedownWidget}
-    }
 
     @meta(_("read by"), allow_tags=True)
     def read_by_with_links(self, instance):
