486730f9cebd0e000ddf494bfe451d477b91bf48
Dmitry Groshev <lambdadmitry@gmail.com>
2014-04-23 18:58:53 +0400

    display deadlines in different colors according to the time left

--- cscsite/core/templatetags/core_tags.py
+++ cscsite/core/templatetags/core_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.timezone import now
 
 from core.admin import get_admin_url
 
@@ -16,3 +17,18 @@ def admin_url(instance_or_qs):
 @register.filter
 def to_css(s):
     return s.replace("_", "-")
+
+
+@register.filter
+def date_soon_css(d):
+    days_diff = (d.date() - now().date()).days
+    if days_diff < 0:
+        return "past"
+    elif days_diff == 0:
+        return "today"
+    elif days_diff == 1:
+        return "tomorrow"
+    elif days_diff == 2:
+        return "day-after-tomorrow"
+    else:
+        return "in-future"
