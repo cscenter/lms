8a81c616c8834b8688a26b06ecdd9653392ba6eb
Dmitry Groshev <lambdadmitry@gmail.com>
2014-06-12 02:13:34 +0400

    add "autocompletion" for ending time in class form

--- cscsite/assets/js/main.js
+++ cscsite/assets/js/main.js
@@ -1,3 +1,5 @@
+var ends_at_touched = false;
+
 $(document).ready(function () {
     hljs.configure({tabReplace: '    '});
 
@@ -52,5 +54,41 @@ $(document).ready(function () {
             editor.getElement('previewerIframe').contentWindow.eval(
                 'MathJax.Hub.Queue(["Typeset", MathJax.Hub]);');
         });
+    });
+
+    $("#id_ends_at").focus(function() {
+        ends_at_touched = true;
+    });
+
+    // this is fragile as hell, didn't find a suitable library
+    $("#id_starts_at").change(function() {
+        var DELTA_MINUTES = 80;
+
+        function pad(num, size) {
+            var s = num+"";
+            while (s.length < size) s = "0" + s;
+            return s;
+        }
+
+        //if ($("#id_ends_at").val().length === 0) {
+        if (!ends_at_touched) {
+            var string_time = $(this).val();
+            var matches = string_time.match("([0-9]{2})([:-])([0-9]{2})");
+            if (matches !== null) {
+                var hours = parseInt(matches[1]);
+                var separator = matches[2];
+                var minutes = parseInt(matches[3]);
+
+                var raw_new_minutes = minutes + DELTA_MINUTES;
+                var new_hours = (hours + Math.floor(raw_new_minutes / 60)) % 24;
+                var new_minutes = raw_new_minutes % 60;
+
+                $("#id_ends_at").val(pad(new_hours, 2)
+                                     + separator
+                                     + pad(new_minutes, 2));
+            } else {
+                console.log("Can't parse " + string_time);
+            }
+        }
     });
 });
