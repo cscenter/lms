73aac51dc74e3d25faca0a7ac224a22a15bf802c
Sergei Lebedev <superbobry@gmail.com>
2014-07-23 01:10:01 +0400

    Fixed more import quirks
      we now correctly handle admin@foo.com

--- cscsite/crutches/management/commands/import_beta_users.py
+++ cscsite/crutches/management/commands/import_beta_users.py
@@ -40,13 +40,21 @@ class Command(BaseCommand):
                 continue
 
             username, domain = row["email"].split("@")
-            user, _ = CSCUser.objects.get_or_create(username=username)
-            user.first_name = force_text(row["first_name"])
-            user.last_name = force_text(row["last_name"])
-            user.patronymic = force_text(row["middle_name"])
-            user.date_joined = parse_datetime(row["date_joined"])
-            user.email = row["email"]
-            user.password = row["password"]
+            if username == "admin":
+                # Don't import any extra admins by accident ;)
+                username = "{first_name}.{last_name}".format(**row).lower()
+
+            if CSCUser.objects.filter(username=username).exists():
+                # Interestingly, some people have registered twice.
+                continue
+
+            user = CSCUser.objects.create(
+                username=username, password=row["password"],
+                email=row["email"],
+                first_name=force_text(row["first_name"]),
+                last_name=force_text(row["last_name"]),
+                patronymic=force_text(row["middle_name"]),
+                date_joined=parse_datetime(row["date_joined"]))
 
             current_year = date.today().year
             if str(row["cs_center_year"]).isdigit():
@@ -57,7 +65,6 @@ class Command(BaseCommand):
                 user.graduation_year = current_year  # unsure about this.
 
             user.save()
-
             user.groups.add(CSCUser.IS_STUDENT_PK)
             if user.graduation_year is not None:
                 user.groups.add(CSCUser.IS_GRADUATE_PK)
