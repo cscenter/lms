b50fa78a6a326e44911997a019ab3d9bf0f89a24
Sergei Lebedev <superbobry@gmail.com>
2014-08-28 21:29:37 +0400

    Fixed 'cscsite.settings' auto-detection

--- cscsite/cscsite/settings/__init__.py
+++ cscsite/cscsite/settings/__init__.py
@@ -3,6 +3,6 @@ import socket
 
 # Note(lebedev): this is currently hardcoded for AWS server.
 if socket.gethostname() == "ip-10-82-139-53":
-    from . import production
+    from .production import *
 else:
-    from . import local
+    from .local import *
