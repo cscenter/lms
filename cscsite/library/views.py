6da2527a6a8d52b735ce9c99a7d3e02b02d7b70c
Sergei Lebedev <superbobry@gmail.com>
2014-09-15 18:58:08 +0400

    '/library' is now restricted to students-only

--- cscsite/library/views.py
+++ cscsite/library/views.py
@@ -1,17 +1,18 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.views.generic import ListView, DetailView
+from django.views import generic
 
+from core.views import StudentOnlyMixin
 from .models import Book
 
 
-class BookListView(ListView):
+class BookListView(StudentOnlyMixin, generic.ListView):
     context_object_name = "books"
     http_method_names = ["head", "get", "options"]
     queryset = Book.objects.select_related()
 
 
-class BookDetailView(DetailView):
+class BookDetailView(StudentOnlyMixin, generic.DetailView):
     context_object_name = "book"
     http_method_names = ["head", "get", "options"]
     model = Book
