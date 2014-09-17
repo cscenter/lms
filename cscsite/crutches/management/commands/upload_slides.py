9015eae29498dc769871a3460b8676db09be3bd1
Sergei Lebedev <superbobry@gmail.com>
2014-09-18 02:23:25 +0400

    Use "ru" locale in 'upload_slides'

--- cscsite/crutches/management/commands/upload_slides.py
+++ cscsite/crutches/management/commands/upload_slides.py
@@ -1,14 +1,18 @@
 from __future__ import print_function
 
 from django.core.management import BaseCommand
+from django.utils import translation
 
 from learning.models import Semester
 
 
 class Command(BaseCommand):
     help = "Uploads slides to SlideShare and Yandex.Disk"
+    can_import_settings = True
 
     def handle(self, *args, **options):
+        translation.activate("ru")
+
         current_semester = Semester.objects.first()
         for course_offering in current_semester.courseoffering_set.all():
             course_classes = course_offering.courseclass_set \
