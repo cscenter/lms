a5d3b7bf4ecd293cb1dab771d37bd5f9b9caaf0f
Sergei Lebedev <superbobry@gmail.com>
2014-09-18 00:22:50 +0400

    More crutches

--- cscsite/crutches/management/commands/export_students.py
+++ cscsite/crutches/management/commands/export_students.py
@@ -7,9 +7,18 @@ import sys
 
 from django.contrib.auth.models import Group
 from django.core.management import BaseCommand
+from django.utils.encoding import force_bytes
 
 from learning.models import Semester
 from users.models import CSCUser
+
+
+class UnicodeWriter(object):
+    def __init__(self, fd, **kwargs):
+        self._writer = csv.writer(fd, **kwargs)
+
+    def writerow(self, row):
+        self._writer.writerow(list(map(force_bytes, row)))
 
 
 class Command(BaseCommand):
@@ -17,7 +26,7 @@ class Command(BaseCommand):
             "they've enrolled on into a CSV file.")
 
     def handle(self, *args, **options):
-        w = csv.writer(sys.stdout)
+        w = UnicodeWriter(sys.stdout, dialect=csv.excel_tab)
 
         current_semester = Semester.objects.first()
         course_offerings = list(current_semester.courseoffering_set
@@ -28,6 +37,9 @@ class Command(BaseCommand):
 
         student_group = Group.objects.get(pk=CSCUser.IS_STUDENT_PK)
         for student in student_group.user_set.all():
+            if student.graduation_year or not student.enrollment_year:
+                continue
+
             enrolled_on = set(student.enrolled_on_set
                               .values_list("course__name", flat=True))
 
