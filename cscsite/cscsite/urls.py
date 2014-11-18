34ceabfc3727e822329947f89832308ca1ce5138
Dmitry Groshev <lambdadmitry@gmail.com>
2014-11-18 17:50:31 +0300

    add .ical view for users

--- cscsite/cscsite/urls.py
+++ cscsite/cscsite/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 
 from index.views import IndexView, AlumniView, TeachersView, RobotsView
 from users.views import LoginView, LogoutView, TeacherDetailView, \
-    UserDetailView, UserUpdateView
+    UserDetailView, UserUpdateView, ICalView
 from textpages.views import TextpageOpenView, TextpageStudentView, \
     CustomTextpageOpenView
 from learning.views import \
@@ -45,6 +45,8 @@ urlpatterns = patterns('',
         name='teacher_detail'),
     url(r'^users/(?P<pk>\d+)/$', UserDetailView.as_view(),
         name='user_detail'),
+    url(r'^users/(?P<pk>\d+)/csc.ics', ICalView.as_view(),
+        name='user_ical'),
     url(r'^users/(?P<pk>\d+)/edit$', UserUpdateView.as_view(),
         name='user_update'),
     url(r'^alumni/$', AlumniView.as_view(), name='alumni'),
