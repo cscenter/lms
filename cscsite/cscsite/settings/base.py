0c7676369bb4eb4869b57b0ee1daaac3a509513c
Dmitry Groshev <Dmitry.Groshev@jetbrains.com>
2014-12-11 16:34:24 +0300

    fixes for tests and logic around login redirect (#33)

--- cscsite/cscsite/settings/base.py
+++ cscsite/cscsite/settings/base.py
@@ -240,7 +240,7 @@ MENU_URL_NAMES = {
     'password_change': {}
     }
 
-LOGIN_URL= "/login"
+LOGIN_URL= "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 # this urls will be used to redirect from '/learning/' and '/teaching/'
