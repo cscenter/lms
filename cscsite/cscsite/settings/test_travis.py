7cb8681abe6f4f1a0ea911a083cd5e6a80d0691f
Dmitry Groshev <Dmitry.Groshev@jetbrains.com>
2015-02-18 21:50:00 +0300

    ensure that postgres is really used

--- cscsite/cscsite/settings/test_travis.py
+++ cscsite/cscsite/settings/test_travis.py
@@ -9,7 +9,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "travis_ci_test",
-        "USER": "postgres",
+        "USER": "postgrest",
         "PASSWORD": "",
         "HOST": "localhost",
         "PORT": ""
