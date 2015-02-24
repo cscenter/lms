1a95403c6f969e54d176af402467d087ff1ff29f
Dmitry Groshev <Dmitry.Groshev@jetbrains.com>
2015-02-24 20:14:35 +0300

    move to Postgres and add newrelic

--- cscsite/cscsite/wsgi.py
+++ cscsite/cscsite/wsgi.py
@@ -10,5 +10,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cscsite.settings")
 
+import newrelic.agent
+from django.conf import settings
+newrelic.agent.initialize(settings.NEWRELIC_CONF, settings.NEWRELIC_ENV)
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
