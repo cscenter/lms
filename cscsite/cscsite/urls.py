0300c88dd9ea01429d57ce7a3e33ea2785991d44
Dmitry Groshev <Dmitry.Groshev@jetbrains.com>
2015-03-01 22:03:57 +0300

    add unsubscription link

--- cscsite/cscsite/urls.py
+++ cscsite/cscsite/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 from django.contrib import admin
 
-from index.views import IndexView, AlumniView, TeachersView, RobotsView
+from index.views import IndexView, AlumniView, TeachersView, RobotsView, \
+    UnsubscribeYaProxyView
 from users.views import LoginView, LogoutView, TeacherDetailView, \
     UserDetailView, UserUpdateView, ICalClassesView, ICalAssignmentsView, \
     ICalEventsView, UserSearchJSONView, UserSearchView #, StudentInfoUpdateView
@@ -42,6 +43,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^robots\.txt$', RobotsView.as_view(), name='robotstxt'),
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^unsubscribe/(?P<hash>[a-f0-9]{32})/',
+        UnsubscribeYaProxyView.as_view(), name='unsubscribe_ya'),
     url(r'^syllabus/$', TextpageOpenView.as_view(), name='syllabus'),
     url(r'^orgs/$', TextpageOpenView.as_view(), name='orgs'),
     url(r'^teachers/$', TeachersView.as_view(), name='teachers'),
