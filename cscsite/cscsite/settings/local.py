4d765c002d5817e2276d8d6700f52f637b945e4d
Dmitry Groshev <Dmitry.Groshev@jetbrains.com>
2015-03-20 18:56:00 +0300

    add template profiling to django-debug-toolbar

--- cscsite/cscsite/settings/local.py
+++ cscsite/cscsite/settings/local.py
@@ -1,6 +1,25 @@
 from .base import *
 
-INSTALLED_APPS += ('fixture_media', 'debug_toolbar', 'rosetta')
+INSTALLED_APPS += ('fixture_media',
+                   'debug_toolbar',
+                   'template_timings_panel',
+                   'rosetta')
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'template_timings_panel.panels.TemplateTimings.TemplateTimings'
+]
 
 ROSETTA_MESSAGES_SOURCE_LANGUAGE_CODE = 'ru'
 ROSETTA_MESSAGES_SOURCE_LANGUAGE_NAME = 'Russian'
