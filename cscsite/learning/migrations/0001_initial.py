d3e521720c9e09b08dbd14f6f4711e744fc1279b
Dmitry Groshev <Dmitry.Groshev@jetbrains.com>
2015-04-03 18:41:02 +0300

    add venues to initial migration

--- cscsite/learning/migrations/0001_initial.py
+++ cscsite/learning/migrations/0001_initial.py
@@ -8,6 +8,18 @@ import django.utils.timezone
 import django.core.validators
 
 
+def forwards_func(apps, schema_editor):
+    Venue = apps.get_model('learning', 'Venue')
+    db_alias = schema_editor.connection.alias
+    initial_venues = [{"name": "\u041f\u041e\u041c\u0418 \u0420\u0410\u041d",
+                       "description": "\u041d\u0430\u0431\u0435\u0440\u0435\u0436\u043d\u0430\u044f \u0440\u0435\u043a\u0438 \u0424\u043e\u043d\u0442\u0430\u043d\u043a\u0438, 27, \u041c\u0440\u0430\u043c\u043e\u0440\u043d\u044b\u0439 \u0437\u0430\u043b (2 \u044d\u0442\u0430\u0436)\r\n\r\n<img src=\"//api-maps.yandex.ru/services/constructor/1.0/static/?sid=hlqT2p1Lqk9imQqe8w2gxQktaxSuP91c&width=600&height=450\" alt=\"\"/>"},
+                      {"name": "\u0424\u041c\u041b 239",
+                       "description": "\u0424\u0443\u0440\u0448\u0442\u0430\u0442\u0441\u043a\u0430\u044f \u0443\u043b\u0438\u0446\u0430, 9, \u0432\u0442\u043e\u0440\u043e\u0439 \u044d\u0442\u0430\u0436, \u0430\u043a\u0442\u043e\u0432\u044b\u0439 \u0437\u0430\u043b \u043b\u0438\u0431\u043e \u0430\u0443\u0434. 25 (\u0432 \u043a\u043e\u043d\u0446\u0435 \u043a\u043e\u0440\u0438\u0434\u043e\u0440\u0430)\r\n\r\n<img src=\"//api-maps.yandex.ru/services/constructor/1.0/static/?sid=95w30endKrJjcgE7ct5DTGxUgfGuvBgx&width=600&height=450\" alt=\"\"/>"}]
+    for page in initial_venues:
+        Venue(**page).save(force_insert=True, using=db_alias)
+
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -275,4 +287,5 @@ class Migration(migrations.Migration):
             },
             bases=(models.Model,),
         ),
+        migrations.RunPython(forwards_func),
     ]
