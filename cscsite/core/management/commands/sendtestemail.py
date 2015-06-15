826714b5fca10aa18f1e76caadddbdc1701ea827
pacahon <pacabest@gmail.com>
2015-06-15 13:16:00 +0300

    remove email_from field from testemail manage command

--- cscsite/core/management/commands/sendtestemail.py
+++ cscsite/core/management/commands/sendtestemail.py
@@ -2,6 +2,9 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import quopri
+
+from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.core.management.base import BaseCommand
 from django.utils.html import strip_tags, linebreaks
@@ -14,18 +17,14 @@ class Command(BaseCommand):
         parser.add_argument('email_to')
         parser.add_argument('subject')
         parser.add_argument('html_body', type=lambda s: unicode(s, 'utf8'))
-        parser.add_argument('--email_from',
-                            dest='email_from',
-                            default='test@compscicenter.ru',
-                            help='Provide email from')
 
     def handle(self, *args, **options):
-        html_content = linebreaks(options['html_body'])
+        html_content = options['html_body']
         text_content = strip_tags(html_content)
 
         msg = EmailMultiAlternatives(options['subject'],
                                      text_content,
-                                     options['email_from'],
+                                     settings.DEFAULT_FROM_EMAIL,
                                      [options['email_to']])
         msg.attach_alternative(html_content, "text/html")
         msg.send()
