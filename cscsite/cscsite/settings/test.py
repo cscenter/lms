3b82d9578fdf6b826da201b7875015e8ab918ba2
pacahon <pacabest@gmail.com>
2015-06-18 19:36:21 +0300

    remove unnecessery migrations due to fixing excerpt field problem #114; say hello to test in memories again

--- cscsite/cscsite/settings/test.py
+++ cscsite/cscsite/settings/test.py
@@ -51,7 +51,8 @@ class DisableMigrations(object):
 
 
 # disable migration in tests; it's a hack until Django 1.8 with --keepdb
-MIGRATION_MODULES = DisableMigrations()
+# MIGRATION_MODULES = DisableMigrations()
+MIGRATION_MODULES = {}
 
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
