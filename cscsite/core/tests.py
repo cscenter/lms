d065c8bb805d0a45e6598b6dd8d3a98673207db4
pacahon <pacabest@gmail.com>
2015-06-24 17:51:51 +0300

    forgot to remove navigation.pyc; Fixing tests

--- cscsite/core/tests.py
+++ cscsite/core/tests.py
@@ -16,7 +16,6 @@ from learning.models import AssignmentNotification, \
 from learning.tests.factories import AssignmentNotificationFactory, \
     CourseOfferingNewsNotificationFactory, AssignmentStudentFactory
 
-from .templatetags.navigation import current
 from .management.commands.notify import Command
 from .models import related_spec_to_list, apply_related_spec
 
@@ -32,104 +31,6 @@ class FakeObj(object):
             else:
                 attr = FakeObj(val) if isinstance(val, dict) else val
                 setattr(self, key, attr)
-
-
-class CurrentTagTest(TestCase):
-    """
-    Verify that "current" template tag works
-    """
-    def test_current(self):
-        test_settings = {'first_page': {},
-                         'second_page': {},
-                         'second_subpage': {'parent': 'second_page'},
-                         'second_page_alias': {'alias': 'second_page'},
-                         'second_subpage_alias': {'alias': 'second_subpage'},
-                         'second_subpage_another_alias':
-                         {'alias': 'second_subpage'},
-                         'second_subpage_alias_alias':
-                         {'alias': 'second_subpage_alias'},
-                         'dangling_subpage': {'parent': 'nonexistent_page'}}
-
-        test_ret = 'test_return'
-
-        def fake_context(url_name):
-            return {'request': FakeObj({'resolver_match':
-                                        {'url_name': url_name}})}
-
-        def call_current(current_url_name, tag_url_name):
-            ret = current(fake_context(current_url_name),
-                          tag_url_name, return_value=test_ret)
-            self.assertIn(ret, [test_ret, ''])
-            return ret == test_ret
-
-        with self.settings(MENU_URL_NAMES=test_settings):
-            # basic stuff
-            self.assertTrue(call_current('first_page', 'first_page'))
-            self.assertFalse(call_current('first_page', 'second_page'))
-            self.assertFalse(call_current('second_page', 'first_page'))
-
-            # logging and unexpected URL names
-            with LogCapture() as l:
-                self.assertTrue(call_current('some_page', 'some_page'))
-                self.assertFalse(call_current('some_other_page', 'some_page'))
-                self.assertFalse(call_current('some_page', 'some_other_page'))
-                l.check(('core.templatetags.navigation', 'WARNING',
-                         "can't find url some_page in MENU_URL_NAMES"),
-                        ('core.templatetags.navigation', 'WARNING',
-                         "can't find url some_other_page in MENU_URL_NAMES"),
-                        ('core.templatetags.navigation', 'WARNING',
-                         "can't find url some_page in MENU_URL_NAMES"))
-
-            # dangling subpage with no info on parent
-            with LogCapture() as l:
-                self.assertTrue(call_current('dangling_subpage',
-                                             'dangling_subpage'))
-                self.assertTrue(call_current('dangling_subpage',
-                                             'nonexistent_page'))
-                self.assertFalse(call_current('nonexistent_page',
-                                              'dangling_subpage'))
-                l.check(('core.templatetags.navigation', 'WARNING',
-                         "can't find url nonexistent_page in MENU_URL_NAMES"),
-                        ('core.templatetags.navigation', 'WARNING',
-                         "can't find url nonexistent_page in MENU_URL_NAMES"))
-
-            # parents
-            self.assertTrue(call_current('second_page', 'second_page'))
-            self.assertTrue(call_current('second_subpage', 'second_page'))
-            self.assertFalse(call_current('second_subpage', 'first_page'))
-            self.assertFalse(call_current('second_subpage', 'some_page'))
-
-            # aliases
-            self.assertTrue(call_current('second_page_alias', 'second_page'))
-            self.assertFalse(call_current('second_page_alias', 'first_page'))
-            self.assertFalse(call_current('second_page', 'second_page_alias'))
-
-            self.assertTrue(call_current('second_subpage_alias',
-                                         'second_page'))
-            self.assertTrue(call_current('second_subpage_alias',
-                                         'second_subpage'))
-            self.assertTrue(call_current('second_subpage_alias',
-                                         'second_subpage_alias'))
-            self.assertFalse(call_current('second_subpage_alias',
-                                          'first_page'))
-            self.assertFalse(call_current('second_page',
-                                          'second_subpage_alias'))
-            self.assertFalse(call_current('second_subpage',
-                                          'second_subpage_alias'))
-
-            self.assertTrue(call_current('second_subpage_alias_alias',
-                                         'second_page'))
-            self.assertTrue(call_current('second_subpage_alias_alias',
-                                         'second_subpage'))
-            self.assertTrue(call_current('second_subpage_alias_alias',
-                                         'second_subpage_alias'))
-
-            self.assertTrue(call_current('second_subpage_another_alias',
-                                         'second_page'))
-            self.assertTrue(call_current('second_subpage_another_alias',
-                                         'second_subpage'))
-            self.assertFalse(call_current('second_subpage_another_alias',
-                                          'second_subpage_alias'))
 
 
 class NotifyCommandTest(TestCase):
