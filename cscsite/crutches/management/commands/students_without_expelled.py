e3a5ba92cb41254c1ba2ac4091b7e016e95177e7
pacahon <pacabest@gmail.com>
2015-06-26 14:54:11 +0300

    add one crutch command

---
+++ cscsite/crutches/management/commands/students_without_expelled.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import csv
+import sys
+import unicodecsv
+
+from django.core.management import BaseCommand
+
+from learning.models import Semester, Enrollment
+from users.models import CSCUser
+from django.db.models import Count, Prefetch
+
+
+class Command(BaseCommand):
+    help = ("Get students without expelled status")
+
+    def handle(self, *args, **options):
+        w = unicodecsv.writer(sys.stdout, encoding='utf-8')
+
+        current_semester = Semester.objects.first()
+
+        students = (CSCUser.objects
+            .filter(
+                is_center_student=True,
+                groups__in=[CSCUser.IS_STUDENT_PK],
+                enrollment_year__isnull=False,
+                graduation_year__isnull=True
+            )
+            .exclude(
+                groups__in=[CSCUser.IS_GRADUATE_PK],
+                status__in=['will_graduate', 'expelled',]
+            )
+            .prefetch_related(
+                Prefetch(
+                    'enrollment_set',
+                    queryset=Enrollment.objects
+                        .exclude(grade__in=['not_graded', 'unsatisfactory'])
+                        .select_related('course_offering',
+                                        'course_offering__semester',
+                                        'course_offering__course'
+                                        ),
+                    to_attr='enrollments'
+                ),
+            )
+        )
+
+
+        for student in students:
+            w.writerow([student.get_full_name(), student.yandex_id, student.enrollment_year, len(student.enrollments), student.id])
+
+        sys.stdout.flush()
