7e42e8591afacb6539900cc83e43c41cae289c8a
pacahon <pacabest@gmail.com>
2015-06-30 13:31:26 +0300

    move flatpage_url templatetag to htmlpages app

--- cscsite/htmlpages/templatetags/htmlpage_url.py
+++ cscsite/htmlpages/templatetags/htmlpage_url.py
@@ -1,16 +1,16 @@
 from django import template
 from django.conf import settings
-from django.contrib.flatpages.models import FlatPage
 from django.contrib.sites.shortcuts import get_current_site
 from django.template import TemplateSyntaxError
 
 from core import FLATPAGE_URL_CACHE
+from htmlpages.models import HtmlPage
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def flatpage_url(context, url_name, **kwargs):
+def htmlpage_url(context, url_name, **kwargs):
     """Chaching flatpage urls. Check url exist in DB"""
     global FLATPAGE_URL_CACHE
     if not FLATPAGE_URL_CACHE:
@@ -18,8 +18,8 @@ def flatpage_url(context, url_name, **kwargs):
             site_pk = get_current_site(context['request']).pk
         else:
             site_pk = settings.SITE_ID
-        qs = FlatPage.objects.filter(sites__id=site_pk).values('url')
+        qs = HtmlPage.objects.filter(sites__id=site_pk).values('url')
         FLATPAGE_URL_CACHE = [p['url'] for p in qs]
     if url_name not in FLATPAGE_URL_CACHE:
-        raise TemplateSyntaxError('"flatpage_url" tag got an unknown variable: %r' % url_name)
+        raise TemplateSyntaxError('"htmlpage_url" tag got an unknown variable: %r' % url_name)
     return url_name
