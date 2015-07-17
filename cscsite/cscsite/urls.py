79c93e66d347a775bd9314418ad99a22033c3237
pacahon <pacabest@gmail.com>
2015-07-17 18:01:40 +0300

    add usefull routes for viewing 4xx/5xx error pages

--- cscsite/cscsite/urls.py
+++ cscsite/cscsite/urls.py
@@ -249,5 +249,16 @@ urlpatterns += patterns('loginas.views',
     url(r"^login/user/(?P<user_id>.+)/$", "user_login", name="loginas-user-login"),
 )
 
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    urlpatterns += [
+        url(r'^400/$', 'django.views.defaults.bad_request'),
+        url(r'^403/$', 'django.views.defaults.permission_denied'),
+        url(r'^404/$', 'django.views.defaults.page_not_found'),
+        url(r'^500/$', 'django.views.defaults.server_error'),
+    ]
+
+# Htmlpages should be the last one
 urlpatterns += patterns('',
     url(r'^(?P<url>.*/)$', views.flatpage, name='html_pages'))
