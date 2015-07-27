11421912af07a10acc49c338f4ad1b2ec8e63416
pacahon <pacabest@gmail.com>
2015-07-27 19:14:35 +0300

    add site specific templates support

--- cscsite/cscenter/settings/base.py
+++ cscsite/cscenter/settings/base.py
@@ -14,6 +14,8 @@ BASE_DIR = Path(__file__).ancestor(2)
 LOCALE_PATHS = (
     Path(BASE_DIR, "locale"),
 )
+# Add site specific templates
+TEMPLATES[0]['DIRS'] += [BASE_DIR.child("templates")]
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'chf0ao=8=ihflu_ln2&z+jke)*cx=k0e3mzuq+pc+x+6@vxrj7'
