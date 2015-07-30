e4277028cbacf966ce4a419cb5f23180ec33f0bf
pacahon <pacabest@gmail.com>
2015-07-30 14:49:50 +0300

    add filters to news detail view

--- cscsite/news/views.py
+++ cscsite/news/views.py
@@ -8,12 +8,17 @@ from news.models import News
 class NewsDetailView(generic.DetailView):
     model = News
 
+    def get_queryset(self):
+        return (self.model.objects
+                .filter(sites__pk=settings.SITE_ID,
+                        language=translation.get_language()))
+
 
 class NewsListView(generic.ListView):
     model = News
 
     def get_queryset(self):
-        return News.public.filter(sites__id=settings.SITE_ID,
+        return News.public.filter(sites__pk=settings.SITE_ID,
                                   language=translation.get_language())
 
 
