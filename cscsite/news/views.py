d47eeb551777bfc688ec32b1f33850992897de61
pacahon <pacabest@gmail.com>
2015-07-31 14:15:29 +0300

    add news rss feed (ru version only)

--- cscsite/news/views.py
+++ cscsite/news/views.py
@@ -1,8 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+
 from django.conf import settings
+from django.contrib.syndication.views import Feed
 from django.views import generic
 from django.utils import translation
+from django.contrib.sites.models import Site
 
-from news.models import News
+from .models import News
+
+
+class NewsFeed(Feed):
+    title = "Новости CSC"
+    link = "/news/"
+    description = ""
+
+    def __init__(self):
+        site = Site.objects.get_current()
+        if site.domain == 'compsciclub.ru':
+            self.title = 'Новости Computer Science Клуба'
+        else:
+            self.title = 'Новости Computer Science Центра'
+
+    def items(self):
+        return (News.public.filter(sites__pk=settings.SITE_ID, language='ru')
+                            .order_by('-created')[:10])
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.text
 
 
 class NewsDetailView(generic.DetailView):
