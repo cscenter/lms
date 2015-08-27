2cd3b850969186e6632f35d09ae20fc2e66b7f17
pacahon <pacabest@gmail.com>
2015-08-27 17:42:52 +0300

    update ya disk root dir

--- cscsite/cscenter/settings/production.py
+++ cscsite/cscenter/settings/production.py
@@ -86,7 +86,7 @@ SLIDESHARE_PASSWORD = "vorobey"
 
 YANDEX_DISK_USERNAME = "csc-slides@yandex.ru"
 YANDEX_DISK_PASSWORD = "deelthisat"
-YANDEX_DISK_SLIDES_ROOT = "/CSCenterMaterials/2014-2015"
+YANDEX_DISK_SLIDES_ROOT = "/CSCenterMaterials/2015-2016"
 
 
 
