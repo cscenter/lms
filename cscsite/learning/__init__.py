01a9b2089c830d04d86ee28c90b83131c775c5a3
pacahon <pacabest@gmail.com>
2015-12-17 14:04:30 +0300

    learning module cosmetics

--- cscsite/learning/__init__.py
+++ cscsite/learning/__init__.py
@@ -2,3 +2,7 @@
 Learning package is for anything learning-related, such as courses, assignments
 and so on. We can split this package in the future.
 """
+
+# TODO replace CSCUser with get_user_model and settings.AUTH_USER_MODEL
+# TODO: create import grades management command
+# TODO: create management command to set completed courses as passed
