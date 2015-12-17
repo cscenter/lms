aacf77532b74a473266db3057362cadf44f184c1
pacahon <pacabest@gmail.com>
2015-12-17 13:48:41 +0300

    add React.js as external var

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -3,24 +3,28 @@ var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 
 module.exports = {
-  context: __dirname,
+    context: __dirname,
 
-  entry: './cscsite/assets/js/index.jsx',
+    entry: './cscsite/assets/js/index.jsx',
 
-  output: {
-      path: path.resolve('./cscsite/assets/bundles/'),
-      filename: "[name]-[hash].js"
-  },
+    output: {
+        path: path.resolve('./cscsite/assets/bundles/'),
+        filename: "[name]-[hash].js"
+    },
 
-  plugins: [
-  ], // add all common plugins here
+    plugins: [], // add all common plugins here
 
-  module: {
-    loaders: [] // add all common loaders here
-  },
+    module: {
+        loaders: [] // add all common loaders here
+    },
 
-  resolve: {
-    modulesDirectories: ['node_modules', 'bower_components'],
-    extensions: ['', '.js', '.jsx']
-  },
+    externals: {
+        // get it from a global 'React' variable
+        'react': 'React'
+    },
+
+    resolve: {
+        modulesDirectories: ['node_modules', 'bower_components'],
+        extensions: ['', '.js', '.jsx']
+    },
 }
