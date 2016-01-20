0e31ec376fb75c66851e336cf17cc751e3b31d5a
pacahon <pacabest@gmail.com>
2016-01-20 17:30:15 +0300

    some cosmetics for password reset

--- cscsite/cscenter/settings/local.py
+++ cscsite/cscenter/settings/local.py
@@ -35,10 +35,9 @@ ROSETTA_MESSAGES_SOURCE_LANGUAGE_NAME = 'Russian'
 THUMBNAIL_DEBUG = True
 
 EMAIL_HOST = '127.0.0.1'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = 'noreply@compscicenter.ru'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 1025
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGGING = {
     'version': 1,
