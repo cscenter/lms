9a9b6e00a67d424cf83e872bea351c980d738c36
pacahon <pacabest@gmail.com>
2016-02-02 18:41:17 +0300

    update slides_url ans slideshare file if course class slides changed

--- cscsite/learning/apps.py
+++ cscsite/learning/apps.py
@@ -1,14 +1,15 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_init
 from django.utils.translation import ugettext_lazy as _
 
-from .signals import (maybe_upload_slides, populate_assignment_students,
-                        create_deadline_change_notification,
-                      populate_student_assignments,
+from .signals import (maybe_upload_slides,
+                      create_student_assignments_for_new_assignment,
+                      create_deadline_change_notification,
+                      populate_assignments_for_new_enrolled_student,
                       create_assignment_comment_notification,
                       update_last_commented_date_on_student_assignment,
                       create_course_offering_news_notification,
-                      mark_assignment_passed)
+                      mark_assignment_passed, track_fields_post_init)
 
 
 class LearningConfig(AppConfig):
@@ -16,8 +17,7 @@ class LearningConfig(AppConfig):
     verbose_name = _("Learning")
 
     def ready(self):
-        # FIXME:????
-        post_save.connect(populate_assignment_students,
+        post_save.connect(create_student_assignments_for_new_assignment,
                           sender=self.get_model('Assignment'))
         post_save.connect(create_deadline_change_notification,
                           sender=self.get_model('Assignment'))
@@ -27,9 +27,13 @@ class LearningConfig(AppConfig):
                           sender=self.get_model('AssignmentComment'))
         post_save.connect(mark_assignment_passed,
                           sender=self.get_model('AssignmentComment'))
+        post_init.connect(track_fields_post_init,
+                          sender=self.get_model('CourseClass'),
+                          dispatch_uid='learning.signals.course_class_post_init')
         post_save.connect(maybe_upload_slides,
-                          sender=self.get_model('CourseClass'))
+                          sender=self.get_model('CourseClass'),
+                          dispatch_uid='learning.signals.course_class_maybe_upload_slides')
         post_save.connect(create_course_offering_news_notification,
                           sender=self.get_model('CourseOfferingNews'))
-        post_save.connect(populate_student_assignments,
+        post_save.connect(populate_assignments_for_new_enrolled_student,
                           sender=self.get_model('Enrollment'))
