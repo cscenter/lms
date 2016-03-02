f6bb1cf1b2d46deb02bf74b24718366e5d6c3b9f
pacahon <pacabest@gmail.com>
2016-03-02 19:35:27 +0300

    management cmd for teacher groups recalculation

---
+++ cscsite/learning/management/commands/check_teachers_groups.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.core.management import BaseCommand
+
+from learning.models import CourseOffering
+from learning.settings import PARTICIPANT_GROUPS
+from users.models import CSCUser
+
+
+class Command(BaseCommand):
+    help = ("Update teachers groups from data based on course offerings info")
+
+    CENTER_ONLY = 1
+    CLUB_ONLY = 2
+    BOTH = 3
+
+    def handle(self, *args, **options):
+        teachers = {}
+        cos = CourseOffering.objects.prefetch_related("teachers")
+        for co in cos:
+            for teacher in co.teachers.all():
+                if not teacher.pk in teachers:
+                    teachers[teacher.pk] = {
+                        "state": self.CLUB_ONLY if co.is_open else self.CENTER_ONLY,
+                        "obj": teacher
+                    }
+                if teachers[teacher.pk]["state"] == self.BOTH:
+                    continue
+                if teachers[teacher.pk]["state"] == self.CENTER_ONLY and co.is_open:
+                    teachers[teacher.pk]["state"] = self.BOTH
+                elif teachers[teacher.pk]["state"] == self.CLUB_ONLY and not co.is_open:
+                    teachers[teacher.pk]["state"] = self.BOTH
+        # Ok, try to update teacher groups
+        for teacher in teachers:
+            if teachers[teacher]["state"] == self.BOTH:
+                teachers[teacher]["obj"].groups.add(PARTICIPANT_GROUPS.TEACHER_CENTER)
+                teachers[teacher]["obj"].groups.add(PARTICIPANT_GROUPS.TEACHER_CLUB)
+            elif teachers[teacher]["state"] == self.CLUB_ONLY:
+                teachers[teacher]["obj"].groups.add(PARTICIPANT_GROUPS.TEACHER_CLUB)
+                teachers[teacher]["obj"].groups.remove(PARTICIPANT_GROUPS.TEACHER_CENTER)
+            elif teachers[teacher]["state"] == self.CENTER_ONLY:
+                teachers[teacher]["obj"].groups.add(PARTICIPANT_GROUPS.TEACHER_CENTER)
+                teachers[teacher]["obj"].groups.remove(PARTICIPANT_GROUPS.TEACHER_CLUB)
+
