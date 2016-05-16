4518452719cdb787e40a469d3a1d57bff5087d76
pacahon <pacabest@gmail.com>
2016-05-16 17:43:42 +0300

    similar applicants tab in interview app

--- cscsite/learning/admission/urls.py
+++ cscsite/learning/admission/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import include, url
 
 from learning.admission.views import InterviewListView, InterviewDetailView, \
-    ApplicantResultsListView, ApplicantResultsDetailView, \
+    ApplicantListView, ApplicantDetailView, \
     ApplicantStatusUpdateView
 
 urlpatterns = [
     url(r'^admission', include([
-        url(r'^/applicants/$', ApplicantResultsListView.as_view(), name='admission_applicants'),
-        url(r'^/applicants/(?P<pk>\d+)/$', ApplicantResultsDetailView.as_view(), name='admission_applicant_detail'),
+        url(r'^/applicants/$', ApplicantListView.as_view(), name='admission_applicants'),
+        url(r'^/applicants/(?P<pk>\d+)/$', ApplicantDetailView.as_view(), name='admission_applicant_detail'),
         url(r'^/applicants/status/(?P<pk>\d+)/$', ApplicantStatusUpdateView.as_view(), name='admission_applicant_status_update'),
-        url(r'^/interviews/$', InterviewListView.as_view(), name='admission_dashboard'),
+        url(r'^/interviews/$', InterviewListView.as_view(), name='admission_interviews'),
         url(r'^/interviews/(?P<pk>\d+)$', InterviewDetailView.as_view(), name='admission_interview_detail'),
     ])),
 ]
