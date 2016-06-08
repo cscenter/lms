f8496c6c2f552e831989cb590400e1d927573b48
pacahon <pacabest@gmail.com>
2016-06-08 15:50:19 +0300

    hide kzn courses on user detail page for center_site and show only open CO on club (with filter by city)

--- cscsite/users/views.py
+++ cscsite/users/views.py
@@ -2,40 +2,34 @@
 
 from __future__ import absolute_import, unicode_literals
 
-from datetime import datetime, time, timedelta
+from datetime import datetime, time
 from itertools import chain, repeat
 
-from six import iteritems
-from six.moves import zip
+from braces.views import LoginRequiredMixin
+from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.contrib import auth
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from django.db.models import Prefetch
-from django.http import HttpResponseRedirect, Http404, HttpResponse,\
-    JsonResponse
-from django.utils.decorators import method_decorator
-from django.views.decorators.debug import sensitive_post_parameters
-from django.views.decorators.cache import never_cache
-from django.views import generic
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.utils import timezone
+from django.utils.decorators import method_decorator
 from django.utils.http import is_safe_url
-from django.utils.translation import ugettext_lazy as _
-from braces.views import LoginRequiredMixin
-
-from dateutil.relativedelta import relativedelta
-import icalendar
+from django.views import generic
+from django.views.decorators.cache import never_cache
+from django.views.decorators.debug import sensitive_post_parameters
 from icalendar import Calendar, Event, vText, vUri
 from icalendar.prop import vInline
-import pytz
+from six.moves import zip
 
 from core.views import ProtectedFormMixin, SuperUserOnlyMixin
-from learning.settings import LEARNING_BASE, TEACHING_BASE
-from learning.viewmixins import CuratorOnlyMixin
 from learning.models import CourseClass, Assignment, StudentAssignment, \
-    CourseOffering, NonCourseEvent, Semester, StudentProject, Enrollment
+    CourseOffering, NonCourseEvent, Semester, Enrollment
+from learning.settings import LEARNING_BASE, TEACHING_BASE
+from users.utils import create_timezone
 from .forms import LoginForm, UserProfileForm, CSCUserReferenceCreateForm
-from .models import CSCUser, CSCUserReference, SHADCourseRecord
+from .models import CSCUser, CSCUserReference
 
 
 # inspired by https://raw2.github.com/concentricsky/django-sky-visitor/
@@ -149,10 +143,10 @@ class UserDetailView(generic.DetailView):
         if not self.request.user.is_authenticated():
             enrollment_queryset = enrollment_queryset.exclude(
                 grade__in=['not_graded', 'unsatisfactory'])
+        co_queryset = (CourseOffering.custom.site_related(self.request)
+                       .select_related('semester', 'course'))
         prefetch_list = [
-            'teaching_set',
-            'teaching_set__semester',
-            'teaching_set__course',
+            Prefetch('teaching_set', queryset=co_queryset.all()),
             'shadcourserecord_set',
             Prefetch('enrollment_set', queryset=enrollment_queryset)
         ]
@@ -267,109 +261,6 @@ class UserReferenceDetailView(SuperUserOnlyMixin, generic.DetailView):
                                   student_info.shads)
 
         return context
-
-
-# The following code has been taken from
-# https://github.com/geier/khal/blob/498df2ef62a99bb1a50
-# 053e982f7f23a1bfb3600/khal/khalendar/event.py
-# See https://github.com/collective/icalendar/issues/44
-# FIXME: it's fixed in upstream, can be removed with retesting
-#        and corresponding code changes
-def to_naive_utc(dtime):
-    """convert a datetime object to UTC and than remove the tzinfo, if
-    datetime is naive already, return it
-    """
-    if not hasattr(dtime, 'tzinfo') or dtime.tzinfo is None:
-        return dtime
-
-    dtime_utc = dtime.astimezone(pytz.UTC)
-    dtime_naive = dtime_utc.replace(tzinfo=None)
-    return dtime_naive
-
-
-def create_timezone(tz, first_date=None, last_date=None):
-    """
-    create an icalendar vtimezone from a pytz.tzinfo
-
-    :param tz: the timezone
-    :type tz: pytz.tzinfo
-    :param first_date: the very first datetime that needs to be included in the
-    transition times, typically the DTSTART value of the (first recurring)
-    event
-    :type first_date: datetime.datetime
-    :param last_date: the last datetime that needs to included, typically the
-    end of the (very last) event (of a recursion set)
-    :returns: timezone information
-    :rtype: icalendar.Timezone()
-
-    we currently have a problem here:
-
-       pytz.timezones only carry the absolute dates of time zone transitions,
-       not their RRULEs. This will a) make for rather bloated VTIMEZONE
-       components, especially for long recurring events, b) we'll need to
-       specify for which time range this VTIMEZONE should be generated and c)
-       will not be valid for recurring events that go into eternity.
-
-    Possible Solutions:
-
-    As this information is not provided by pytz at all, there is no
-    easy solution, we'd really need to ship another version of the OLSON DB.
-
-    """
-
-    # TODO last_date = None, recurring to infintiy
-
-    first_date = (datetime.today() if not first_date
-                  else to_naive_utc(first_date))
-    last_date = datetime.today() if not last_date else to_naive_utc(last_date)
-    timezone = icalendar.Timezone()
-    timezone.add('TZID', tz)
-
-    dst = {one[2]: 'DST' in two.__repr__()
-           for one, two in iteritems(tz._tzinfos)}
-
-    # looking for the first and last transition time we need to include
-    first_num, last_num = 0, len(tz._utc_transition_times) - 1
-    first_tt = tz._utc_transition_times[0]
-    last_tt = tz._utc_transition_times[-1]
-    for num, dt in enumerate(tz._utc_transition_times):
-        if dt > first_tt and dt < first_date:
-            first_num = num
-            first_tt = dt
-        if dt < last_tt and dt > last_date:
-            last_num = num
-            last_tt = dt
-
-    timezones = dict()
-    for num in range(first_num, last_num + 1):
-        name = tz._transition_info[num][2]
-        if name in timezones:
-            ttime = (tz.fromutc(tz._utc_transition_times[num])
-                     .replace(tzinfo=None))
-            if 'RDATE' in timezones[name]:
-                timezones[name]['RDATE'].dts.append(
-                    icalendar.prop.vDDDTypes(ttime))
-            else:
-                timezones[name].add('RDATE', ttime)
-            continue
-
-        if dst[name]:
-            subcomp = icalendar.TimezoneDaylight()
-        else:
-            subcomp = icalendar.TimezoneStandard()
-
-        subcomp.add('TZNAME', tz._transition_info[num][2])
-        subcomp.add(
-            'DTSTART',
-            tz.fromutc(tz._utc_transition_times[num]).replace(tzinfo=None))
-        subcomp.add('TZOFFSETTO', tz._transition_info[num][0])
-        subcomp.add('TZOFFSETFROM', tz._transition_info[num - 1][0])
-        timezones[name] = subcomp
-
-    for subcomp in timezones.values():
-        timezone.add_component(subcomp)
-
-    return timezone
 
 
 class ICalView(generic.base.View):
