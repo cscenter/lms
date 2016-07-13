7b3a162a27a146e5ad435b8c2d3aa8b0cd7496c2
pacahon <pacabest@gmail.com>
2016-07-13 13:21:39 +0300

    fix 404 handler behavior for Django 1.9

--- cscsite/cscenter/urls.py
+++ cscsite/cscenter/urls.py
@@ -153,10 +153,14 @@ if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        url(r'^400/$', 'django.views.defaults.bad_request'),
-        url(r'^403/$', 'django.views.defaults.permission_denied'),
-        url(r'^404/$', 'django.views.defaults.page_not_found'),
-        url(r'^500/$', 'django.views.defaults.server_error'),
+        url(r'^400/$', 'django.views.defaults.bad_request',
+            kwargs={'exception': Exception("Page not Found")}),
+        url(r'^403/$', 'django.views.defaults.permission_denied',
+            kwargs={'exception': Exception("Page not Found")}),
+        url(r'^404/$', 'django.views.defaults.page_not_found',
+            kwargs={'exception': Exception("Page not Found")}),
+        url(r'^500/$', 'django.views.defaults.server_error',
+            kwargs={'exception': Exception("Page not Found")}),
     ]
 
 # Note: htmlpages should be the last one
