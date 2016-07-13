a4f050a8ad07c91f0981af3d837ee53ce0d2bc76
pacahon <pacabest@gmail.com>
2016-07-13 15:34:29 +0300

    fix import settings problems

--- cscsite/csclub/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-
-# Note(lebedev): this is currently hardcoded for AWS server.
-if socket.gethostname() == "ip-10-82-139-53":
-    from .production import *
-else:
-    from .local import *
