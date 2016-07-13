c7064b1377826949d986dad3299d0bd10f3e73b3
pacahon <pacabest@gmail.com>
2016-07-13 12:37:30 +0300

    minor fix on assignment_submission page; fix Django 1.9 warnings

--- cscsite/learning/admission/urls.py
+++ cscsite/learning/admission/urls.py
@@ -5,12 +5,12 @@ from learning.admission.views import InterviewListView, InterviewDetailView, \
     ApplicantStatusUpdateView, InterviewResultsView
 
 urlpatterns = [
-    url(r'^admission', include([
-        url(r'^/applicants/$', ApplicantListView.as_view(), name='admission_applicants'),
-        url(r'^/applicants/(?P<pk>\d+)/$', ApplicantDetailView.as_view(), name='admission_applicant_detail'),
-        url(r'^/applicants/status/(?P<pk>\d+)/$', ApplicantStatusUpdateView.as_view(), name='admission_applicant_status_update'),
-        url(r'^/interviews/$', InterviewListView.as_view(), name='admission_interviews'),
-        url(r'^/interviews/(?P<pk>\d+)$', InterviewDetailView.as_view(), name='admission_interview_detail'),
-        url(r'^/results/$', InterviewResultsView.as_view(), name='admission_interview_results'),
+    url(r'^admission/', include([
+        url(r'^applicants/$', ApplicantListView.as_view(), name='admission_applicants'),
+        url(r'^applicants/(?P<pk>\d+)/$', ApplicantDetailView.as_view(), name='admission_applicant_detail'),
+        url(r'^applicants/status/(?P<pk>\d+)/$', ApplicantStatusUpdateView.as_view(), name='admission_applicant_status_update'),
+        url(r'^interviews/$', InterviewListView.as_view(), name='admission_interviews'),
+        url(r'^interviews/(?P<pk>\d+)$', InterviewDetailView.as_view(), name='admission_interview_detail'),
+        url(r'^results/$', InterviewResultsView.as_view(), name='admission_interview_results'),
     ])),
 ]
