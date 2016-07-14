a235fabb0f083d590bda8853c25c1a57a1eb3d73
pacahon <pacabest@gmail.com>
2016-07-14 14:14:47 +0300

    not enough space for backup *.tar on /tmp/, move tmp backup folder to /shared/

--- cscsite/cscenter/settings/production.py
+++ cscsite/cscenter/settings/production.py
@@ -101,6 +101,7 @@ DBBACKUP_STORAGE_OPTIONS = {
     'secret_key': '2TA5synS+IQW9LISnuwAbnFwOvdKC31XBeeEUTqd',
     'calling_format': 'boto.s3.connection.OrdinaryCallingFormat'
 }
+DBBACKUP_TMP_DIR = '/shared/backup_tmp'
 DBBACKUP_DATE_FORMAT = '%d-%m-%Y-%H'
 DBBACKUP_FILENAME_TEMPLATE = 'backups/{servername}/{datetime}/{content_type}.{extension}'
 DBBACKUP_MEDIA_FILENAME_TEMPLATE = 'backups/{servername}/{datetime}/{content_type}.{extension}'
