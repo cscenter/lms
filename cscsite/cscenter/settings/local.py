f5a9c3ea157c9a70b115667d41630c24ea883ad2
pacahon <pacabest@gmail.com>
2016-07-14 11:29:00 +0300

    improve logging settings on local env

--- cscsite/cscenter/settings/local.py
+++ cscsite/cscenter/settings/local.py
@@ -40,6 +40,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGGING = {
     'version': 1,
+    'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
