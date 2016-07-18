8d795f09904ecc822a1118ad6ea55f0416375eb1
pacahon <pacabest@gmail.com>
2016-07-18 17:15:35 +0300

    attempt to fix learning migrations #2

--- cscsite/learning/migrations/0003_recalculate_semester_indexes.py
+++ cscsite/learning/migrations/0003_recalculate_semester_indexes.py
@@ -3,17 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
-def recalculate_semester_indexes(apps, schema_editor):
-    from django.core.management import call_command
-    call_command('recalculate_semester_indexes')
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('learning', '0002_auto_20160302_1445'),
-        ('learning', '0020_auto_20160718_1654'),
     ]
 
     operations = [
-        migrations.RunPython(recalculate_semester_indexes),
     ]
