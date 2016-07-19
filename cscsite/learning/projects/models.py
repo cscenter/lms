2c20b42af8830e28503a726beb92ff17bdd07a41
pacahon <pacabest@gmail.com>
2016-07-19 16:32:46 +0300

    more grades on project model; remove legacy code

--- cscsite/learning/projects/models.py
+++ cscsite/learning/projects/models.py
@@ -5,21 +5,19 @@ from __future__ import absolute_import, unicode_literals
 import os.path
 
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible, smart_text
 from django.utils.translation import ugettext_lazy as _
-from model_utils import Choices, choices
-from model_utils.fields import StatusField
+from model_utils import Choices
 from model_utils.models import TimeStampedModel
 
 from core.models import LATEX_MARKDOWN_HTML_ENABLED
 from learning.models import Semester
 from learning.settings import GRADES, PARTICIPANT_GROUPS
-from learning.utils import get_current_semester_pair
 
 
 def project_slides_file_name(self, filename):
-    year, type = get_current_semester_pair()
     return os.path.join('project_presentations',
                         '{}-{}'.format(self.semester.year, self.semester.type),
                         filename)
@@ -30,24 +28,24 @@ class ProjectStudent(models.Model):
     GRADES = GRADES
     student = models.ForeignKey(settings.AUTH_USER_MODEL)
     project = models.ForeignKey('Project')
+    supervisor_grade = models.SmallIntegerField(
+        verbose_name=_("Supervisor grade"),
+        validators=[MinValueValidator(-15), MaxValueValidator(15)],
+        blank=True,
+        null=True)
+    supervisor_review = models.TextField(
+        _("Review from supervisor"),
+        blank=True)
+    presentation_grade = models.PositiveSmallIntegerField(
+        verbose_name=_("Presentation grade"),
+        validators=[MaxValueValidator(10)],
+        blank=True,
+        null=True)
     final_grade = models.CharField(
         verbose_name=_("Final grade"),
         choices=GRADES,
         max_length=15,
         default=GRADES.not_graded)
-    supervisor_review = models.TextField(
-        _("Review from supervisor"),
-        blank=True)
-    # supervisor_grade = models.CharField(
-    #     verbose_name=_("Supervisor grade"),
-    #     choices=GRADES,
-    #     max_length=15,
-    #     default=GRADES.not_graded)
-    # presentation_grade = models.CharField(
-    #     verbose_name=_("Presentation grade"),
-    #     choices=GRADES,
-    #     max_length=15,
-    #     default=GRADES.not_graded)
 
     class Meta:
         verbose_name = _("Project student")
@@ -98,9 +96,6 @@ class Project(TimeStampedModel):
         default=False)
 
     class Meta:
-        # NOTE(Dmitry): we should probably order by min of semesters,
-        #               but it can't be enforced here
-        ordering = ["name"]
         verbose_name = _("Student project")
         verbose_name_plural = _("Student projects")
 
@@ -109,9 +104,3 @@ class Project(TimeStampedModel):
 
     def get_absolute_url(self):
         return self.student.get_absolute_url()
-
-    # this is needed to share code between CourseClasses and this model
-    # FIXME: really?!
-    @property
-    def project_type_display(self):
-        return self.PROJECT_TYPES[self.project_type]
