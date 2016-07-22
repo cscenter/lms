5618a00d91459a79c6a662755e2a1c0830c6f81e
pacahon <pacabest@gmail.com>
2016-07-22 19:40:58 +0300

    try to fix base url in notification emails

--- cscsite/core/management/commands/notify.py
+++ cscsite/core/management/commands/notify.py
@@ -19,29 +19,52 @@ from index.models import EnrollmentApplEmail
 
 # import cscenter.urls
 
-BASE_URL = "http://compscicenter.ru"
+EMAILS = {
+    'new_comment_for_student': {
+        'title': "Преподаватель оставил комментарий к решению задания",
+        'template': "emails/new_comment_for_student.html"
+    },
+    'assignment_passed': {
+        'title': "Студент сдал домашнее задание",
+        'template': "emails/assignment_passed.html"
+    },
+    'new_comment_for_teacher': {
+        'title': "Студент оставил комментарий к решению задания",
+        'template': "emails/new_comment_for_teacher.html"
+    },
+    'new_courseoffering_news': {
+        'title': "Добавлена новость к курсу",
+        'template': "emails/new_courseoffering_news.html"
+    },
+    'deadline_changed': {
+        'title': "Изменился срок сдачи домашнего задания",
+        'template': "emails/deadline_changed.html"
+    },
+    'new_assignment': {
+        'title': "Появилось новое домашнее задание",
+        'template': "emails/new_assignment.html"
+    },
+    'enrollment_application': {
+        'title': "Спасибо за заполнение заявки на поступление в CSC",
+        'template': "emails/enrollment_application.html"
+    }
+}
 
-EMAILS = {'new_comment_for_student':
-          {'title': "Преподаватель оставил комментарий к решению задания",
-           'template': "emails/new_comment_for_student.html"},
-          'assignment_passed':
-          {'title': "Студент сдал домашнее задание",
-           'template': "emails/assignment_passed.html"},
-          'new_comment_for_teacher':
-          {'title': "Студент оставил комментарий к решению задания",
-           'template': "emails/new_comment_for_teacher.html"},
-          'new_courseoffering_news':
-          {'title': "Добавлена новость к курсу",
-           'template': "emails/new_courseoffering_news.html"},
-          'deadline_changed':
-          {'title': "Изменился срок сдачи домашнего задания",
-           'template': "emails/deadline_changed.html"},
-          'new_assignment':
-          {'title': "Появилось новое домашнее задание",
-           'template': "emails/new_assignment.html"},
-          'enrollment_application':
-          {'title': "Спасибо за заполнение заявки на поступление в CSC",
-           'template': "emails/enrollment_application.html"}}
+
+def get_base_url(notification):
+    user = notification.user
+    if isinstance(notification, AssignmentNotification):
+        co = notification.student_assignment.assignment.course_offering
+    elif isinstance(notification, CourseOfferingNewsNotification):
+        co = notification.course_offering_news.course_offering
+    else:
+        raise NotImplementedError()
+    if not user.is_student_center:
+        if co.get_city() == "spb":
+            return "http://compsciclub.ru"
+        else:
+            return "http://{}.compsciclub.ru".format(co.get_city())
+    return "https://compscicenter.ru"
 
 
 def report(f, s):
@@ -74,6 +97,7 @@ def notify(notification, name, context, f):
     notification.save()
 
 
+# FIXME: investigate and delete legacy code
 def notify_enrollment_appl(application, context, f):
     name = 'enrollment_application'
 
@@ -98,31 +122,35 @@ class Command(BaseCommand):
     can_import_settings = True
 
     def handle(self, *args, **options):
+        # TODO: Looks ugly, rewrite it cls
         from django.conf import settings
         translation.activate(settings.LANGUAGE_CODE)
 
-        notifications_assignments \
-            = (AssignmentNotification.objects
-               .filter(is_unread=True, is_notified=False)
-               .prefetch_related(
-                   'user',
-                   'student_assignment',
-                   'student_assignment__assignment',
-                   'student_assignment__assignment__course_offering',
-                   'student_assignment__assignment__course_offering__course',
-                   'student_assignment__student'))
+        notifications_assignments = (
+            AssignmentNotification.objects
+            .filter(is_unread=True, is_notified=False)
+            .select_related("user")
+            .prefetch_related(
+                "user__groups",
+                'student_assignment',
+                'student_assignment__assignment',
+                'student_assignment__assignment__course_offering',
+                'student_assignment__assignment__course_offering__course',
+                'student_assignment__student')
+        )
 
         for notification in notifications_assignments:
+            base_url = get_base_url(notification)
             a_s = notification.student_assignment
             context = {'a_s_link_student':
-                       BASE_URL + reverse('a_s_detail_student',
-                                          args=[a_s.pk]),
+                           base_url + reverse('a_s_detail_student',
+                                              args=[a_s.pk]),
                        'a_s_link_teacher':
-                       BASE_URL + reverse('a_s_detail_teacher',
-                                          args=[a_s.pk]),
+                           base_url + reverse('a_s_detail_teacher',
+                                              args=[a_s.pk]),
                        'assignment_link':
-                       BASE_URL + reverse('assignment_detail_teacher',
-                                          args=[a_s.assignment.pk]),
+                           base_url + reverse('assignment_detail_teacher',
+                                              args=[a_s.assignment.pk]),
                        'a_s_created':
                        a_s.created,
                        'assignment_name':
@@ -152,20 +180,21 @@ class Command(BaseCommand):
         notifications_courseoffering_news \
             = (CourseOfferingNewsNotification.objects
                .filter(is_unread=True, is_notified=False)
+               .select_related("user")
                .prefetch_related(
-                   'user',
+                   'user__groups',
                    'course_offering_news',
                    'course_offering_news__course_offering',
                    'course_offering_news__course_offering__course',
                    'course_offering_news__course_offering__semester'))
 
         for notification in notifications_courseoffering_news:
+            base_url = get_base_url(notification)
             course_offering = notification.course_offering_news.course_offering
-
             name = 'new_courseoffering_news'
             context = {'courseoffering_link':
-                       BASE_URL
-                       + reverse('course_offering_detail',
+                           base_url
+                           + reverse('course_offering_detail',
                                  args=[course_offering.course.slug,
                                        course_offering.semester.slug]),
                        'courseoffering_name':
@@ -178,7 +207,7 @@ class Command(BaseCommand):
                        smart_text(course_offering.course)}
 
             notify(notification, name, context, self.stdout)
-
+        # FIXME: looks like I have to delete this legacy code!
         enrollment_applications = (EnrollmentApplEmail.objects
                                    .filter(is_notified=False))
         for ea in enrollment_applications:
