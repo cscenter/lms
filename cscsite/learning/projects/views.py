56aeb0892ec93366e83ad321beda8f464842e7c2
pacahon <pacabest@gmail.com>
2016-07-22 16:00:37 +0300

    some tests on projects list

--- cscsite/learning/projects/views.py
+++ cscsite/learning/projects/views.py
@@ -27,14 +27,16 @@ class ReviewerProjectsView(ProjectReviewerOnlyMixin, generic.ListView):
     paginate_by = 50
     context_object_name = "projects"
     FILTER_NAME = "show"
-    PROJECT_ENROLLED = "enrolled"
-    PROJECT_ALL = "all"  # FIXME: by fact - shows only active from current term
-    PROJECT_ARCHIVE = "archive"
-    FILTER_VALUES = [PROJECT_ENROLLED, PROJECT_ALL, PROJECT_ARCHIVE]
+    PROJECT_ACTIVE = "active"  # show enrollments for current term
+    PROJECT_AVAILABLE = "available"  # show all projects for current term
+    PROJECT_ARCHIVE = "archive"  # past enrollments
+    PROJECT_ALL = "all"  # all projects
+    FILTER_VALUES = [PROJECT_ACTIVE, PROJECT_AVAILABLE, PROJECT_ARCHIVE,
+                     PROJECT_ALL]
 
     def get_template_names(self):
         project_type = self.request.GET[self.FILTER_NAME]
-        if project_type == self.PROJECT_ALL:
+        if project_type == self.PROJECT_AVAILABLE:
             return ["learning/projects/all.html"]
         elif project_type == self.PROJECT_ARCHIVE:
             return ["learning/projects/archive.html"]
@@ -42,37 +44,32 @@ class ReviewerProjectsView(ProjectReviewerOnlyMixin, generic.ListView):
             return ["learning/projects/enrolled.html"]
 
     def get(self, request, *args, **kwargs):
-        # Set default filter if not provided
-        if (self.FILTER_NAME not in request.GET or
-                request.GET[self.FILTER_NAME] not in self.FILTER_VALUES):
+        # Dispatch, by default show enrollments from current term
+        is_valid_filter = (self.FILTER_NAME in request.GET and
+                           request.GET[self.FILTER_NAME] in self.FILTER_VALUES)
+        if not is_valid_filter:
             path = request.path
             params = request.GET.copy()
-            params[self.FILTER_NAME] = self.PROJECT_ENROLLED
-            return HttpResponsePermanentRedirect("{}?{}".format(
-                path, params.urlencode()))
+            params[self.FILTER_NAME] = self.PROJECT_ACTIVE
+            return HttpResponsePermanentRedirect(
+                "{}?{}".format(path, params.urlencode())
+            )
 
-        response = super(ReviewerProjectsView, self).get(request, *args,
-                                                         **kwargs)
-        # FIXME: Remove implicit redirect
-        # If enrolled list empty - show all projects
-        # if not response.context_data["paginator"].count:
-        #     return HttpResponseRedirect("{}?{}={}".format(
-        #         reverse("projects:reviewer_projects"),
-        #         self.FILTER_NAME,
-        #         self.PROJECT_ALL))
-        return response
+        return super(ReviewerProjectsView, self).get(request, *args, **kwargs)
 
     def get_queryset(self):
+        current_year, term_type = get_current_semester_pair()
+        current_term_index = get_term_index(current_year, term_type)
+        project_type = self.request.GET[self.FILTER_NAME]
         queryset = (Project.objects
                     .select_related("semester")
                     .prefetch_related("students"))
-        current_year, term_type = get_current_semester_pair()
-        term_index = get_term_index(current_year, term_type)
-        project_type = self.request.GET[self.FILTER_NAME]
-        if project_type == self.PROJECT_ENROLLED:
-            queryset = queryset.filter(reviewers=self.request.user,
-                                       semester__index=term_index)
-        return queryset.order_by("name", "pk")
+
+        if project_type in [self.PROJECT_ACTIVE, self.PROJECT_AVAILABLE]:
+            queryset = queryset.filter(semester__index=current_term_index)
+        if project_type in [self.PROJECT_ACTIVE, self.PROJECT_ARCHIVE]:
+            queryset = queryset.filter(reviewers=self.request.user)
+        return queryset.order_by("-semester__index", "name", "pk")
 
 
 class ReviewerAvailableProjectsView(ProjectReviewerOnlyMixin, generic.ListView):
