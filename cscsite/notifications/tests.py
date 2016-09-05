44f94d185264199df4181e86666b4d2cd05be628
pacahon <pacabest@gmail.com>
2016-09-05 17:16:11 +0300

    update pytest-django

--- cscsite/notifications/tests.py
+++ cscsite/notifications/tests.py
@@ -51,7 +51,6 @@ class NotificationManagersTest(TestCase):
         self.message_count = 10
         self.from_user = UserFactory.create(username="from2", password="pwd", email="example@example.com")
         self.to_user = UserFactory.create(username="to2", password="pwd", email="example@example.com")
-        print("FIND ME", len(Group.objects.all()))
         self.to_group = Group.objects.create(name="to2_g")
         self.to_group.user_set.add(self.to_user)
         for i in range(self.message_count):
