1c9146ac853acbd7c808526e5c6d749a262f99f6
pacahon <pacabest@gmail.com>
2016-09-07 16:31:58 +0300

    redesign top menu

--- cscsite/menu_extension/templatetags/csc_menu_tags.py
+++ cscsite/menu_extension/templatetags/csc_menu_tags.py
@@ -13,7 +13,6 @@ register = template.Library()
 
 
 # TODO: Add nested level support
-# TODO: Rewrite like a class
 # FIXME: Remove deepcopy
 # FIXME: Don't modify current list in `handle_selected`
 @register.assignment_tag(takes_context=True)
@@ -23,11 +22,12 @@ def csc_menu(context, menu_name, root_id=False):
         try:
             menu = Menu.objects.get(name=menu_name)
             flattened = list(MenuItem.objects.filter(menu__pk=menu.id)
-                                         .select_related('extension',)
-                                         .prefetch_related('extension__groups',)
-                                         .order_by('level', 'rank').all())
+                             .select_related('extension',)
+                             .prefetch_related('extension__groups',)
+                             .order_by('level', 'rank')
+                             .all())
             for item in flattened:
-                # Caching groups. Empty, if allowed for all groups
+                # Groups list is empty if item available for all users.
                 item.groups_allowed = [g.id for g in item.extension.groups.all()]
                 # Override url from named_url if specified
                 if item.named_url:
@@ -37,7 +37,7 @@ def csc_menu(context, menu_name, root_id=False):
                         if settings.DEBUG:
                             raise e
                         else:
-                            item.url = "/None/"
+                            item.url = "/#undefined"
             CSCMENU_CACHE[menu_name] = flattened
         except (MenuItem.DoesNotExist, Menu.DoesNotExist) as e:
             if settings.DEBUG:
@@ -47,36 +47,35 @@ def csc_menu(context, menu_name, root_id=False):
     else:
         flattened = CSCMENU_CACHE[menu_name]
 
-    flattened_copy = copy.deepcopy(flattened)
-
     root_id = int(root_id)
-
     # Flattened to tree
     menu_tree = []
-    for item in flattened_copy:
-        if not allowed_for_user(item, context):
+    for item in flattened:
+        if not has_permissions(item, context['request'].user):
             continue
-        # FIXME: root_id support only first level. It's stupid.
+        # FIXME: What is root id?
         if root_id and item.level == 1 and item.id != root_id:
             continue
         if item.level == 1:
-            item.children = children(item.id, flattened_copy)
+            item.children = children(item.id, flattened)
             menu_tree.append(item)
     # For simplicity at the current time we have only one selected item.
-    handle_selected(menu_tree, context['request'])
-    return menu_tree
+    active_items = set()
+    find_active_menu_items(menu_tree, context['request'], active_items)
+    return {
+        "tree": menu_tree,
+        "active_items": active_items
+    }
 
 
-def allowed_for_user(item, context, **kwargs):
-    """Validate item visibility for current user"""
-    # Current user have no enough group permissions
-    user = context['request'].user
+def has_permissions(item, user, **kwargs):
+    """Check user has permissions for view menu item"""
     if len(item.groups_allowed) > 0:
         if user.is_authenticated():
-            user_groups = set(context['request'].user._cs_group_pks)
+            user_groups = set(user._cs_group_pks)
         else:
             user_groups = set()
-        if not set(user_groups).intersection(item.groups_allowed):
+        if not user_groups.intersection(item.groups_allowed):
             return False
     if item.extension.unauthenticated and user.is_authenticated():
         return False
@@ -94,18 +93,17 @@ def children(parent_id, items):
     return cs
 
 
-def handle_selected(menu, request):
-    class_selected = ' current'
+def find_active_menu_items(menu, request, active_items):
     for item in menu:
         if item.children:
-            selected_item = handle_selected(item.children, request)
-            if selected_item is not None:
-                if class_selected not in item.extension.classes:
-                    item.extension.classes += class_selected
+            item_selected = find_active_menu_items(item.children, request,
+                                                   active_items)
+            if item_selected:
+                active_items.add(item.id)
         if match_url(item, request.get_full_path()):
-            if class_selected not in item.extension.classes:
-                item.extension.classes += class_selected
+            active_items.add(item.id)
             return True
+    return False
 
 
 def match_url(item, current_url):
