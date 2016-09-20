7c13851a81f108afaf45751adc94d97f7d813926
pacahon <pacabest@gmail.com>
2016-09-20 11:30:25 +0300

    replace buggy yandexwebdav with webdavclient

--- cscsite/slides/yandex_disk.py
+++ cscsite/slides/yandex_disk.py
@@ -2,57 +2,56 @@
 
 import logging
 import posixpath
+import webdav.client as wc
 
-import yandexwebdav
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+
+logger = logging.getLogger(__name__)
 
 REQUIRED_SETTINGS = [
     "YANDEX_DISK_USERNAME",
     "YANDEX_DISK_PASSWORD",
     "YANDEX_DISK_SLIDES_ROOT"
 ]
-
 for attr in REQUIRED_SETTINGS:
     if not hasattr(settings, attr):
         raise ImproperlyConfigured(
             "Please add {0!r} to your settings module".format(attr))
 
 
-logger = logging.getLogger(__name__)
-
-
-def mkdirs(config, path):
+def mkdirs(client, path):
+    """Makes all intermediate-level directories before save file"""
     if path == posixpath.sep:
         return
 
-    mkdirs(config, posixpath.dirname(path))
+    mkdirs(client, posixpath.dirname(path))
     _, ext = posixpath.splitext(path)
     if not ext:
         try:
-            config.list(path)
-        except yandexwebdav.ConnectionException:
-            config.mkdir(path)
+            client.list(path)
+        except wc.RemoteParentNotFound:
+            client.mkdir(path)
 
 
-def upload_slides(handle, path, retries=5):
-    config = yandexwebdav.Config({
-        "user": settings.YANDEX_DISK_USERNAME,
-        "password": settings.YANDEX_DISK_PASSWORD
-    })
+def upload_slides(handle, path, retries=3):
+    options = {
+        'webdav_hostname': "https://webdav.yandex.ru",
+        'webdav_login': settings.YANDEX_DISK_USERNAME,
+        'webdav_password': settings.YANDEX_DISK_PASSWORD
+    }
+    client = wc.Client(options)
 
     path = posixpath.join(settings.YANDEX_DISK_SLIDES_ROOT, path)
 
     exc = None
     for i in range(retries):
         try:
-            mkdirs(config, path)
+            mkdirs(client, path)
 
-            # FIXME(lebedev): check if file exists.
-            config.upload(handle.name, path)
-        except yandexwebdav.ConnectionException as connection_exc:
-            exc = connection_exc
+            client.upload_sync(remote_path=path, local_path=handle.name)
+        except wc.WebDavException as webdav_exc:
+            exc = webdav_exc
         else:
             return
-
     logger.error(exc)
