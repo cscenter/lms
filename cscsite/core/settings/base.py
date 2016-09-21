30580b04c3f39d9546187c31466b939cbe0ca807
pacahon <pacabest@gmail.com>
2016-09-21 18:34:16 +0300

    add download from yandex disk tasks

--- cscsite/core/settings/base.py
+++ cscsite/core/settings/base.py
@@ -211,14 +211,12 @@ RQ_QUEUES = {
         'PORT': 6379,
         'DB': 0,
         'PASSWORD': '***REMOVED***',
-        'DEFAULT_TIMEOUT': 350,
     },
     'high': {
         'HOST': '127.0.0.1',
         'PORT': 6379,
         'DB': 0,
         'PASSWORD': '***REMOVED***',
-        'DEFAULT_TIMEOUT': 500,
     },
     'club': {
         'HOST': '127.0.0.1',
