1435bf2a76e5712d1f753018ab1b422c51e386b0
pacahon <pacabest@gmail.com>
2016-09-30 16:32:16 +0300

    minor improvements

--- cscsite/notifications/__init__.py
+++ cscsite/notifications/__init__.py
@@ -21,6 +21,34 @@ Set `target` only if you have no action object, e.g. report status was updated.
 
 Limitations:
     actor, action_object and target models should have PK as `int`
+
+Existing notifications:
+    # TODO: specify recipients
+    1. Student <actor> added <verb> comment <action_object> on
+        "Report for project" <target>
+        action_object - ReportComment
+        target - Report
+    2. Student <actor> sent <verb> Report <action_object> on project" <target>
+        action_object - Report
+        target - Project
+    3. Was sent notification <action_object> about the beginning of the
+        reporting period <verb> for project <target> to Student <recipient>
+        target - Project
+        action_object - None
+    4. Student <actor> added <verb> comment <action_object> on Assignment <target>
+        action_object - AssignmentComment
+        target - StudentAssignment
+    5. Teacher <actor> added new "Assignment" <action_object> on
+            CourseOffering <target>
+            action_object - Assignment
+            target - CourseOffering
+    6. Deadline has been changed <verb> on "Assignment" <action_object>
+        for CourseOffering <target>
+
+    7. Teacher <actor> published <verb> news <action_object> on
+            Course Offering <target>
+    8. Student <actor> added <verb> submission <action_object> for
+            Assignment <target>
 """
 import os
 import sys
