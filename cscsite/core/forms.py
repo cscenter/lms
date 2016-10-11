86b0620d0283ab11277c28147cdb6ee50c50cecd
pacahon <pacabest@gmail.com>
2016-10-11 14:06:34 +0300

    hotfix for ubereditor in admin view

--- cscsite/core/forms.py
+++ cscsite/core/forms.py
@@ -2,6 +2,8 @@ import floppyforms.__future__ as forms
 
 
 class Ubereditor(forms.Textarea, object):
+    template_name = 'ubertextarea.html'
+
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("attrs", {})
         kwargs["attrs"].setdefault("class", "ubereditor")
