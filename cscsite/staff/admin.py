0101c44485ea4c05138a2cdba5f4a0c7b54c1c62
pacahon <pacabest@gmail.com>
2016-10-11 14:06:34 +0300

    hotfix for ubereditor in admin view

--- cscsite/staff/admin.py
+++ cscsite/staff/admin.py
@@ -2,10 +2,12 @@ from __future__ import absolute_import, unicode_literals
 
 from django.contrib import admin
 
+from core.admin import UbereditorMixin
 from staff.models import Hint
 
 
-class HintAdmin(admin.ModelAdmin):
+class HintAdmin(UbereditorMixin, admin.ModelAdmin):
     list_display = ['question', 'sort']
 
 admin.site.register(Hint, HintAdmin)
+
