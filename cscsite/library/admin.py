1d126c2cb13d90f57f296fe0deb3aaea26fc452e
pacahon <pacabest@gmail.com>
2016-10-11 15:49:20 +0300

    remove buggy UberEditorMixin

--- cscsite/library/admin.py
+++ cscsite/library/admin.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.contrib import admin
+from django.db import models as db_models
 from django.utils.translation import ugettext_lazy as _
 
-from core.admin import UbereditorMixin, meta, urlize
+from core.admin import meta, urlize
+from core.forms import AdminRichTextAreaWidget
 from .models import Book, Borrow
 
 
@@ -12,13 +14,15 @@ class BorrowInline(admin.TabularInline):
     extra = 1
 
 
-class BookAdmin(UbereditorMixin, admin.ModelAdmin):
+class BookAdmin(admin.ModelAdmin):
     list_display = ["author", "title", "read_by_with_links"]
     list_display_links = ["author", "title"]
     list_filter = ["tags"]
     list_select_related = True
-
     inlines = [BorrowInline]
+    formfield_overrides = {
+        db_models.TextField: {'widget': AdminRichTextAreaWidget},
+    }
 
     @meta(_("read by"), allow_tags=True)
     def read_by_with_links(self, instance):
