397086f82cee085e75114b3f9b2753dba65f66a7
pacahon <pacabest@gmail.com>
2016-10-26 19:06:41 +0300

    cancel attr for project_student model

--- cscsite/learning/projects/apps.py
+++ cscsite/learning/projects/apps.py
@@ -2,9 +2,6 @@ from django.apps import AppConfig
 from django.db.models.signals import post_save, pre_save
 from django.utils.translation import ugettext_lazy as _
 
-from learning.projects.signals import post_save_project, pre_save_project, \
-    post_save_comment, post_save_report, post_save_review
-
 
 class ProjectsConfig(AppConfig):
     name = 'learning.projects'
@@ -13,13 +10,15 @@ class ProjectsConfig(AppConfig):
     REPORT_COMMENT_ATTACHMENT = 2
 
     def ready(self):
-        pre_save.connect(pre_save_project,
+        from learning.projects import signals
+        from learning.projects.signals import post_save_project_student
+        pre_save.connect(signals.pre_save_project,
                          sender=self.get_model('Project'))
-        post_save.connect(post_save_project,
+        post_save.connect(signals.post_save_project,
                           sender=self.get_model('Project'))
-        post_save.connect(post_save_report,
+        post_save.connect(signals.post_save_report,
                           sender=self.get_model('Report'))
-        post_save.connect(post_save_review,
+        post_save.connect(signals.post_save_review,
                           sender=self.get_model('Review'))
-        post_save.connect(post_save_comment,
+        post_save.connect(signals.post_save_comment,
                           sender=self.get_model('ReportComment'))
--- cscsite/learning/projects/signals.py
+++ cscsite/learning/projects/signals.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.apps import apps
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+from learning.projects.models import ProjectStudent, Report, Project
 from learning.settings import PARTICIPANT_GROUPS
 from notifications import types
 from notifications.signals import notify
@@ -24,6 +28,33 @@ def pre_save_project(sender, instance, **kwargs):
             setattr(instance, _UNSAVED_FILE_PRESENTATION,
                     instance.presentation)
             instance.presentation = None
+
+
+@receiver(post_save, sender=ProjectStudent)
+def post_save_project_student(sender, instance, *args, **kwargs):
+    """
+    Auto cancel project if all participants leave it without reporting.
+    All participants should have `unsatisfactory` grade in that case.
+    """
+    project_student = instance
+    project_id = project_student.project_id
+    project_students = (ProjectStudent.objects
+                        .filter(project=project_id)
+                        .select_related("report"))
+    all_left_project = True
+    for ps in project_students:
+        try:
+            # Skip if any participant has report
+            _ = ps.report.pk
+            all_left_project = False
+            break
+        except ObjectDoesNotExist:
+            pass
+        # Or has any positive/neutral grade
+        if ps.final_grade != ProjectStudent.GRADES.unsatisfactory:
+            all_left_project = False
+            break
+    Project.objects.filter(pk=project_id).update(canceled=all_left_project)
 
 
 def post_save_project(sender, instance, created, *args, **kwargs):
