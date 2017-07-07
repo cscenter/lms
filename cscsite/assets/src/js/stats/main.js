26de59cf9de64a42f956d125f1b9bc2b01ad3104
pacahon <pacabest@gmail.com>
2017-07-07 19:21:39 +0300

    fix stats js

--- cscsite/assets/src/js/stats/main.js
+++ cscsite/assets/src/js/stats/main.js
@@ -1,17 +1,18 @@
 import $ from 'jquery';
+import {init as initCoursesStats}  from './learning/main';
+import {init as initAdmissionStats}  from './admission/main';
 // TODO: How explicitly import global variables like window.URLS and json_data?
 
 
 let fn = {
     init: function () {
+        initCoursesStats();
+        initAdmissionStats();
         // TODO: Сейчас есть проблема - все тяжеловесные библиотеки типа d3/c3 будут перетекать в чанки
         // Если и использовать их - то только разобравшись, как засунуть их в родительский модуль.
-        import(/* webpackChunkName: "_stats_courses" */ './learning/main').then(module => {
-            module.init();
-        });
-        import(/* webpackChunkName: "_stats_admission" */ './admission/main').then(module => {
-            module.init();
-        });
+        // import(/* webpackChunkName: "_stats_courses" */ './learning/main').then(module => {
+        //     module.init();
+        // });
     },
 };
 
