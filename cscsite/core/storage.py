01321c2305d4a39dc6b8b567847afd6315b29411
pacahon <pacabest@gmail.com>
2017-07-27 19:59:41 +0300

    move ignore patterns for static files to app config

--- cscsite/core/storage.py
+++ cscsite/core/storage.py
@@ -1,6 +1,12 @@
+from django.contrib.staticfiles.apps import \
+    StaticFilesConfig as _StaticFilesConfig
 from django.contrib.staticfiles.storage import CachedStaticFilesStorage
 
 from pipeline.storage import GZIPMixin
+
+
+class StaticFilesConfig(_StaticFilesConfig):
+    ignore_patterns = ['CVS', '.*', '*~', 'src', '*.map', '_builds']
 
 
 class PipelineCachedGZIPedStorage(GZIPMixin, CachedStaticFilesStorage):
