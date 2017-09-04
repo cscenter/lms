b02ebb1bb6e05fc7855aec2c42ebdebd6c3c56f5
pacahon <pacabest@gmail.com>
2017-09-04 17:15:09 +0300

    fix stale notifications management cmd

--- cscsite/core/management/commands/read_stale_notifications.py
+++ cscsite/core/management/commands/read_stale_notifications.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, unicode_literals
-
 from django.core.management.base import BaseCommand
 
 from learning.models import AssignmentNotification, \
@@ -9,12 +7,12 @@ from learning.models import AssignmentNotification, \
 
 
 class Command(BaseCommand):
-    help = "Delete notifications from past terms"
+    help = "Soft delete for notifications from past terms"
 
     def handle(self, *args, **options):
         current_term = Semester.get_current()
         AssignmentNotification.objects.filter(
-            created__lt=current_term.starts_at).delete()
+            created__lt=current_term.starts_at, is_unread=True).update(is_unread=False)
         CourseOfferingNewsNotification.objects.filter(
-            created__lt=current_term.starts_at).delete()
+            created__lt=current_term.starts_at, is_unread=True).update(is_unread=False)
         print("done")
