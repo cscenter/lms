dfd9bee380d6c793c8cbde80deac273c1822af82
pacahon <pacabest@gmail.com>
2017-09-08 12:40:37 +0300

    send restore password with redis queue

--- cscsite/csclub/tasks.py
+++ cscsite/csclub/tasks.py
@@ -3,8 +3,8 @@ from django.utils import translation
 
 
 def send_activation_email(site_id, registration_profile_id, language_code):
-    from registration.models import RegistrationProfile
     Site = apps.get_model('sites', 'Site')
+    RegistrationProfile = apps.get_model('registration', 'RegistrationProfile')
     site = Site.objects.get(pk=site_id)
     translation.activate(language_code)
     reg_profile = RegistrationProfile.objects.get(pk=registration_profile_id)
