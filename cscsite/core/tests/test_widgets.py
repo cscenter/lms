61a707f1326ad2af07f039832fa7738f8cdfb743
pacahon <pacabest@gmail.com>
2018-01-15 14:31:08 +0300

    test for prev widget hotfix

---
+++ cscsite/core/tests/test_widgets.py
@@ -0,0 +1,13 @@
+import pytest
+
+from bs4 import BeautifulSoup
+from core.widgets import AdminRichTextAreaWidget
+
+
+@pytest.mark.django_db
+def test_admin_ubertextarea():
+    widget = AdminRichTextAreaWidget()
+    rendered_widget = widget.render(name="test", value="widget_value")
+    widget_dom = BeautifulSoup(rendered_widget, "html.parser").find('textarea')
+    assert widget_dom.get('name') == "test"
+    assert widget_dom.text == "widget_value"
