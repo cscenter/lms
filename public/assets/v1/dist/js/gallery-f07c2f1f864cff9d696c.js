993cc94c33b196d4508e3912d1ed4f628d4f0036
pacahon <pacabest@gmail.com>
2018-02-07 15:33:07 +0300

    update js dists

---
+++ public/assets/v1/dist/js/gallery-f07c2f1f864cff9d696c.js
@@ -0,0 +1,1 @@
+webpackJsonp([3],{QaOo:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={launch:function(){$(".gallery a").magnificPopup({type:"image",image:{titleSrc:function(e){return e.el.attr("title")}},gallery:{enabled:!0,tCounter:'<span class="mfp-counter">%curr% из %total%</span>'},disableOn:400,key:"teacher-gallery"})}};t.default=l}});
