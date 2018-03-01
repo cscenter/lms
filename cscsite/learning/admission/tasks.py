294de5ba2b0a206c7993715116988f081ab4bf64
pacahon <pacabest@gmail.com>
2018-03-01 18:00:01 +0300

    improved application form task runner

--- cscsite/learning/admission/tasks.py
+++ cscsite/learning/admission/tasks.py
@@ -1,13 +1,12 @@
 import logging
 
-import requests
 from django.apps import apps
 from django.utils import translation
 from django_rq import job
 from post_office import mail
 
-from core.api.yandex_contest import CONTEST_PARTICIPANTS_URL, \
-    YandexContestAPIException, YandexContestAPI
+from core.api.yandex_contest import YandexContestAPIException, YandexContestAPI, \
+    RegisterStatus
 
 logger = logging.getLogger(__name__)
 
@@ -17,16 +16,18 @@ def register_in_yandex_contest(applicant_id, language_code):
     """Register user in Yandex.Contest, then send email with summary"""
     translation.activate(language_code)
     Applicant = apps.get_model('admission', 'Applicant')
-    Contest = apps.get_model('admission', 'Contest')
     applicant = (Applicant.objects
                  .filter(pk=applicant_id)
                  .select_related("campaign", "campaign__city")
                  .first())
     if not applicant.yandex_id:
         logger.error(f"Empty yandex login for applicant id = {applicant_id}")
+        raise AttributeError("Empty yandex id")
     if not applicant.contest_id:
-        logger.error(f"Empty contest id for applicant id = {applicant_id}")
-        # FIXME: Get attempt to set contest id for user. If still fail - raise an Error
+        applicant.refresh_contest_id()
+        if not applicant.contest_id:
+            logger.error(f"Empty contest id for applicant id = {applicant_id}")
+            raise AttributeError("Empty contest id")
     campaign = applicant.campaign
     api = YandexContestAPI(access_token=campaign.access_token,
                            refresh_token=campaign.refresh_token)
@@ -34,13 +35,16 @@ def register_in_yandex_contest(applicant_id, language_code):
         status_code, data = api.register_in_contest(applicant.yandex_id,
                                                     applicant.contest_id)
     except YandexContestAPIException as e:
-        # TODO: send message to admin if token is wrong
-        logger.error("Yandex.Contest request error")
-        return
+        error_status_code, text = e.args
+        if error_status_code == RegisterStatus.BAD_TOKEN:
+            # TODO: send message to admin if token is wrong
+            pass
+        logger.error(f"Yandex.Contest api request error [id = {applicant_id}]")
+        raise
 
     # Generate notification
     update_fields = {"status_code": status_code}
-    if status_code == 201:
+    if status_code == RegisterStatus.CREATED:
         participant_id = data
         update_fields["participant_id"] = participant_id
     else:  # 409 - already registered for this contest
@@ -49,6 +53,7 @@ def register_in_yandex_contest(applicant_id, language_code):
     (Applicant.objects
      .filter(pk=applicant_id)
      .update(**update_fields))
+
     mail.send(
         [applicant.email],
         sender='CS центр <info@compscicenter.ru>',
