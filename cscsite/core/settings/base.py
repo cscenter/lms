fd395ee3ba8531a203054380ddc9234d1b4cee6e
pacahon <pacabest@gmail.com>
2018-03-05 19:35:14 +0300

    separated TEMPLATES settings for club and center sites

--- cscsite/core/settings/base.py
+++ cscsite/core/settings/base.py
@@ -37,38 +37,6 @@ CLUB_DOMAIN = 'compsciclub.ru'
 CENTER_SITE_ID = 1
 CLUB_SITE_ID = 2
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': False,
-        'DIRS': [
-            str(PROJECT_DIR / "templates"),
-            django.__path__[0] + '/forms/templates',
-        ],
-        'OPTIONS': {
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-            ],
-            'context_processors': (
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request',
-
-                'learning.context_processors.redirect_bases'
-            ),
-            'debug': DEBUG
-        }
-    },
-]
-
-FORM_RENDERER = 'django.forms.renderers.DjangoTemplates'
-
 INSTALLED_APPS = [
     'modeltranslation',  # insert before admin
     'dal',
