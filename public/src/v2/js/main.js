1df3a9daca358ab201ab657b0b97d7a4abd57e48
pacahon <pacabest@gmail.com>
2018-04-20 14:48:02 +0300

    [backport] added animation on v2 index page to `Show programs` button

--- public/src/v2/js/main.js
+++ public/src/v2/js/main.js
@@ -1,3 +1,19 @@
-import 'jquery';
+import $ from 'jquery';
 import 'bootstrap/js/src/collapse';
 import 'bootstrap/js/src/dropdown';
+
+$(function () {
+    // Click on `Show programs'
+    $('a[href="#offline-courses"]').click(function (e) {
+        e.preventDefault();
+        let scrollTo = $(this).attr('href');
+        // Adjustment for top navbar height on small screens
+        let offset = parseInt($('.cover').css('padding-top'), 10);
+        if (offset > 0) {
+            offset = $('.navbar-container').outerHeight(true);
+        }
+        $('html, body').animate({
+            scrollTop: $(scrollTo).offset().top - offset
+        }, 700);
+    });
+});
