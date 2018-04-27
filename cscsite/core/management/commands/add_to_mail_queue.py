7707b0fb1ce25a4daca9d67aedb6a9fb4bdaff34
pacahon <pacabest@gmail.com>
2018-04-27 18:56:24 +0300

    updated `add_to_mail_queue` command

--- cscsite/core/management/commands/add_to_mail_queue.py
+++ cscsite/core/management/commands/add_to_mail_queue.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import csv
+
 import tablib
 from django.core.management.base import BaseCommand, CommandError
 from post_office import mail
@@ -11,46 +13,60 @@ from post_office.utils import get_email_template
 
 class Command(BaseCommand):
     help = """
-    Generate emails from csv and add them to django post_office mailing queue.
-    Sender is info@compscicenter.ru
+    Generates emails from csv and add them to `post_office` mailing queue.
 
     Provide `email` column header in csv. 
     Other columns will be added to email context.
     """
 
     def add_arguments(self, parser):
-        parser.add_argument('src', metavar='SRC',
+        parser.add_argument('src', metavar='PATH_TO_CSV',
                             help='path to file with emails')
         parser.add_argument(
-            '--template', type=str,
+            '--template', type=str, metavar='NAME',
             help='Post office email template name')
+        parser.add_argument(
+            '--from', type=str,
+            default='noreply@compscicenter.ru',
+            help='`From` header, e.g. `CS центр <info@compscicenter.ru>`')
 
     def handle(self, *args, **options):
         file_path = options["src"]
-        if not options['template']:
-            raise CommandError("Provide template name")
+        header_from = options["from"]
         template_name = options['template']
-        try:
-            # Use post office method for caching purpose
-            template = get_email_template(template_name)
-        except EmailTemplate.DoesNotExist:
-            raise CommandError("Email template {} "
-                               "not found".format(template_name))
+        if template_name:
+            try:
+                template = get_email_template(template_name)
+            except EmailTemplate.DoesNotExist:
+                raise CommandError(f"Email template {template_name} not found")
 
         with open(file_path, "r") as f:
-            data = tablib.Dataset().load(f.read(), format='csv')
-            if "email" not in data.headers:
+            reader = csv.DictReader(f)
+            if "email" not in reader.fieldnames:
                 raise CommandError("Add `email` header")
+            if not template_name and "template_name" not in reader.fieldnames:
+                raise CommandError("Provide template name in csv headers or "
+                                   "with command argument")
+
             generated = 0
-            for row in data.dict:
-                recipients = row["email"]  # Or provide list
-                if not Email.objects.filter(to=recipients,
+            for row in reader:
+                row = {k: row[k].strip() for k in row}
+                if "template_name" in row:
+                    template_name = row["template_name"]
+                    try:
+                        template = get_email_template(template_name)
+                    except EmailTemplate.DoesNotExist:
+                        msg = f"Template {template_name} not found. Skip row."
+                        self.stdout.write(msg)
+
+                recipient = row["email"]
+                if not Email.objects.filter(to=recipient,
                                             template=template).exists():
-                    context = row.copy()
-                    del context['email']
+                    context = {k: row[k] for k in row
+                               if k != "email" and k != "template_name"}
                     mail.send(
-                        recipients,
-                        sender='info@compscicenter.ru',
+                        recipient,
+                        sender=header_from,
                         template=template,
                         context=context,
                         # Render on delivery, we have no really big amount of
