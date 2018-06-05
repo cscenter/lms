a7b5074f12e9af8d6eb6f11ff3ae12ae64a3c10d
pacahon <pacabest@gmail.com>
2018-06-05 12:54:21 +0300

    fixed js on assignment form

--- public/src/v1/js/teaching/index.js
+++ public/src/v1/js/teaching/index.js
@@ -1,4 +1,5 @@
 import {showComponentError, getSections} from 'utils';
+import {TIMEPICKER_ICONS, TIMEPICKER_TOOLTIPS} from "../conf";
 
 $(document).ready(function () {
     let sections = getSections();
@@ -14,6 +15,45 @@ $(document).ready(function () {
             .then(m => {
                 const component = m.default;
                 component.launch();
+            })
+            .catch(error => showComponentError(error));
+    } else if (sections.includes("assignmentForm")) {
+        $('[data-toggle="tooltip"]').tooltip();
+        import('forms')
+            .then(_ => {
+                $('.datepicker').datetimepicker({
+                    locale: 'ru',
+                    format: 'DD.MM.YYYY',
+                    stepping: 5,
+                    allowInputToggle: true,
+                    toolbarPlacement: "bottom",
+                    keyBinds: {
+                        left: false,
+                        right: false,
+                        escape: function () {
+                            this.hide();
+                        },
+                    },
+                    icons: TIMEPICKER_ICONS,
+                    tooltips: TIMEPICKER_TOOLTIPS
+                });
+
+                $('#timepicker').datetimepicker({
+                    locale: 'ru',
+                    format: 'HH:mm',
+                    stepping: 1,
+                    useCurrent: false,
+                    allowInputToggle: true,
+                    icons: TIMEPICKER_ICONS,
+                    tooltips: TIMEPICKER_TOOLTIPS,
+                    defaultDate: new Date("01/01/1980 23:59"),
+                    keyBinds: {
+                        left: false,
+                        right: false,
+                        up: false,
+                        down: false
+                    }
+                });
             })
             .catch(error => showComponentError(error));
     }
