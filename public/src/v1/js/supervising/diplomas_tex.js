9d6f230f2ff1955a82f6a4c650f4a604f9dc2826
pacahon <pacabest@gmail.com>
2018-06-06 12:17:06 +0300

    [backport] replaced zeroclipboardjs

--- public/src/v1/js/supervising/diplomas_tex.js
+++ public/src/v1/js/supervising/diplomas_tex.js
@@ -1,35 +1,23 @@
-import ZeroClipboard from 'zeroclipboard';
+import ClipboardJS from 'clipboard';
 
 if (document.getElementById('diplomas-code') !== null) {
     // Highlight TeX code
-    $('pre code').each(function(i, block) {
+    $('pre code').each(function (i, block) {
         hljs.highlightBlock(block);
     });
 
-    if (ZeroClipboard.isFlashUnusable()) {
-        $('#diplomas-code .btn-clipboard').html('Enable Flash to activate <b>copy</b> feature');
-    }
-
-    ZeroClipboard.config({
-      swfPath: require('zeroclipboard/dist/ZeroClipboard.swf')
-    });
-
-    // Copy to clipboard
-    var client = new ZeroClipboard( $(".btn-clipboard") );
-    client.on({
-        copy: function(e) {
-            var text = $(e.target).next('code').text();
-            client.setText(text);
-            $(e.target).text('Copied!');
-        },
-        aftercopy: function(e) {
-            setTimeout(function() { $(e.target).text("Copy")}, 700);
-        },
-        complete: function(client, args) {
-        },
-        error: function(e) {
-            ZeroClipboard.destroy();
+    let clipboard = new ClipboardJS('.btn-clipboard', {
+        target: function (trigger) {
+            return trigger.nextElementSibling;
         }
     });
 
+    clipboard.on('success', function(e) {
+        $(e.trigger).text('Copied!');
+        setTimeout(function () {
+            $(e.trigger).text("Copy");
+        }, 700);
+
+        e.clearSelection();
+    });
 }
