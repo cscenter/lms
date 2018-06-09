12f76fb989c8247af110d18a830cb99d140cc745
pacahon <pacabest@gmail.com>
2018-06-09 17:51:16 +0300

    Squashed 'public/' changes from 263a75d2..eea22871
    eea22871 alumni 2018 page fixes
    a3b02f71 [backport] honorBoard.js component
    
    git-subtree-dir: public
    git-subtree-split: eea22871789115468271a8e22d246bb3ce39c5ff

---
+++ src/v2/js/sections/honorBoard.js
@@ -0,0 +1,38 @@
+import 'bootstrap/js/src/tooltip';
+import $ from 'jquery';
+
+export function launch() {
+    let achievementGrid = window.achievementGrid;
+    if (achievementGrid !== "undefined") {
+        let studentAchievements = {};
+        Object.keys(achievementGrid).forEach((code) => {
+            let students = achievementGrid[code];
+            students.forEach((userId) => {
+                studentAchievements[userId] = studentAchievements[userId] || [];
+                studentAchievements[userId].push(code);
+            })
+        });
+        Object.keys(studentAchievements).forEach((userId) => {
+            let codes = studentAchievements[userId];
+            let wrapper = document.createElement("div");
+            wrapper.className = "achievements";
+            codes.forEach((code) => {
+                let div = document.createElement('div');
+                div.className = "achievements__item";
+                div.setAttribute("data-toggle", "tooltip");
+                div.setAttribute("title", window.ACHIEVEMENTS[code]);
+                div.innerHTML = `<svg class="sprite-img _${code}" xmlns:xlink="http://www.w3.org/1999/xlink"><use xlink:href="#${code}"></use></svg>`;
+                wrapper.appendChild(div);
+            });
+            $(`#user-card-${userId} .user-card__photo`)[0].appendChild(wrapper);
+        });
+        $('[data-toggle="tooltip"]').tooltip({
+            animation: false,
+            placement: "auto",
+            delay: {"show": 100, "hide": 0}
+        }).click(function (e) {
+            e.preventDefault();
+        });
+
+    }
+};
