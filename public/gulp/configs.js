82fdaffebe77a53a29f8870f497fad4bab1a7d99
pacahon <pacabest@gmail.com>
2018-08-07 15:03:07 +0300

    [backport] store gulp dependencies in gulp/ dir

--- public/gulp/configs.js
+++ public/gulp/configs.js
@@ -8,31 +8,36 @@ export const rootDir = path.dirname(__dirname);
 
 export const paths = {
     build: {
-        css: `assets/${staticVersion}/dist/css/`,
+        css: path.join(rootDir, `assets/${staticVersion}/dist/css/`),
         img: `assets/${staticVersion}/dist/img/`,
         svgSprites: `assets/${staticVersion}/dist/img/sprites/`,
         svgSpritesSCSS: `src/${staticVersion}/scss/sprites/`,
     },
     src: {
-        scss: `src/${staticVersion}/scss/`,
-        img: `src/${staticVersion}/img/`,
-        svgSprites: `src/${staticVersion}/img/sprites/svg/`,
+        scss: path.join(rootDir, `src/${staticVersion}/scss/`),
+        img: path.join(rootDir, `src/${staticVersion}/img/`),
+        svgSprites: path.join(rootDir, `src/${staticVersion}/img/sprites/svg/`),
         // js managed by webpack, ok
     },
     watch: {
-        jinja2: `templates/${staticVersion}/**/*.jinja2`,
-        scss: `src/${staticVersion}/scss/**/*.scss`,
-        img: `src/${staticVersion}/img/**/*.*`,
-        fonts: `src/${staticVersion}/fonts/**/*.*`
+        jinja2: path.join(rootDir, `templates/${staticVersion}/**/*.jinja2`),
+        scss: path.join(rootDir, `src/${staticVersion}/scss/**/*.scss`),
+        img: path.join(rootDir, `src/${staticVersion}/img/**/*.*`),
+        fonts: path.join(rootDir, `src/${staticVersion}/fonts/**/*.*`)
     },
     clean: {
-        css: `./assets/${staticVersion}/dist/**/*.css`
+        css: path.join(rootDir, `assets/${staticVersion}/dist/css/**/*.css`),
+        svgSprites: path.join(rootDir, `assets/${staticVersion}/dist/img/sprites/`),
     }
 };
 
+
 export const sassConfig = {
     outputStyle: 'compressed',
-    includePaths: ['./node_modules/']
+    includePaths: [
+        './node_modules/',
+        '../node_modules/',
+    ]
 };
 
 
@@ -95,4 +100,4 @@ export const browserSyncConfig = {
 };
 
 // Cleanup configuration
-export const delConfig = [paths.clean.css, paths.build.svgSprites];
+export const delConfig = [paths.clean.css, paths.clean.svgSprites];
--- public/src/v2/js/.babelrc.js
+++ public/src/v2/js/.babelrc.js
@@ -2,14 +2,9 @@ var env = process.env.BABEL_ENV || process.env.NODE_ENV;
 var plugins = [
     "@babel/plugin-proposal-object-rest-spread",
     "@babel/syntax-object-rest-spread",
-    "transform-es2015-spread",
+    // FIXME: `transform-class-properties` included with preset-stage-2, but without spec=True. How to deal with it?
     // Allow class constants
-    [
-        "transform-class-properties",
-        {
-            "spec": true
-        }
-    ]
+    // ["transform-class-properties", {"spec": true}]
 ];
 if (env === 'production') {
     plugins.push.apply(plugins, [
@@ -22,21 +17,45 @@ if (env === 'production') {
     ])
 }
 
+if (env === 'test') {
+    plugins.push.apply(plugins, [
+        "transform-es2015-modules-commonjs",
+        "dynamic-import-node"
+    ])
+}
+
+
 module.exports = {
     presets: [
+        [
+            "@babel/preset-env",
+            {
+                modules: false,
+                useBuiltIns: false,
+                debug: true,
+                loose: true,
+                spec: true,
+                targets: {
+                    browsers: [
+                        "last 1 major version",
+                        ">= 1%",
+                        "Chrome >= 45",
+                        "Firefox >= 38",
+                        "Edge >= 12",
+                        "Explorer >= 10",
+                        "iOS >= 9",
+                        "Safari >= 9",
+                        "Android >= 4.4",
+                        "Opera >= 30"
+
+                    ]
+                },
+            }
+        ],
         [
             "@babel/preset-stage-2",
             {
                 "decoratorsLegacy": true
-            }
-        ],
-        [
-            "@babel/preset-env",
-            {
-                // FIXME: specify targets
-                "modules": false,
-                "useBuiltIns": false,
-                "loose": true
             }
         ],
         "@babel/preset-react"
