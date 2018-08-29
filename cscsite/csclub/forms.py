9f42a8ad1bd6580eb2d423d32ef184d65729da38
pacahon <pacabest@gmail.com>
2018-08-29 20:19:27 +0300

    fixed registration form

--- cscsite/csclub/forms.py
+++ cscsite/csclub/forms.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
 from captcha.fields import ReCaptchaField
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django.contrib.auth.models import Group
-from django.forms import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 from registration.forms import RegistrationFormUniqueEmail
@@ -12,21 +14,15 @@ from users.models import CSCUser
 class RegistrationUniqueEmailAndUsernameForm(RegistrationFormUniqueEmail):
     captcha = ReCaptchaField()
 
-    def clean_username(self):
-        """
-        Validate that the supplied username is unique for the
-        site.
-        """
-        if CSCUser.objects.filter(username__iexact=self.cleaned_data['username']):
-            raise ValidationError(_("This username is already in use. Please supply a different username."))
-        return self.cleaned_data['username']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout.append(
+            FormActions(Submit('submit', _('Submit')))
+        )
 
     def save(self, commit=True):
-        user = super(RegistrationUniqueEmailAndUsernameForm, self).save(commit=False)
-        if commit:
-            user.save()
-        # Note: It's ok save user here, even commit=False
-        user.save()
+        user = super().save(commit)
         group = Group.objects.get(pk=CSCUser.group.STUDENT_CLUB)
         user.groups.add(group)
         return user
