2fe783a8a95fa79cdb182fe0528883c72e6d2936
pacahon <pacabest@gmail.com>
2018-10-11 16:51:59 +0300

    fixed GradeField validation

--- cscsite/core/db/models.py
+++ cscsite/core/db/models.py
@@ -6,14 +6,12 @@ from core import forms
 
 # TODO: rename?
 class GradeField(DecimalField):
+    default_validators = [MinValueValidator(0)]
     """Stores positive value with 2 decimal places"""
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("max_digits", 6)
         kwargs.setdefault("decimal_places", 2)
-        validators = kwargs.pop("validators", [])
-        validators.append(MinValueValidator(0))
-        kwargs["validators"] = validators
-        super(GradeField, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def from_db_value(self, value, expression, connection, context):
         if value is None:
