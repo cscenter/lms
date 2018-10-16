b9f0ec707899ae8c04c4e6c5bac18f2017a9ed1e
pacahon <pacabest@gmail.com>
2018-10-16 20:01:48 +0300

    translated survey app

--- cscsite/surveys/views.py
+++ cscsite/surveys/views.py
@@ -1,5 +1,4 @@
 from django.http import Http404
-from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import FormView
