997b461c94ef5f70ac7d5b6e32bfdd9d96784245
pacahon <pacabest@gmail.com>
2018-12-21 16:22:13 +0300

    remove legacy context processor

--- cscsite/learning/context_processors.py
+++
@@ -1,7 +0,0 @@
-from learning import settings
-
-
-def redirect_bases(request):
-    # return any necessary values
-    return {'LEARNING_BASE': settings.LEARNING_BASE,
-            'TEACHING_BASE': settings.TEACHING_BASE}
