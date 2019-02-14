ff98011821c151085d93a088b335313624e60a4a
pacahon <pacabest@gmail.com>
2019-02-14 17:11:46 +0300

    fix bug with semester ordering on new teachers/ page

--- apps/courses/tests/test_api.py
+++ apps/courses/tests/test_api.py
@@ -12,18 +12,19 @@ def test_teachers_list(client):
     t1, t2 = TeacherCenterFactory.create_batch(2)
     term = SemesterFactory.create_current()
     next_term = SemesterFactory.create_next(term)
-    c1, c2 = CourseFactory.create_batch(2, semester=term, teachers=[t1])
-    c2.semester = next_term
-    c2.save()
+    course1, course2 = CourseFactory.create_batch(2, semester=term,
+                                                  teachers=[t1])
+    course2.semester = next_term
+    course2.save()
     t1.roles = CourseTeacher.roles.lecturer
     t1.save()
     response = client.get(url)
     assert response.status_code == 200
     assert len(response.data) == 1
     assert len(response.data[0]["courses"]) == 2
-    assert c1.meta_course_id in response.data[0]["courses"]
-    assert c2.meta_course_id in response.data[0]["courses"]
-    assert response.data[0]["last_session"] == c2.semester.index
+    assert course1.meta_course_id in response.data[0]["courses"]
+    assert course2.meta_course_id in response.data[0]["courses"]
+    assert response.data[0]["last_session"] == course2.semester.index
     assert response.data[0]["city"] == t1.city_id
 
 
