3e469c8e00f786d066ffd4bcd7df54b6c760a239
pacahon <pacabest@gmail.com>
2019-03-06 11:40:51 +0300

    [backport] fix for video cards

--- public/src/v2/js/apps/CourseVideos.jsx
+++ public/src/v2/js/apps/CourseVideos.jsx
@@ -112,7 +112,7 @@ class CourseVideosPage extends React.Component {
                         <div className="row">
                             {filteredItems.map((course) =>
                                 <div key={course.id} className="col-12 col-sm-6 col-lg-4 mb-4">
-                                    <a className="card _shadowed _video"  href={`${course.url}/classes/`}>
+                                    <a className="card _shadowed _video h-100"  href={`${course.url}classes/`}>
                                         <div className="card__content">
                                             <h4 className="card__title">{ course.name }</h4>
                                             <div className="author">
