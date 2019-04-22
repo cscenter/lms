dd36d23665d274f2290fe4d2c0b6f21a429dabd2
pacahon <pacabest@gmail.com>
2019-04-22 11:42:14 +0300

    fix ckeditor route

--- compscicenter_ru/urls.py
+++ compscicenter_ru/urls.py
@@ -69,9 +69,9 @@ urlpatterns += [
 
     path('narnia/', admin.site.urls),
     path('narnia/', include(loginas_urls)),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
     # Required `is_staff` only. Mb restrict to `is_superuser`?
     path('narnia/django-rq/', include('django_rq.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
