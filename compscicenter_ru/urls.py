81ceecec53a33b39f5534dfaa88c343043f922aa
pacahon <pacabest@gmail.com>
2019-05-31 10:21:09 +0300

    redirect /syllabus/ to on-campus

--- compscicenter_ru/urls.py
+++ compscicenter_ru/urls.py
@@ -40,6 +40,7 @@ urlpatterns += [
     re_path(r'^(?P<year>201[3-7])/$', views.AlumniByYearView.as_view(), name='alumni_memory'),
     re_path(r'^(?P<year>20[0-9]{2})/$', views.AlumniHonorBoardView.as_view(), name='alumni_honor'),
     # Programs
+    path('syllabus/', RedirectView.as_view(url='/syllabus/on-campus/', permanent=False)),
     path('syllabus/on-campus/', views.OnCampusProgramsView.as_view(), name='on_campus_programs'),
     path('syllabus/on-campus/<slug:discipline_code>/', views.OnCampusProgramDetailView.as_view(), name='on_campus_program_detail'),
     path('syllabus/distance/', views.DistanceProgramView.as_view(), name='distance_program'),
