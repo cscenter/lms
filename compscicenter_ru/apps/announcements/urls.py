3f4605e6b444a2d9ad8bb69317c21ccc21d3dc75
pacahon <pacabest@gmail.com>
2019-07-05 18:17:32 +0300

    on www.compscicenter.ru replace html_pages with announcements

--- compscicenter_ru/apps/announcements/urls.py
+++ compscicenter_ru/apps/announcements/urls.py
@@ -5,6 +5,6 @@ from . import views as v
 app_name = 'announcements'
 
 urlpatterns = [
-    path("announcements/tags-autocomplete/", v.AnnouncementTagAutocomplete.as_view(), name="tags_autocomplete"),
-    path("announcements/<slug:slug>/", v.AnnouncementDetailView.as_view(), name="announcement_detail"),
+    path("tags-autocomplete/", v.AnnouncementTagAutocomplete.as_view(), name="tags_autocomplete"),
+    path("<slug:slug>/", v.AnnouncementDetailView.as_view(), name="announcement_detail"),
 ]
