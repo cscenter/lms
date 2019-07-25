88811f4621abc72a0c2fb05baf95814fa85e9e1c
pacahon <pacabest@gmail.com>
2019-07-25 14:56:40 +0300

    move club specific apps to club project dir

--- compsciclub_ru/apps/international_schools/admin.py
+++ compsciclub_ru/apps/international_schools/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.db import models as db_models
 
 from core.widgets import AdminRichTextAreaWidget
+from international_schools.models import InternationalSchool
 
 
+@admin.register(InternationalSchool)
 class InternationalSchoolAdmin(admin.ModelAdmin):
     list_display = ['name', 'deadline', 'has_grants']
     formfield_overrides = {
