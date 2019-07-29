8450b146457b9e944f0e13e333562ce7a9c1e050
pacahon <pacabest@gmail.com>
2019-07-29 12:21:00 +0300

    smoke test on menu item permissions attr

--- compscicenter_ru/tests/test_menu.py
+++ compscicenter_ru/tests/test_menu.py
@@ -1,16 +1,20 @@
 import pytest
+from core.tests.utils import TEST_DOMAIN
+from users.models import ExtendedAnonymousUser
+from users.tests.factories import StudentFactory
 
 
 @pytest.mark.django_db
-def test_menu_selected_patterns(rf):
+def test_menu_selected_patterns(rf, mocker):
     from compscicenter_ru.menus import Menu
     from core.menu import MenuItem
+    mocker.patch.dict(Menu.items, clear=True)
     menu_name = 'test_menu'
     menu_items = [
         MenuItem("Item1", '/about/', weight=10,
                  selected_patterns=[r"^/events/"]),
         MenuItem("Item2", '/about2/', weight=20,
-                 selected_patterns=[r"^http://compscicenter.ru/events2/"]),
+                 selected_patterns=[r"^http://{}/events2/".format(TEST_DOMAIN)]),
         MenuItem("Item3", '/about3/', weight=30,
                  selected_patterns=[r"^http://externaldomain.ru/events/"]),
     ]
@@ -18,7 +22,7 @@ def test_menu_selected_patterns(rf):
         Menu.add_item(menu_name, menu_item)
     env = {
         'PATH_INFO': '/events/1/',
-        'SERVER_NAME': 'compscicenter.ru',
+        'SERVER_NAME': TEST_DOMAIN,
         'wsgi.url_scheme': 'http'
     }
     request = rf.request(**env)
@@ -30,11 +34,42 @@ def test_menu_selected_patterns(rf):
     assert processed_menu_item.visible
     assert processed_menu_item.selected
     env['PATH_INFO'] = '/events2/'
-    env['SERVER_NAME'] = 'subdomain.compscicenter.ru'
+    env['SERVER_NAME'] = f'subdomain.{TEST_DOMAIN}'
     request = rf.request(**env)
     processed_menu = Menu.process(request, name=menu_name)
     assert not any(item.selected for item in processed_menu)
-    env['SERVER_NAME'] = 'compscicenter.ru'
+    env['SERVER_NAME'] = TEST_DOMAIN
     request = rf.request(**env)
     processed_menu = Menu.process(request, name=menu_name)
     assert processed_menu[1].selected
+
+
+@pytest.mark.django_db
+def test_menu_permissions(rf, mocker):
+    from compscicenter_ru.menus import Menu
+    from core.menu import MenuItem
+    mocker.patch.dict(Menu.items, clear=True)
+    menu_items = [
+        MenuItem("Item1", '/about/', weight=10),
+        MenuItem("Item2", '/about2/', weight=20,
+                 permissions=["learning.view_study_menu"]),
+    ]
+    menu_name = 'test_menu'
+    for menu_item in menu_items:
+        Menu.add_item(menu_name, menu_item)
+    env = {
+        'PATH_INFO': '/',
+        'SERVER_NAME': TEST_DOMAIN,
+        'wsgi.url_scheme': 'http'
+    }
+    request = rf.request(**env)
+    request.user = ExtendedAnonymousUser()
+    assert request.get_full_path() == '/'
+    processed_menu = Menu.process(request, name=menu_name)
+    assert len(processed_menu) == 1
+    processed_menu_item = processed_menu[0]
+    assert processed_menu_item.weight == 10
+    request.user = StudentFactory()
+    assert request.user.has_perm("learning.view_study_menu")
+    processed_menu = Menu.process(request, name=menu_name)
+    assert len(processed_menu) == 2
