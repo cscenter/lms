07ec9bef20e597f8f69bb677e9869fce9b27baa4
pacahon <pacabest@gmail.com>
2019-08-15 12:23:31 +0300

    move override_perm to auth.utils

--- compsciclub_ru/permissions.py
+++ compsciclub_ru/permissions.py
@@ -1,25 +1,7 @@
 import rules
-from rules import predicate
 
-from auth.permissions import all_permissions
-from auth.registry import role_registry
+from auth.utils import override_perm
 from courses.models import Course
-
-
-def override_perm(name, pred=None):
-    """
-    Override permissions to meet compsciclub.ru requirements:
-        update global permissions rule set
-        update already registered roles which have ref to the old permission
-    """
-    if not all_permissions.rule_exists(name):
-        return
-    if pred is None:
-        pred = predicate(lambda: True, name=name)
-    all_permissions.set_rule(name, pred)
-    for _, role_perms in role_registry.items():
-        if role_perms.rule_exists(name):
-            role_perms.set_rule(name, pred)
 
 
 @rules.predicate
