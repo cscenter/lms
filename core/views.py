3d7768c5b3515a0d0f6e497e4fe784ef50e27f31
pacahon <pacabest@gmail.com>
2019-09-24 12:25:24 +0300

    Squashed 'public/' changes from d60769bbb..0360c926c
    0360c926c minor fix
    382326cbd Merge branch 'master' of github.com:cscenter/site-frontend
    93b9e6c25 fixes for courses page (#311)
    79c27660d fix Alumni.jsx, Teachers.jsx
    dc0a41dd5 Merge branch 'master' of github.com:cscenter/site-frontend
    8f6d85d4a hot-fix (#313)
    989731a89 Merge branch 'master' of github.com:cscenter/site-frontend
    6136dec29 [backport] update learning js
    785198e87 [backport] update student search
    cec034653 updated package.json
    f13a1b3b8 minor update
    fd2315c21 update browserslist
    7a6d0474c new course_list page (#308)
    b5f23655e add modals (#307)
    6964a594b Fix img (#309)
    
    git-subtree-dir: public
    git-subtree-split: 0360c926c6e0ebbae847c602766aed846b1b63f2

--- core/views.py
+++ core/views.py
@@ -21,21 +21,21 @@ class AlumniView(TemplateView):
         app_data = {
             "state": {
                 "area": self.kwargs.get("area", None),
-                "city": self.kwargs.get("city", None),
+                "branch": self.kwargs.get("branch", None),
                 "year": self.kwargs.get("year", {"label": '2019', "value": 2019})
             },
             "props": {
-                "entry_url": '/ajax/alumni.json',
-                "cities": [
+                "entryURL": '/ajax/alumni.json',
+                "branchOptions": [
                     {"label": 'Санкт-Петербург', "value": 'spb'},
                     {"label": 'Новосибирск', "value": 'nsk'}
                 ],
-                "areas": [
+                "areaOptions": [
                     {"label": 'Современная информатика', "value": 'cs'},
                     {"label": 'Разработка ПО', "value": 'se'},
                     {"label": 'Анализ данных', "value": 'ds'},
                 ],
-                "years": [{"label": y, "value": y} for y in reversed(range(2013, 2020))]
+                "yearOptions": [{"label": str(y), "value": y} for y in reversed(range(2013, 2020))]
             }
         }
         return {
@@ -49,17 +49,16 @@ class TeachersView(TemplateView):
     def get_context_data(self, **kwargs):
         app_data = {
             "state": {
-                "query": self.kwargs.get("area", ""),
-                "city": self.kwargs.get("city", None),
+                "branch": self.kwargs.get("branch", None),
             },
             "props": {
-                "entry_url": '/ajax/teachers.json',
-                "courses_url": '/ajax/courses.json',
-                "cities": [
+                "entryURL": '/ajax/teachers.json',
+                "coursesURL": '/ajax/courses.json',
+                "branchOptions": [
                     {"label": 'Санкт-Петербург', "value": 'spb'},
                     {"label": 'Новосибирск', "value": 'nsk'}
                 ],
-                "term_index": 34
+                "termIndex": 34
             }
         }
         return {
