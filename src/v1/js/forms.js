592a5b077372613c68be902f396d926786768a5c
pacahon <pacabest@gmail.com>
2019-10-08 01:13:26 +0300

    Squashed 'public/' changes from 3232d51db..dba1610eb
    dba1610eb all missing backports :<
    
    git-subtree-dir: public
    git-subtree-split: dba1610eba6171b961d2136c3730860adce8f5eb

--- src/v1/js/forms.js
+++ src/v1/js/forms.js
@@ -1,8 +1,6 @@
 import "bootstrap-select/js/bootstrap-select";
 import "bootstrap-select/js/i18n/defaults-ru_RU";
-// FIXME: А это как-то используется сейчас вообще?
 import moment from "moment";
 import "moment/locale/ru";
 import "eonasdan-bootstrap-datetimepicker";
-// TODO: replace with new tempusdominus-bootstrap-3?
 
