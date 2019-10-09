54c811f682063413b3f0ed27d2f369a1597c2ffd
pacahon <pacabest@gmail.com>
2019-10-09 18:56:50 +0300

    [backport] remove jquery from apps/surveys.js

--- public/src/v2/js/apps/surveys.js
+++ public/src/v2/js/apps/surveys.js
@@ -1,5 +1,4 @@
-import $ from 'jquery';
-
+// fixme: remove?
 let Logic = (function createLogicFactory() {
   let logicPrototype = {
     getActionType: function() {
@@ -28,29 +27,29 @@ export function launch() {
                 actions.forEach(function (logic) {
                     if (logic.action_type === "show") {
                         for (let rule of logic.rules) {
-                            const inputSelector = `input[name="field_${rule.field_name}"]`;
-                            $(inputSelector).on("change", function () {
-                                let selected;
-                                if (this.type === 'checkbox') {
-                                    const inputs = document.querySelectorAll(inputSelector);
-                                    selected = Array.from(inputs)
-                                        .filter(i => i.checked)
-                                        .map(i => i.value);
-                                } else {
-                                    selected = [this.value];
-                                }
-                                const expected = new Set(
-                                    rule.value.map(item => item.toString())
-                                );
-                                let intersection = new Set(
-                                    [...selected].filter( i => expected.has(i))
-                                );
-                                if (intersection.size > 0) {
-                                    row.classList.remove("hidden");
-                                } else {
-                                    row.classList.add("hidden");
-                                }
-                            })
+                            document.querySelectorAll(`input[name="field_${rule.field_name}"]`)
+                                .forEach(input => input.addEventListener("change", function () {
+                                    let selected;
+                                    if (this.type === 'checkbox') {
+                                        const inputs = document.querySelectorAll(inputSelector);
+                                        selected = Array.from(inputs)
+                                            .filter(i => i.checked)
+                                            .map(i => i.value);
+                                    } else {
+                                        selected = [this.value];
+                                    }
+                                    const expected = new Set(
+                                        rule.value.map(item => item.toString())
+                                    );
+                                    let intersection = new Set(
+                                        [...selected].filter( i => expected.has(i))
+                                    );
+                                    if (intersection.size > 0) {
+                                        row.classList.remove("hidden");
+                                    } else {
+                                        row.classList.add("hidden");
+                                    }
+                            }));
                         }
                     }
                 })
@@ -58,4 +57,4 @@ export function launch() {
 
         });
     }
-};
+}
