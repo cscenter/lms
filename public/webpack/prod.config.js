0379abc0d82dd4cbddee8d5601951fcda36179f6
pacahon <pacabest@gmail.com>
2019-10-09 15:05:42 +0300

    [backport] test source map uploading to sentry.io

--- public/webpack/prod.config.js
+++ public/webpack/prod.config.js
@@ -1,14 +1,19 @@
 const webpack = require('webpack');
 
 const TerserPlugin = require('terser-webpack-plugin');
+const DeleteSourceMapWebpackPlugin = require('delete-sourcemap-webpack-plugin');
 
 const APP_VERSION = process.env.APP_VERSION || "v1";
 
 // TODO: add css minimization
 module.exports = {
     mode: "production",
+
+    devtool: "hidden-source-map",
+
     output: {
         filename: '[name]-[chunkhash].js',
+        sourceMapFilename: '[name]-[chunkhash].js.map',
         chunkFilename: '[name]-[chunkhash].js',
         publicPath: `/static/${APP_VERSION}/dist/js/`,
     },
@@ -29,7 +34,7 @@ module.exports = {
         concatenateModules: true,
         minimizer: [
             new TerserPlugin({
-                sourceMap: false, // Must be set to true if using source-maps in production
+                sourceMap: true, // Must be set to true if using source-maps in production
                 terserOptions: {
                     compress: {
                         pure_funcs: [ "console.debug" ]
@@ -47,5 +52,7 @@ module.exports = {
         // Need this plugin for deterministic hashing
         // until this issue is resolved: https://github.com/webpack/webpack/issues/1315
         new webpack.HashedModuleIdsPlugin(),
+        // Delete source maps after uploading to sentry.io
+        new DeleteSourceMapWebpackPlugin()
     ],
 };
