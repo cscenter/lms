627335a24051e7f6f05cfcfc548da14c80d8242f
pacahon <pacabest@gmail.com>
2019-10-10 15:23:04 +0300

    [backport] revert test handler

--- public/src/v2/js/apps/scrollspy.js
+++ public/src/v2/js/apps/scrollspy.js
@@ -6,9 +6,4 @@ export function launch() {
         offset: 220,
         target: '#history-navigation'
     });
-
-    // FIXME: test sentry source mapping!
-    document.getElementById('year2011').addEventListener('click', function () {
-        myUndefinedFunction();
-    })
 }
