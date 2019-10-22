c07f528273133d160ae111a7dc089e272227bdd2
pacahon <pacabest@gmail.com>
2019-10-22 11:22:47 +0300

    [backport] updated jasny fileinput js

--- public/src/v1/js/learning/index.js
+++ public/src/v1/js/learning/index.js
@@ -100,6 +100,7 @@ const fn = {
         $('.jasny.fileinput')
             .on('clear.bs.fileinput', function(event) {
                 $(event.target).find('.fileinput-clear-checkbox').val('on');
+                $(event.target).find('.fileinput-filename').text('Файл не выбран');
             })
             .on('change.bs.fileinput', function(event) {
                 $(event.target).find('.fileinput-clear-checkbox').val('');
