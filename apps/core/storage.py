de28df46a135089aa47e18bd6f0d126dc064f288
pacahon <pacabest@gmail.com>
2020-01-10 14:48:38 +0300

    removed legacy code

--- apps/core/storage.py
+++ apps/core/storage.py
@@ -1,14 +1,6 @@
 from django.contrib.staticfiles.apps import \
     StaticFilesConfig as _StaticFilesConfig
-from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
-
-from static_compress import CompressMixin
 
 
 class StaticFilesConfig(_StaticFilesConfig):
     ignore_patterns = ['CVS', '.*', '*~', 'src', '*.map', '_builds']
-
-
-# FIXME: remove?
-class PipelineCachedGZIPedStorage(CompressMixin, ManifestStaticFilesStorage):
-    pass
