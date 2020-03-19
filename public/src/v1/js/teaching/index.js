ed4ec98941ece553931874c9b23158c1499ed75c
pacahon <pacabest@gmail.com>
2020-03-19 16:31:42 +0300

    [backport] remove legacy

--- public/src/v1/js/teaching/index.js
+++ public/src/v1/js/teaching/index.js
@@ -50,46 +50,5 @@ $(document).ready(function () {
                 return $(helpBlockId).html();
             }
         });
-    } else if (sections.includes("courseClassForm")) {
-        // FIXME: omg, what is this? remove?
-        let ends_at_touched = false;
-        // FIXME: use .on here
-        $("#id_ends_at").focus(function () {
-            ends_at_touched = true;
-        });
-
-        // this is fragile as hell, didn't find a suitable library
-        $("#id_starts_at").change(function () {
-            var DELTA_MINUTES = 80;
-
-            function pad(num, size) {
-                var s = num + "";
-                while (s.length < size) s = "0" + s;
-                return s;
-            }
-
-            if (!ends_at_touched) {
-                const string_time = $(this).val();
-                var matches = string_time.match(
-                    "([0-9]{2})([:\-])([0-9]{2})([:0-9\-]*)");
-                if (matches !== null) {
-                    var hours = parseInt(matches[1]);
-                    var separator = matches[2];
-                    var minutes = parseInt(matches[3]);
-                    var maybe_seconds = matches[4];
-
-                    var raw_new_minutes = minutes + DELTA_MINUTES;
-                    var new_hours = (hours + Math.floor(raw_new_minutes / 60)) % 24;
-                    var new_minutes = raw_new_minutes % 60;
-
-                    $("#id_ends_at").val(pad(new_hours, 2)
-                        + separator
-                        + pad(new_minutes, 2)
-                        + maybe_seconds);
-                } else {
-                    console.warning("Can't parse " + string_time);
-                }
-            }
-        });
     }
 });
