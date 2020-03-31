21adaea4edf722c7ab64b85519dda8fa319a2fef
pacahon <pacabest@gmail.com>
2020-03-31 13:34:10 +0300

    WIP Calendar.week_starts_on support (no tests)

--- apps/courses/views/calendar.py
+++ apps/courses/views/calendar.py
@@ -9,6 +9,8 @@ from courses.calendar import CalendarQueryParams, MonthFullWeeksEventsCalendar, 
 
 __all__ = ('MonthEventsCalendarView', 'WeekEventsView')
 
+from courses.constants import SUNDAY_WEEKDAY
+
 from courses.utils import MonthPeriod
 
 
