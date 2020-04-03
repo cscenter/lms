2b2c2d22b172bf7ac1586fc3fdec67f677a45076
pacahon <pacabest@gmail.com>
2020-04-03 11:42:39 +0300

    minor update

--- apps/study_programs/models.py
+++ apps/study_programs/models.py
@@ -1,5 +1,4 @@
 from django.core.validators import MinValueValidator
-from django.core.validators import MinValueValidator
 from django.db import models, transaction
 from django.db.models import query, Prefetch
 from django.utils.encoding import smart_text
