3ae4d58378de716138bdab5f5aa6584df1d20989
Nicholas Kapralov <4dvlup@gmail.com>
2020-04-16 20:21:28 +0300

    Test index view of compsciclub.ru & Course.is_club_course-dependent functions

--- compsciclub_ru/tests.py
+++ compsciclub_ru/tests.py
@@ -14,6 +14,40 @@ from users.tests.factories import StudentFactory
 
 
 @pytest.mark.django_db
+def test_index_view_course_list(client, settings):
+    """
+    Only courses that were shared with CS Club in the current semester should be shown on the index page
+    """
+    current_semester = SemesterFactory.create_current()
+    previous_semester = SemesterFactory.create_prev(current_semester)
+
+    branch_spb_center = BranchFactory(code=Branches.SPB,
+                                      site__domain=settings.ANOTHER_DOMAIN)
+    branch_spb_club = BranchFactory(code=Branches.SPB,
+                                    site__domain=settings.TEST_DOMAIN)
+
+    course_center_private = CourseFactory(semester=current_semester,
+                                          branch=branch_spb_center)
+    course_center_public = CourseFactory(semester=current_semester,
+                                         branch=branch_spb_center)
+    course_center_outdated = CourseFactory(semester=previous_semester,
+                                         branch=branch_spb_center)
+    course_club_actual = CourseFactory(semester=current_semester)
+    course_club_outdated = CourseFactory(semester=previous_semester)
+
+    # Some of CS Courses were shared with CS Club
+    course_center_public.additional_branches.add(branch_spb_club)
+    course_center_outdated.additional_branches.add(branch_spb_club)
+
+    response = client.get(reverse('index'))
+    assert smart_bytes(course_center_private.meta_course.name) not in response.content
+    assert smart_bytes(course_center_public.meta_course.name) in response.content
+    assert smart_bytes(course_center_outdated.meta_course.name) not in response.content
+    assert smart_bytes(course_club_actual.meta_course.name) in response.content
+    assert smart_bytes(course_club_outdated.meta_course.name) not in response.content
+
+
+@pytest.mark.django_db
 def test_course_list(client, settings):
     """
     Сlub students can see all Club or CS Center courses that have been shared with their branch
