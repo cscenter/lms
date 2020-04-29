43107f276fdb4e9aa0dd6e3b657c062ef7fda6e3
Nicholas Kapralov <4dvlup@gmail.com>
2020-04-29 16:20:32 +0300

    Reworked CourseClassManager:  * Fix test for CourseClassManager.in_branches  * Show club classes regardless of is_open  * Made CourseClass.in_branches return distinct classes & added tests

--- apps/courses/tests/test_managers.py
+++ apps/courses/tests/test_managers.py
@@ -16,7 +16,7 @@ def test_course_manager_available_in_branch():
     assert Course.objects.available_in(branch1.pk).count() == 1
     assert Course.objects.available_in(course.main_branch_id).count() == 1
 
-@pytest.mark.skip("Duplicates in CourseClass.objects.in_branches")
+
 @pytest.mark.django_db
 def test_course_class_manager_in_branches():
     branch_spb = BranchFactory(code=Branches.SPB)
@@ -24,23 +24,40 @@ def test_course_class_manager_in_branches():
     branch_xxx = BranchFactory()
     course = CourseFactory(main_branch=branch_spb,
                            branches=[branch_nsk, branch_xxx])
-    date_on = datetime.date(year=2018, month=1, day=1)
-    starts_at = datetime.time(hour=12, minute=0)
-    cc1 = CourseClassFactory(course=course, date=date_on, starts_at=starts_at)
-    assert CourseClass.objects.in_branches(branch_spb.pk).count() == 1
-    # Make sure no duplicates even if course root branch added as additional
-    CourseBranch(course=course, branch=branch_spb).save()
+    cc1 = CourseClassFactory(course=course)
     assert CourseClass.objects.in_branches(branch_spb.pk).count() == 1
     course2 = CourseFactory(main_branch=branch_nsk)
     assert course2.pk > course.pk
-    # This class is goes earlier than `cc1`
-    cc2 = CourseClassFactory(course=course2,
-                             date=date_on - datetime.timedelta(days=1),
-                             starts_at=starts_at)
+    cc2 = CourseClassFactory(course=course2)
+
+    # Course2 was not shared with SPB branch yet
     assert CourseClass.objects.in_branches(branch_spb.pk).count() == 1
+
+    # Share course2 with SPB branch
     CourseBranch(course=course2, branch=branch_spb).save()
-    classes = list(CourseClass.objects.in_branches(branch_spb.pk))
+    assert CourseClass.objects.in_branches(branch_spb.pk).count() == 2
+
+    # No duplicates even if looking for courses in several branches
+    classes = list(CourseClass.objects.in_branches(branch_spb.pk, branch_nsk.pk))
     assert len(classes) == 2
+
+
+@pytest.mark.django_db
+def test_course_class_manager_sort_order():
+    branch_spb = BranchFactory(code=Branches.SPB)
+    course1 = CourseFactory(main_branch=branch_spb)
+    course2 = CourseFactory(main_branch=branch_spb)
+    date_on = datetime.date(year=2018, month=1, day=1)
+    starts_at = datetime.time(hour=12, minute=0)
+    cc1 = CourseClassFactory(course=course1, date=date_on, starts_at=starts_at)
+    cc2 = CourseClassFactory(course=course2,
+                             date=date_on + datetime.timedelta(days=1),
+                             starts_at=starts_at)
+    assert CourseClass.objects.in_branches(branch_spb.pk).count() == 2
+    classes = list(CourseClass.objects.in_branches(branch_spb.pk))
+    # Course classes are sorted by date DESC, course ASC, starts_at DESC (see CourseClass.Meta)
+    # So cc2 should be the first class in the list
     assert classes[0] == cc2
     assert classes[1] == cc1
+
 
--- apps/courses/tests/test_services.py
+++ apps/courses/tests/test_services.py
@@ -4,7 +4,9 @@ from django.core.exceptions import ValidationError
 from core.tests.factories import BranchFactory
 from courses.models import CourseBranch
 from courses.services import CourseService
-from courses.tests.factories import CourseFactory
+from courses.tests.factories import CourseFactory, CourseClassFactory
+from learning.services import get_teacher_classes, get_classes
+from users.tests.factories import TeacherFactory
 
 
 @pytest.mark.django_db
@@ -40,3 +42,29 @@ def test_get_course_sync_branches_change_main_branch():
     CourseService.sync_branches(course)
     assert CourseBranch.objects.count() == 1
     assert CourseBranch.objects.get() == cb2
+
+
+@pytest.mark.django_db
+def test_get_teacher_classes_should_not_return_duplicate_classes(settings):
+    branch1, branch2 = BranchFactory.create_batch(2)
+    t = TeacherFactory(branch=branch1,
+                       required_groups__site_id=settings.TEST_DOMAIN_ID)
+    course = CourseFactory(teachers=[t],
+                           main_branch=branch1,
+                           branches=[branch1, branch2])
+    assert len(course.branches.all()) == 2
+    cc = CourseClassFactory(course=course)
+    assert len(get_teacher_classes(t)) == 1
+
+
+@pytest.mark.django_db
+def test_get_classes_should_not_return_duplicate_classes():
+    branch1, branch2 = BranchFactory.create_batch(2)
+    course = CourseFactory(main_branch=branch1,
+                           branches=[branch1, branch2])
+    assert len(course.branches.all()) == 2
+    cc = CourseClassFactory(course=course)
+    assert len(get_classes([branch1.pk])) == 1
+    assert len(get_classes([branch2.pk])) == 1
+    print(get_classes([branch1.pk, branch2.pk]).query)
+    assert len(get_classes([branch1.pk, branch2.pk])) == 1
