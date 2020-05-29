c893e49340f8421a51ca6a90741fb750ed441e5c
Nicholas Kapralov <4dvlup@gmail.com>
2020-05-29 14:40:17 +0300

    test(info_blocks): avoid using gettext in test_managers.py

--- apps/info_blocks/tests/test_managers.py
+++ apps/info_blocks/tests/test_managers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from info_blocks.models import CurrentInfoBlockTags, InfoBlock
+from info_blocks.models import InfoBlock
 from info_blocks.tests.factories import InfoBlockFactory
 
 
@@ -17,10 +17,10 @@ def test_info_blocks_manager_for_site(settings):
 
 @pytest.mark.django_db
 def test_info_blocks_manager_with_tag():
-    u1, u2 = InfoBlockFactory.create_batch(2, tags=[CurrentInfoBlockTags.USEFUL])
-    u3 = InfoBlockFactory(tags=[CurrentInfoBlockTags.HONOR_CODE])
+    u1, u2 = InfoBlockFactory.create_batch(2, tags=["Useful"])
+    u3 = InfoBlockFactory(tags=["Honor Code"])
 
-    infoblocks_useful = list(InfoBlock.objects.with_tag(CurrentInfoBlockTags.USEFUL))
+    infoblocks_useful = list(InfoBlock.objects.with_tag("Useful"))
     assert len(infoblocks_useful) == 2
     assert u1 in infoblocks_useful
     assert u2 in infoblocks_useful
