6a41a2aa56db0a605d7b4d64ef5233c889e6ddee
pacahon <pacabest@gmail.com>
2020-06-04 12:47:57 +0300

    refactor(info_blocks): make tag_list sortable in admin, update tag slugs in constants

--- apps/info_blocks/constants.py
+++ apps/info_blocks/constants.py
@@ -3,6 +3,6 @@ class CurrentInfoBlockTags:
     These tag slugs are used to display relevant items in current views for useful, honor code and internships.
     Need to be kept in sync with actual DB values, will be removed in favor of tabs in the new design.
     """
-    USEFUL = "poleznoe"
-    HONOR_CODE = "kodeks-chesti"
-    INTERNSHIP = "proekty-organizatorov"
+    USEFUL = "hint"
+    HONOR_CODE = "honor-code"
+    INTERNSHIP = "internship"
