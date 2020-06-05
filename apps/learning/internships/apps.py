770ddb7bff847421f42bd4c4f4df8817debfbbe6
pacahon <pacabest@gmail.com>
2020-06-05 16:06:26 +0300

    refactor!: remove internships app

--- apps/learning/internships/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class InternshipsConfig(AppConfig):
-    name = 'learning.internships'
-    verbose_name = _("Internships")
