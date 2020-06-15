45cd8c15d1f88757f4c02f40ec45176d4807e2f8
pacahon <pacabest@gmail.com>
2020-06-15 18:50:22 +0300

    build: configure redis port with env var

--- compsciclub_ru/settings/base.py
+++ compsciclub_ru/settings/base.py
@@ -56,16 +56,17 @@ INSTALLED_APPS += [
 
 REDIS_PASSWORD = env.str('REDIS_PASSWORD', default=None)
 REDIS_HOST = env.str('REDIS_HOST', default='127.0.0.1')
+REDIS_PORT = env.int('REDIS_PORT', default=6379)
 RQ_QUEUES = {
     'default': {
         'HOST': REDIS_HOST,
-        'PORT': 6379,
+        'PORT': REDIS_PORT,
         'DB': 0,
         'PASSWORD': REDIS_PASSWORD,
     },
     'high': {
         'HOST': REDIS_HOST,
-        'PORT': 6379,
+        'PORT': REDIS_PORT,
         'DB': 0,
         'PASSWORD': REDIS_PASSWORD,
     },
@@ -77,6 +78,7 @@ THUMBNAIL_DEBUG = DEBUG
 THUMBNAIL_DUMMY = True
 THUMBNAIL_PRESERVE_FORMAT = True
 THUMBNAIL_KVSTORE = 'sorl.thumbnail.kvstores.redis_kvstore.KVStore'
+THUMBNAIL_REDIS_PORT = REDIS_PORT
 THUMBNAIL_REDIS_HOST = REDIS_HOST
 THUMBNAIL_REDIS_PASSWORD = REDIS_PASSWORD
 
