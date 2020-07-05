46db9a768b64beeb93c35645ed283ceb065fd9ac
pacahon <pacabest@gmail.com>
2020-07-05 17:48:51 +0300

    feat: base project settings for ynd site

---
+++ yandexdataschool_ru/settings/base.py
@@ -0,0 +1,26 @@
+# Read project environment into os.environ before importing base configuration
+import environ
+env = environ.Env()
+environ.Env.read_env(env_file=env.str('ENV_FILE', default=None))
+
+from lms.settings.base import *
+
+SITE_ID = 3
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["lk.yandexdataschool.ru"])
+
+WSGI_APPLICATION = 'yandexdataschool_ru.wsgi.application'
+ROOT_URLCONF = 'lms.urls'
+# FIXME: move to the SiteConfiguration model
+LMS_SUBDOMAIN = None
+
+SUBDOMAIN_URLCONFS = {
+    None: ROOT_URLCONF,
+}
+
+INSTALLED_APPS += [
+    'announcements.apps.AnnouncementsConfig',
+    'application.apps.ApplicationConfig',
+    'online_courses.apps.Config',
+    'publications.apps.PublicationsConfig',
+]
