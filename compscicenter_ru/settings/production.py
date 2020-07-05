4c909e2949c932943d1d9aacfae6d7545031f86d
pacahon <pacabest@gmail.com>
2020-07-05 17:46:26 +0300

    refactor: lms base settings

--- compscicenter_ru/settings/production.py
+++ compscicenter_ru/settings/production.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import logging
-
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
