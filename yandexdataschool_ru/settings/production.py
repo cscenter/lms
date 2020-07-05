6a861b4ea94b4c46890f852fabb75e25300d68c4
pacahon <pacabest@gmail.com>
2020-07-05 18:45:55 +0300

    fix: disable sentry on ynd site

--- yandexdataschool_ru/settings/production.py
+++ yandexdataschool_ru/settings/production.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.logging import LoggingIntegration
-
 from .base import *
 
 MEDIA_ROOT = str(Path('/shared', 'media'))
@@ -12,16 +8,6 @@ CDN_SOURCE_STATIC_URL = STATIC_URL
 # STATIC_URL = CDN_STATIC_URL
 # for webpack_config in WEBPACK_LOADER.values():
 #    webpack_config['LOADER_CLASS'] = 'core.webpack_loader.BundleDirectoryWebpackLoader'
-
-# Sentry
-sentry_logging = LoggingIntegration(
-    level=logging.INFO,        # Capture info and above as breadcrumbs
-    event_level=logging.ERROR  # Send errors as events
-)
-sentry_sdk.init(
-    dsn=SENTRY_DSN,
-    integrations=[sentry_logging, DjangoIntegration()]
-)
 
 
 CACHES['default'] = {
