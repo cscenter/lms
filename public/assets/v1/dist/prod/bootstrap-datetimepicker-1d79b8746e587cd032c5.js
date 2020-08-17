e57b69fb8e82e774f38f72d7b122f44dca268eff
pacahon <pacabest@gmail.com>
2020-08-17 14:54:10 +0300

    build: update js v1

--- public/assets/v1/dist/prod/bootstrap-datetimepicker-1d79b8746e587cd032c5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{HUua:function(n,w,o){}}]);
---
+++ public/assets/v1/dist/prod/gallery-dbcd03a36cd162f5ca9d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"hRW/":function(e,n,t){"use strict";function a(){$(".gallery a").magnificPopup({type:"image",image:{titleSrc:function(e){return e.el.attr("title")}},gallery:{enabled:!0,tCounter:'<span class="mfp-counter">%curr% из %total%</span>'},disableOn:400,key:"teacher-gallery"})}t.r(n),t.d(n,"launch",(function(){return a}))}}]);
