24ddd9a052b3e0d5a59fb6fe3d6bca4419e88140
Nicholas Kapralov <4dvlup@gmail.com>
2020-08-21 20:34:30 +0300

    feat(lms): use correct year of establishment in the footer

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -1,8 +1,13 @@
 from django.conf import settings
 
+from core.models import Branch
+
 
 def common_context(request):
+    branches = Branch.objects.for_site(settings.SITE_ID)
+    min_established = min(b.established for b in branches)
     return {
+        "ESTABLISHED": min_established,
         "FAVICON_PATH": getattr(settings, "FAVICON_PATH", ""),
         "LOGO_PATH": getattr(settings, "LOGO_PATH", ""),
     }
