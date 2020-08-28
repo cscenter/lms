76e1139fef31cf0fdcb7ed9637ea46c474d716b9
pacahon <pacabest@gmail.com>
2020-08-28 15:12:44 +0300

    refactor(code_reviews): move upload_attachment_to_gerrit to the tasks module

--- apps/code_reviews/tasks.py
+++ apps/code_reviews/tasks.py
@@ -1,14 +1,18 @@
 import logging
 
 from django.apps import apps
+from django.conf import settings
 from django_rq import job
 
+from code_reviews.api.gerrit import Gerrit
 from code_reviews.api.ldap import init_client
+from code_reviews.gerrit import get_or_create_change, list_change_files
 from code_reviews.ldap import get_ldap_username, user_to_ldap_entry, \
     get_password_hash
+from learning.models import AssignmentComment
 from users.models import User
 
-logger = logging.getLogger('auth')
+logger = logging.getLogger(__name__)
 
 
 @job('high')
@@ -51,3 +55,50 @@ def update_password_in_gerrit(*, user_id: int):
         changed = client.set_password_hash(username, password_hash)
         if not changed:
             logger.error(f"Password hash for user {user_id} wasn't changed")
+
+
+@job('default')
+def upload_attachment_to_gerrit(assignment_comment_id):
+    assignment_comment = (AssignmentComment.objects
+                          .select_related('student_assignment')
+                          .get(pk=assignment_comment_id))
+    student_assignment = assignment_comment.student_assignment
+    attached_file = assignment_comment.attached_file
+
+    client = Gerrit(settings.GERRIT_API_URI,
+                    auth=(settings.GERRIT_CLIENT_USERNAME,
+                          settings.GERRIT_CLIENT_HTTP_PASSWORD))
+
+    change = get_or_create_change(client, student_assignment)
+    if not change:
+        logger.info('Failed to get or create a change')
+        return
+
+    response = client.get_change_edit(change.change_id)
+    if not response.no_content:
+        logger.info('Found previous change edit')
+        response = client.delete_change_edit(change.change_id)
+        if not response.no_content:
+            logger.error('Failed to delete previous change edit')
+
+    # Save extension to enable syntax highlighting in the UI
+    extension = attached_file.name.split('.')[-1]
+    solution_filename = f"solution.{extension}"
+
+    # Delete other existing files
+    change_files = list_change_files(client, change)
+    for file in change_files:
+        if file != solution_filename:
+            client.delete_file(change.change_id, file)
+
+    # Upload new solution as a Change Edit
+    response = client.upload_file(change.change_id, solution_filename,
+                                  attached_file)
+    if not response.no_content:
+        logger.info('Failed to upload the solution')
+
+    # Publish Change Edit with all modifications
+    response = client.publish_change_edit(change.change_id)
+    if not response.no_content:
+        logger.info('Failed to publish change edit')
+        return
