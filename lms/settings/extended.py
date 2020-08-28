aca8628da38fdeefac3741c7cf003d0483e37a84
Nicholas Kapralov <4dvlup@gmail.com>
2020-08-28 13:48:52 +0300

    fix: default value for GERRIT_ROBOT_USERNAME, minor fixes

--- lms/settings/extended.py
+++ lms/settings/extended.py
@@ -59,7 +59,7 @@ LDAP_SYNC_PASSWORD = env.bool('LDAP_SYNC_PASSWORD', default=True)
 GERRIT_API_URI = env.str('GERRIT_API_URI', default="https://review.compscicenter.ru/a/")
 GERRIT_CLIENT_USERNAME = env.str('GERRIT_CLIENT_USERNAME', default="admin")
 GERRIT_CLIENT_HTTP_PASSWORD = env.str('GERRIT_CLIENT_HTTP_PASSWORD')
-GERRIT_ROBOT_USERNAME = env.str('GERRIT_ROBOT_USERNAME')
+GERRIT_ROBOT_USERNAME = env.str('GERRIT_ROBOT_USERNAME', default="gerrit.bot")
 LDAP_TLS_TRUSTED_CA_CERT_FILE = env.str('LDAP_TLS_TRUSTED_CA_CERT_FILE', default=str(ROOT_DIR / "lms" / "LDAPTrustedCA.crt"))
 
 # Registration by invitation link
