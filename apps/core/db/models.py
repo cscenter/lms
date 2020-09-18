655d9857fbda8d4bbcd1dd86a5ae115dafb6d218
pacahon <pacabest@gmail.com>
2020-09-18 18:31:47 +0300

    refactor: update gradebook class

--- apps/core/db/models.py
+++ apps/core/db/models.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from typing import Optional
+from typing import Optional, Union
 
 from django.contrib.postgres.fields import JSONField
 from django.core.validators import MinValueValidator
@@ -8,7 +8,14 @@ from django.db.models import DecimalField
 from core import forms
 
 
-def normalize_score(value: Optional[Decimal]):
+def normalize_score(value: Optional[Decimal]) -> Optional[Union[int, Decimal]]:
+    """
+    This method used for humanizing score value - we want to show `5`
+    instead of `5.00` when it's possible.
+
+    When decimal is provided cast it to integer. If the result is exact
+    as decimal value returns integer instead of the original decimal.
+    """
     if value is None:
         return value
     decimal_as_int = value.to_integral_value()
