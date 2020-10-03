f19f1058eda632771448de200f4d80dc32e63816
pacahon <pacabest@gmail.com>
2020-10-03 16:59:47 +0300

    feat(admin): update submission admin views

--- apps/contests/admin.py
+++ apps/contests/admin.py
@@ -1,18 +1,33 @@
 from django.contrib import admin
-
+from django.utils.translation import gettext_lazy as _
 from contests.models import CheckingSystem, Submission, Checker
+from core.admin import meta
 
 
 class CheckingSystemAdmin(admin.ModelAdmin):
-    model = CheckingSystem
+    pass
 
 
 class CheckerAdmin(admin.ModelAdmin):
-    model = Checker
+    pass
 
 
 class SubmissionAdmin(admin.ModelAdmin):
-    model = Submission
+    list_select_related = [
+        'assignment_submission__student_assignment__assignment',
+        'assignment_submission__student_assignment__student'
+    ]
+    search_fields = ['assignment_submission__student_assignment__assignment__title']
+    list_display = ['id', 'status', 'assignment_name', 'student_name']
+    raw_id_fields = ['assignment_submission']
+
+    @meta(_("Assignment"))
+    def assignment_name(self, obj):
+        return obj.assignment_submission.student_assignment.assignment.title
+
+    @meta(_("Student"))
+    def student_name(self, obj):
+        return obj.assignment_submission.student_assignment.student
 
 
 admin.site.register(CheckingSystem, CheckingSystemAdmin)
