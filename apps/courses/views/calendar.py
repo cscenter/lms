3f700f30fa68aab4a8c923fb27540c3ed15508cb
pacahon <pacabest@gmail.com>
2021-03-02 22:00:18 +0300

    refactor: simplify calendar month events base views

--- apps/courses/views/calendar.py
+++ apps/courses/views/calendar.py
@@ -17,27 +17,23 @@ class MonthEventsCalendarView(generic.TemplateView):
     template_name = "lms/courses/calendar.html"
 
     def get(self, request, *args, **kwargs):
-        """Validates GET-parameters, set defaults if no values provided."""
+        # Get month period from GET-parameters or current month by default
         query_params = CalendarQueryParams(data=request.GET)
         if not query_params.is_valid():
             return HttpResponseRedirect(request.path)
-        today_local = now_local(request.user.time_zone).date()
-        # FIXME: validate in a MonthPeriod instead?
+        today_local = now_local(request.user.time_zone)
         year = query_params.validated_data.get('year', today_local.year)
         month = query_params.validated_data.get('month', today_local.month)
         month_period = MonthPeriod(year, month)
-        context = self.get_context_data(month_period, today_local, **kwargs)
-        return self.render_to_response(context)
 
-    def get_context_data(self, month_period, today, **kwargs):
         events = self.get_events(month_period)
         calendar = MonthFullWeeksEventsCalendar(month_period, events)
         context = {
-            "today": today,
+            "today": today_local.date(),
             "calendar_type": self.calendar_type,
             "calendar": calendar
         }
-        return context
+        return self.render_to_response(context)
 
     def get_events(self, month_period: MonthPeriod, **kwargs) -> Iterable:
         raise NotImplementedError()
@@ -53,16 +49,12 @@ class WeekEventsView(generic.TemplateView):
         today_iso_year, today_iso_week, _ = today_local.isocalendar()
         iso_year = query_params.validated_data.get('year', today_iso_year)
         iso_week = query_params.validated_data.get('week', today_iso_week)
-        context = self.get_context_data(iso_year, iso_week, **kwargs)
-        return self.render_to_response(context)
-
-    def get_context_data(self, iso_year, iso_week, **kwargs):
         events = self.get_events(iso_year, iso_week)
         calendar = WeekEventsCalendar(iso_year, iso_week, events)
         context = {
             "calendar": calendar
         }
-        return context
+        return self.render_to_response(context)
 
     def get_events(self, iso_year, iso_week) -> Iterable[CalendarEventW]:
         raise NotImplementedError()
