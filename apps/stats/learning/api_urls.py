874326fbd79e4eca20218ec7fc9fbc4610c2340a
pacahon <pacabest@gmail.com>
2021-03-19 13:53:15 +0300

    style: reformat urls

--- apps/stats/learning/api_urls.py
+++ apps/stats/learning/api_urls.py
@@ -4,16 +4,8 @@ from .views import CourseParticipantsStatsByYear, AssignmentsStats, \
     EnrollmentsStats, CourseParticipantsStatsByGroup
 
 urlpatterns = [
-    re_path(r'^participants/(?P<course_id>\d+)/groups/$',
-            CourseParticipantsStatsByGroup.as_view(),
-            name='stats_learning_participants_group'),
-    re_path(r'^participants/(?P<course_id>\d+)/year/$',
-            CourseParticipantsStatsByYear.as_view(),
-            name='stats_learning_participants_year'),
-    re_path(r'^assignments/(?P<course_id>\d+)/$',
-            AssignmentsStats.as_view({'get': 'list'}),
-            name='stats_assignments'),
-    re_path(r'^enrollments/(?P<course_id>\d+)/$',
-            EnrollmentsStats.as_view(),
-            name='stats_enrollments'),
+    re_path(r'^participants/(?P<course_id>\d+)/groups/$', CourseParticipantsStatsByGroup.as_view(), name='stats_learning_participants_group'),
+    re_path(r'^participants/(?P<course_id>\d+)/year/$', CourseParticipantsStatsByYear.as_view(), name='stats_learning_participants_year'),
+    re_path(r'^assignments/(?P<course_id>\d+)/$', AssignmentsStats.as_view({'get': 'list'}), name='stats_assignments'),
+    re_path(r'^enrollments/(?P<course_id>\d+)/$', EnrollmentsStats.as_view(), name='stats_enrollments'),
 ]
