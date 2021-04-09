0e0cd6ecef70a804cb37ab1f9d7ba387c98a916f
pacahon <pacabest@gmail.com>
2021-04-09 11:46:09 +0300

    feat: support common template name for stage1_time_to_start_test

--- apps/admission/management/commands/stage1_time_to_start_test.py
+++ apps/admission/management/commands/stage1_time_to_start_test.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand, CommandError
 from post_office import mail
+from post_office.models import Email
 from post_office.utils import get_email_template
 
 from ._utils import CurrentCampaignMixin, EmailTemplateMixin
@@ -18,8 +20,7 @@ class Command(EmailTemplateMixin, CurrentCampaignMixin, BaseCommand):
         super().add_arguments(parser)
         parser.add_argument(
             '--template', type=str,
-            default=self.TEMPLATE_TYPE,
-            help='Override default post office email template prefix')
+            help='Post Office template name common for all campaigns')
 
     def handle(self, *args, **options):
         campaigns = self.get_current_campaigns(options)
@@ -27,34 +28,43 @@ class Command(EmailTemplateMixin, CurrentCampaignMixin, BaseCommand):
             self.stdout.write("Canceled")
             return
 
-        template_type = options['template']
-        self.validate_templates(campaigns, types=[template_type],
-                                validate_campaign_settings=False)
+        common_template = options['template']
+        if common_template:
+            try:
+                self.check_template_exists(common_template)
+            except ValidationError as e:
+                raise CommandError(e.message)
+        else:
+            self.validate_templates(campaigns, types=[self.TEMPLATE_TYPE],
+                                    validate_campaign_settings=False)
 
         generated = 0
         for campaign in campaigns:
             email_from = get_email_from(campaign)
-            template_name = self.get_template_name(campaign, type=template_type)
+            template_name = common_template or self.get_template_name(campaign, type=self.TEMPLATE_TYPE)
             template = get_email_template(template_name)
             tests = (Test.objects
                      .filter(applicant__campaign_id=campaign.pk,
+                             applicant__is_unsubscribed=False,
                              score__isnull=True,
                              status=Test.REGISTERED)
                      .values("applicant__email",
                              "yandex_contest_id"))
 
             for t in tests.iterator():
-                email = t["applicant__email"]
-                mail.send(
-                    [email],
-                    sender=email_from,
-                    template=template,
-                    context={
-                      "CONTEST_ID": t["yandex_contest_id"]
-                    },
-                    render_on_delivery=True,
-                    backend='ses',
-                )
-                generated += 1
+                recipients = [t["applicant__email"]]
+                if not Email.objects.filter(to=recipients,
+                                            template=template).exists():
+                    mail.send(
+                        recipients,
+                        sender=email_from,
+                        template=template,
+                        context={
+                          "CONTEST_ID": t["yandex_contest_id"]
+                        },
+                        render_on_delivery=True,
+                        backend='ses',
+                    )
+                    generated += 1
         self.stdout.write("Generated emails: {}".format(generated))
         self.stdout.write("Done")
