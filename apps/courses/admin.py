85bd9cbb7ca2d92c85a86fba418f2e4f2338e37d
Andrey Komendantov <9400878@gmail.com>
2021-04-23 16:56:29 +0300

    refactor: removed commented code

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -94,7 +94,6 @@ class CourseAdmin(TranslationAdmin, admin.ModelAdmin):
     list_display = ['meta_course', 'semester', 'main_branch',
                     'is_published_in_video']
     inlines = (CourseBranchInline, CourseTeacherInline,)
-    # readonly_fields = ('group_mode',)
     raw_id_fields = ('meta_course',)
 
     class Media:
