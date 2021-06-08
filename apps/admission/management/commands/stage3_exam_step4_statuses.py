80df4a8c383247db9d01d5beb07aa4cf080a7068
pacahon <pacabest@gmail.com>
2021-06-08 14:15:40 +0300

    refactor: improve stage3_exam_step4_statuses

--- apps/admission/management/commands/stage3_exam_step4_statuses.py
+++ apps/admission/management/commands/stage3_exam_step4_statuses.py
@@ -10,12 +10,14 @@ from ._utils import CurrentCampaignMixin
 
 class Command(CurrentCampaignMixin, BaseCommand):
     help = """
-    Updates applicant status for selected campaign based on exam results.
+    Updates applicant status based on exam result:
+        INTERVIEW_TOBE_SCHEDULED - exam score >= Campaign.exam_score_pass
+        REJECTED_BY_EXAM - exam score <= `reject_value`
+        PENDING - exam score in (reject_value, exam_score_pass) range
 
-    Details:
-        Set `INTERVIEW_TOBE_SCHEDULED` to applicants with exam score >= Campaign.exam_score_pass
-        Set `REJECTED_BY_EXAM` to applicants with exam score <= `reject_value`
-        Set `PENDING` to applicants with exam score in (reject_value, exam_score_pass) range
+    Assumptions:
+        There is no applicants in PENDING state from the previous stages or without status at all
+        If applicant omitted the exam set new status to REJECTED_BY_EXAM (not THEY_REFUSED)
     """
 
     def add_arguments(self, parser):
@@ -26,15 +28,14 @@ class Command(CurrentCampaignMixin, BaseCommand):
                  ' below this value.')
 
     def handle(self, *args, **options):
-        campaigns = self.get_current_campaigns(options, branch_is_required=True)
+        campaigns = self.get_current_campaigns(options, branch_is_required=True, confirm=False)
         assert len(campaigns) == 1
+        campaign = campaigns[0]
 
-        campaign = campaigns.get()
         self.stdout.write("Минимальный шаг оценки - 0.01")
+        if not campaign.exam_passing_score:
+            raise CommandError(f"Set exam passing score for {campaign}")
         exam_score_pass = Decimal(campaign.exam_passing_score)
-        if not exam_score_pass:
-            self.stdout.write(f"Zero exam passing score for {campaign}. Cancel")
-            return
         self.stdout.write(f"{exam_score_pass} и больше - прошёл на собеседование.")
 
         reject_value = options["reject_value"]
@@ -47,88 +48,84 @@ class Command(CurrentCampaignMixin, BaseCommand):
             self.stdout.write(f"От {reject_value} до {exam_score_pass} (не "
                               f"включая крайние значения) - в ожидании решения.")
 
+        applicants = Applicant.objects.filter(campaign=campaign.pk)
+
+        without_status_total = applicants.filter(status__isnull=True).count()
+        self.stdout.write(f"Applicants without status: {without_status_total}")
+
+        in_pending_state_total = applicants.filter(status=Applicant.PENDING).count()
+        self.stdout.write(f"There are {in_pending_state_total} applicants with PENDING status.")
+
         if input(f"{reject_value} и меньше - отказ по результатам экзамена. "
                  f"Продолжить? [y/n] ") != "y":
             self.stdout.write("Aborted")
             return
 
-        self.stdout.write("Total applicants: {}".format(
-            Applicant.objects.filter(campaign=campaign.pk).count()))
+        self.stdout.write("Total applicants: {}".format(applicants.count()))
 
-        total_cheaters = (Applicant.objects
-                          .filter(campaign=campaign.pk,
-                                  status=Applicant.REJECTED_BY_CHEATING)
+        cheaters_total = (applicants
+                          .filter(status=Applicant.REJECTED_BY_CHEATING)
                           .count())
 
-        total_rejected_by_test = (Applicant.objects
-                                  .filter(campaign=campaign.pk,
-                                          status=Applicant.REJECTED_BY_TEST)
-                                  .count())
+        # Do not include cheaters here
+        rejects_by_test_total = (applicants
+                                 .filter(status=Applicant.REJECTED_BY_TEST)
+                                 .count())
 
-        rejects_by_exam_q = (Applicant.objects
-                             .filter(campaign=campaign.pk)
-                             .filter(Q(exam__score__lte=reject_value) |
-                                     Q(exam__score__isnull=True))
-                             .filter(Q(status__isnull=True) |
-                                     Q(status=Applicant.PENDING) |
-                                     Q(status=Applicant.PERMIT_TO_EXAM) |
-                                     Q(status=Applicant.REJECTED_BY_EXAM)))
-        total_rejects_by_exam = rejects_by_exam_q.update(
-            status=Applicant.REJECTED_BY_EXAM)
+        rejects_by_exam_total = (applicants
+                                 .filter(Q(exam__score__lte=reject_value) |
+                                         Q(exam__score__isnull=True))
+                                 .filter(status__in=[Applicant.PENDING,
+                                                     Applicant.PERMIT_TO_EXAM,
+                                                     Applicant.REJECTED_BY_EXAM
+                                                     ])
+                                 .update(status=Applicant.REJECTED_BY_EXAM))
 
-        pass_exam_q = (Applicant.objects
-                       .filter(campaign=campaign.pk,
-                               exam__score__gte=exam_score_pass)
-                       .filter(Q(status__isnull=True) |
-                               Q(status=Applicant.PENDING) |
-                               Q(status=Applicant.PERMIT_TO_EXAM) |
-                               Q(status=Applicant.INTERVIEW_TOBE_SCHEDULED)))
-        total_pass_exam = pass_exam_q.update(
-            status=Applicant.INTERVIEW_TOBE_SCHEDULED)
+        pass_exam_total = (applicants
+                           .filter(exam__score__gte=exam_score_pass,
+                                   status__in=[Applicant.PENDING,
+                                               Applicant.PERMIT_TO_EXAM,
+                                               Applicant.INTERVIEW_TOBE_SCHEDULED])
+                           .update(status=Applicant.INTERVIEW_TOBE_SCHEDULED))
         # Some applicants could have exam score < passing score, but they
         # still pass to the next stage (by manual application form check)
         # Also count those who passed the interview phase and waiting
         # for the final decision
-        interviewed = (Applicant.objects
-                       .filter(campaign=campaign.pk)
-                       .filter(Q(status=Applicant.INTERVIEW_TOBE_SCHEDULED) |
-                               Q(status=Applicant.INTERVIEW_SCHEDULED) |
-                               Q(status=Applicant.INTERVIEW_COMPLETED) |
-                               Q(status=Applicant.REJECTED_BY_INTERVIEW))
-                       .count())
-        total_pass_exam = interviewed
+        pass_exam_total = (applicants
+                           .filter(status__in=[Applicant.INTERVIEW_TOBE_SCHEDULED,
+                                               Applicant.INTERVIEW_SCHEDULED,
+                                               Applicant.INTERVIEW_COMPLETED,
+                                               Applicant.REJECTED_BY_INTERVIEW])
+                           .count())
 
-        pending_q = (Applicant.objects
-                     .filter(campaign=campaign.pk,
-                             exam__score__gt=reject_value,
-                             exam__score__lt=exam_score_pass)
-                     .filter(Q(status__isnull=True) |
-                             Q(status=Applicant.PERMIT_TO_EXAM) |
-                             Q(status=Applicant.PENDING)))
-        total_pending = pending_q.update(status=Applicant.PENDING)
+        pending_total = (applicants
+                         .filter(exam__score__gt=reject_value,
+                                 exam__score__lt=exam_score_pass,)
+                         .filter(status__in=[Applicant.PERMIT_TO_EXAM, Applicant.PENDING])
+                         .update(status=Applicant.PENDING))
 
-        # If the applicant skipped the exam, he also becomes refused
-        refused_qs = (Applicant.objects
-                      .filter(campaign=campaign.pk,
-                              status=Applicant.THEY_REFUSED))
-        total_refused = refused_qs.count()
+        # Applicants who skipped the exam could be resolved
+        # with THEY_REFUSED or REJECTED_BY_EXAM status
+        refused_total = (applicants
+                         .filter(status=Applicant.THEY_REFUSED)
+                         .count())
 
-        total_all = (
-            total_cheaters,
-            total_rejected_by_test,
-            total_rejects_by_exam,
-            total_pass_exam,
-            total_pending,
-            total_refused
+        total = (
+            cheaters_total,
+            rejects_by_test_total,
+            rejects_by_exam_total,
+            pass_exam_total,
+            pending_total,
+            refused_total
         )
 
-        self.stdout.write("Cheaters: {}".format(total_cheaters))
-        self.stdout.write("Rejected by test: {}".format(total_rejected_by_test))
-        self.stdout.write("Rejected by exam: {}".format(total_rejects_by_exam))
-        self.stdout.write("Pass exam stage: {}".format(total_pass_exam))
-        self.stdout.write("Pending status: {}".format(total_pending))
-        self.stdout.write("Refused status: {}".format(total_refused))
-        self.stdout.write("{} = {}".format(
-            " + ".join((str(t) for t in total_all)),
-            sum(total_all)
+        self.stdout.write("Cheaters: {}".format(cheaters_total))
+        self.stdout.write("Rejected by test: {}".format(rejects_by_test_total))
+        self.stdout.write("Rejected by exam: {}".format(rejects_by_exam_total))
+        self.stdout.write("Pass exam stage: {}".format(pass_exam_total))
+        self.stdout.write("Pending status: {}".format(pending_total))
+        self.stdout.write("Refused status: {}".format(refused_total))
+        self.stdout.write("{exp} = {result}".format(
+            exp=" + ".join((str(t) for t in total)),
+            result=sum(total)
         ))
