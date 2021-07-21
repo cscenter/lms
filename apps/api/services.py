02a5f4f6a345f3bbe1c0fb58d48aa418c5af29df
pacahon <pacabest@gmail.com>
2021-07-21 19:34:29 +0300

    refactor: add typings for api services module

--- apps/api/services.py
+++ apps/api/services.py
@@ -1,6 +1,7 @@
 import binascii
 import random
 from os import urandom as generate_bytes
+from typing import Tuple
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
@@ -15,13 +16,13 @@ from api.settings import (
 )
 
 
-def create_token_string():
+def create_token_string() -> str:
     return binascii.hexlify(
         generate_bytes(int(AUTH_TOKEN_CHARACTER_LENGTH / 2))
     ).decode()
 
 
-def hash_token(token):
+def hash_token(token) -> str:
     """
     Calculates the hash of a token.
 
@@ -32,7 +33,7 @@ def hash_token(token):
 
 
 # TODO: move to core utils
-def generate_hash(*bits: bytes, salt=True):
+def generate_hash(*bits: bytes, salt=True) -> str:
     """Use salt=False if key needs to be mapped 1 to 1"""
     digest = hashes.Hash(SECURE_HASH_ALGORITHM(), backend=default_backend())
     salt = force_bytes(str(random.random())[2:]) if salt else b''
@@ -43,7 +44,7 @@ def generate_hash(*bits: bytes, salt=True):
 
 class TokenService:
     @staticmethod
-    def create(user, expire_at=None):
+    def create(user, expire_at=None) -> Tuple[Token, str]:
         token = create_token_string()
         digest = hash_token(token)
 
@@ -53,7 +54,7 @@ class TokenService:
         return instance, token
 
     @staticmethod
-    def renew(token: Token):
+    def renew(token: Token) -> None:
         current_expiry = token.expire_at
         assert current_expiry is not None
         new_expiry = timezone.now() + TOKEN_TTL
