0043ca7dd3330b53ddb1fbdc3502ff93ea78b8ec
pacahon <pacabest@gmail.com>
2021-09-10 15:42:49 +0300

    feat: add bounce/complaint event handlers for AWS SES webhook

--- apps/core/signals.py
+++ apps/core/signals.py
@@ -1,4 +1,6 @@
-from __future__ import absolute_import, unicode_literals
+import logging
+
+from django_ses.signals import bounce_received, complaint_received
 
 from django.db import models
 from django.dispatch import receiver
@@ -6,8 +8,23 @@ from django.dispatch import receiver
 from compsciclub_ru.context_processors import BRANCHES
 from core.models import City
 
+logger = logging.getLogger()
+
 
 @receiver(models.signals.post_save, sender=City)
 @receiver(models.signals.post_delete, sender=City)
 def city_cache_clear_after_save(sender, *args, **kwargs):
     BRANCHES["CACHE"] = []
+
+
+@receiver(bounce_received)
+def bounce_handler(sender, mail_obj, bounce_obj, raw_message, *args, **kwargs):
+    message_id = mail_obj['messageId']
+    recipient_list = mail_obj['destination']
+    logger.error(f"Bounce received. Recipients: {recipient_list}")
+    # TODO: Find all models with EmailAddressSuspension mixin and update data
+
+
+@receiver(complaint_received)
+def complaint_handler(sender, mail_obj, complaint_obj, raw_message,  *args, **kwargs):
+    pass
