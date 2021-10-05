c9c198d5269137e20c79bd00ebdad19ab3a6339d
pacahon <pacabest@gmail.com>
2021-10-05 15:02:22 +0300

    content: add distance branch on syllabus detail pages

--- compscicenter_ru/views.py
+++ compscicenter_ru/views.py
@@ -341,7 +341,7 @@ class AlumniView(TemplateView):
 
 
 class OnCampusProgramsView(generic.TemplateView):
-    template_name = "compscicenter_ru/syllabus/on_campus.html"
+    template_name = "compscicenter_ru/syllabus/syllabus.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
