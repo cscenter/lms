a1bcd58e7d02bf8e0ad6f51b9ed6dbf5cf414a6c
pacahon <pacabest@gmail.com>
2021-10-05 21:33:42 +0300

    feat: add DRF http request classes for better typing support

--- apps/core/http.py
+++ apps/core/http.py
@@ -1,15 +1,25 @@
 from typing import Union
 
-from django.http import HttpRequest as _HttpRequest
+from rest_framework.request import Request as _HttpDRFRequest
+
+from django.http import HttpRequest as _HttpDjangoRequest
 
 from users.models import ExtendedAnonymousUser, User
 
-__all__ = ["HttpRequest", "AuthenticatedHttpRequest"]
+__all__ = ["HttpRequest", "AuthenticatedHttpRequest", "APIRequest", "AuthenticatedAPIRequest"]
 
 
-class HttpRequest(_HttpRequest):
+class HttpRequest(_HttpDjangoRequest):
     user: Union[User, ExtendedAnonymousUser]
 
 
 class AuthenticatedHttpRequest(HttpRequest):
+    user: User
+
+
+class APIRequest(_HttpDRFRequest):
+    user: Union[User, ExtendedAnonymousUser]
+
+
+class AuthenticatedAPIRequest(_HttpDRFRequest):
     user: User
