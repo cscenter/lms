6fe006d81323dfda1e0ea14531c4998086bcaea1
pacahon <pacabest@gmail.com>
2022-02-25 12:30:29 +0300

    refactor: update maketranslation management cmd

--- apps/core/management/commands/maketranslation.py
+++ apps/core/management/commands/maketranslation.py
@@ -1,9 +1,4 @@
-import re
-
-from django_jinja.management.commands.makemessages import strip_whitespaces
-
-from django.core.management.commands import makemessages
-from django.utils.translation import template as trans_real
+from django_jinja.management.commands import makemessages
 
 EXTRA_KEYWORDS = [
     '--keyword=_p:1c,2'  # alias for pgettext_lazy
@@ -29,35 +24,4 @@ class Command(makemessages.Command):
                 makemessages.Command.xgettext_options[:] +  # type: ignore[attr-defined]
                 ['--keyword=%s' % kwd for kwd in xgettext_keywords]
             )
-        # super(Command, self).handle(*args, **options)
-
-        # TODO: remove code below after upgrading to django-jinja > 2.7.0 and inherit from django-jinja command
-        # https://github.com/niwinz/django-jinja/issues/272
-        old_endblock_re = trans_real.endblock_re
-        old_block_re = trans_real.block_re
-        old_constant_re = trans_real.constant_re
-
-        old_templatize = trans_real.templatize
-        # Extend the regular expressions that are used to detect
-        # translation blocks with an "OR jinja-syntax" clause.
-        trans_real.endblock_re = re.compile(
-            trans_real.endblock_re.pattern + '|' + r"""^-?\s*endtrans\s*-?$""")
-        trans_real.block_re = re.compile(
-            trans_real.block_re.pattern + '|' + r"""^-?\s*trans(?:\s+(?:no)?trimmed)?(?:\s+(?!'|")(?=.*?=.*?)|\s*-?$)""")
-        trans_real.plural_re = re.compile(
-            trans_real.plural_re.pattern + '|' + r"""^-?\s*pluralize(?:\s+.+|-?$)""")
-        trans_real.constant_re = re.compile(r""".*?_\(((?:".*?(?<!\\)")|(?:'.*?(?<!\\)')).*?\)""")
-
-        def my_templatize(src, *args, **kwargs):
-            new_src = strip_whitespaces(src)
-            return old_templatize(new_src, *args, **kwargs)
-
-        trans_real.templatize = my_templatize
-
-        try:
-            super(Command, self).handle(*args, **options)
-        finally:
-            trans_real.endblock_re = old_endblock_re
-            trans_real.block_re = old_block_re
-            trans_real.templatize = old_templatize
-            trans_real.constant_re = old_constant_re
+        super(Command, self).handle(*args, **options)
