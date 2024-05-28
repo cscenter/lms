37b3d8c7277047bc8d1fa8635749ec003bc3ee22
Charnushevich Dzmitry <dmi4er4@gmail.com>
2024-05-28 19:42:40 +0300

    Update interview import scripts

--- apps/admission/management/commands/import_interviewstreams_from_csv.py
+++ apps/admission/management/commands/import_interviewstreams_from_csv.py
@@ -8,147 +8,165 @@ from django.db import transaction
 from admission.constants import InterviewSections, InterviewFormats
 from admission.models import InterviewStream, Campaign, InterviewFormat, InterviewSlot
 from admission.utils import slot_range
-from core.models import Location
+from core.models import Location, Branch
 from users.models import User
 
-prefix = "Выбери время, когда ты сможешь провести собеседование."
+from django.conf import settings
 
-MONTH_NUMBER = {
-    'июня': 6,
-    'июля': 7,
-    'августа': 8
-}
+from django.core.management import BaseCommand
 
+class Command(BaseCommand):
+    help = """Import interview streams from csv"""
 
-def get_stream_date(s: str, year: int) -> datetime.date:
-    assert prefix in s
-    date_start_index = 0
-    date_end_index = date_start_index + 1
-    if s[date_start_index + 1] != ' ':
-        date_end_index += 1
-    day = int(s[date_start_index:date_end_index])
-    month_start_index = date_end_index + 1
-    month_end_index = s.find('.')
-    month = MONTH_NUMBER[s[month_start_index:month_end_index]]
-    return datetime.date(day=day, month=month, year=year)
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            "--filename",
+            type=str,
+            default='interviews.csv',
+            help="csv file name",
+        )
+        parser.add_argument(
+            "--delimiter",
+            type=str,
+            default=',',
+            help="csv delimiter",
+        )
 
+    prefix = "Выбери время, когда ты сможешь провести собеседование."
 
-def parse_time_pair(string) -> Tuple[datetime.time, datetime.time]:
-    begin, _, end = string.split(' ')
-    begin = datetime.datetime.strptime(begin, '%H:%M').time()
-    end = datetime.datetime.strptime(end, '%H:%M').time()
-    return begin, end
-
-
-def get_stream_begin_end(s: str):
-    prefix = '/ '
-    time_part = s[s.find(prefix) + len(prefix):]
-    return parse_time_pair(time_part)
-
-
-def get_section(section_name: str) -> InterviewSections:
-    return {
-        "общая секция": InterviewSections.ALL_IN_ONE,
-        "математика": InterviewSections.MATH,
-        "код": InterviewSections.PROGRAMMING,
-        "мотивация": InterviewSections.MOTIVATION
-    }[section_name]
-
-
-msk_campaign = Campaign.objects.get(pk=42)
-msk_venue = Location.objects.get(pk=87)
-msk_format = InterviewFormat.objects.get(pk=31)
-
-distant_campaign = Campaign.objects.get(pk=48)
-distant_venue = Location.objects.get(pk=105)
-distant_format = InterviewFormat.objects.get(pk=32)
-
-spb_campaign = Campaign.objects.get(pk=43)
-spb_venue = Location.objects.get(pk=106)
-spb_format = InterviewFormat.objects.get(pk=33)
-
-
-def get_filial_info(campaign_name: str) -> Campaign:
-    return {
-        "Москва": (msk_campaign, msk_venue, msk_format),
-        "Заочное": (distant_campaign, distant_venue, distant_format),
-        "Санкт-Петербург": (spb_campaign, spb_venue, spb_format),
-    }[campaign_name]
-
-
-def main():
-    year = 2023
+    year = 2024
     format = InterviewFormats.ONLINE
     with_assignments = False
     commit = True
-    with open('interviews.csv') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        headers = next(reader)
-        slot_columns = [i for i, s in enumerate(headers) if prefix in s]
-        streams = defaultdict(list)
-        for row in reader:
-            interviewer_mail = row[1]
-            campaign_name = row[2]
-            interviewer_section = row[3] + row[4] + row[5]
-            interview_date, interview_start, interview_last_stamp = None, None, None
-            for column in slot_columns:
-                if row[column]:
-                    begin, end = get_stream_begin_end(headers[column])
-                    date = get_stream_date(headers[column], year)
-                    if interview_start is None:
-                        interview_date = get_stream_date(headers[column], year)
-                        interview_start = begin
-                    elif interview_date != date:
+
+    def get_month_number(self, month_name: str) -> int:
+        return {
+            "июня": 6,
+            "июля": 7,
+            "августа": 8
+        }[month_name]
+
+    def get_section(self, section_name: str) -> InterviewSections:
+        return {
+            "общая секция": InterviewSections.ALL_IN_ONE,
+            "математика": InterviewSections.MATH,
+            "код": InterviewSections.PROGRAMMING,
+            "мотивация": InterviewSections.MOTIVATION
+        }[section_name]
+
+    def get_stream_date(self, s: str, year: int) -> datetime.date:
+        """Returns a date of the slot"""
+        assert self.prefix in s
+        date_start_index = 0
+        date_end_index = date_start_index + 1
+        if s[date_end_index] != ' ':
+            date_end_index += 1
+        day = int(s[date_start_index:date_end_index])
+        month_start_index = date_end_index + 1
+        month_end_index = s.find('.')
+        month = self.get_month_number(s[month_start_index:month_end_index])
+        return datetime.date(day=day, month=month, year=year)
+
+    def parse_time_pair(self, string: str) -> Tuple[datetime.time, datetime.time]:
+        begin, _, end = string.split(' ')
+        begin = datetime.datetime.strptime(begin, '%H:%M').time()
+        end = datetime.datetime.strptime(end, '%H:%M').time()
+        return begin, end
+
+    def get_stream_begin_end(self, s: str) -> Tuple[datetime.time, datetime.time]:
+        """Returns a pair: begin and end time of available slot"""
+        dash = '/ '
+        time_part = s[s.find(dash) + len(dash):]
+        return self.parse_time_pair(time_part)
+
+    filial_cache = {}
+    def get_filial_info(self, branch_name: str):
+        if branch_name in self.filial_cache:
+            return self.filial_cache[branch_name]
+        branch = Branch.objects.get(name=branch_name if branch_name!='Заочное' else 'Заочное отделение', site_id=settings.SITE_ID)
+        campaign = Campaign.objects.get(branch=branch, current=True)
+        venue_name = f'Онлайн-собеседование в ШАД {branch_name} {self.year}'
+        venue = Location.objects.get(name=venue_name)
+        interview_format = InterviewFormat.objects.get(campaign=campaign)
+        result = (campaign, venue, interview_format)
+        self.filial_cache[branch_name] = result
+        return result
+
+    def handle(self, *args, **options):
+        delimiter = options["delimiter"]
+        filename = options["filename"]
+        with open(filename) as csvfile:
+            reader = csv.reader(csvfile, delimiter=delimiter)
+            headers = next(reader)
+            # Колонки с выбором времени
+            slot_columns = [i for i, s in enumerate(headers) if self.prefix in s]
+            streams = defaultdict(list)
+            for row in reader:
+                interviewer_mail = row[1]
+                campaign_name = row[2]
+                interviewer_section = row[3] + row[4] + row[5]
+                interview_date, interview_start, interview_last_stamp = None, None, None
+                for column in slot_columns:
+                    if row[column]:
+                        # Получаем дату и время начала/конца собеседования
+                        begin, end = self.get_stream_begin_end(headers[column])
+                        date = self.get_stream_date(headers[column], self.year)
+                        if interview_start is None:
+                            interview_date = date
+                            interview_start = begin
+                        elif interview_date != date:
+                            # заполняем dict с потоками возможными временами
+                            key = (interviewer_mail, interviewer_section, interview_date, campaign_name)
+                            streams[key].append((interview_start, interview_last_stamp))
+                            interview_date = date
+                            interview_start = begin
+                        interview_last_stamp = end
+                    elif interview_start is not None:
                         key = (interviewer_mail, interviewer_section, interview_date, campaign_name)
                         streams[key].append((interview_start, interview_last_stamp))
-                        interview_date = get_stream_date(headers[column], year)
-                        interview_start = begin
-                    interview_last_stamp = end
-                elif interview_start is not None:
+                        interview_date, interview_start, interview_last_stamp = None, None, None
+                if interview_start is not None:
                     key = (interviewer_mail, interviewer_section, interview_date, campaign_name)
                     streams[key].append((interview_start, interview_last_stamp))
-                    interview_date, interview_start, interview_last_stamp = None, None, None
-            if interview_start is not None:
-                key = (interviewer_mail, interviewer_section, interview_date, campaign_name)
-                streams[key].append((interview_start, interview_last_stamp))
-        begin, end = get_stream_begin_end(headers[slot_columns[0]])
-        slot_size = (datetime.timedelta(hours=end.hour, minutes=end.minute) -
-                     datetime.timedelta(hours=begin.hour, minutes=begin.minute))
-        with transaction.atomic():
-            for stream_info, slots_ranges in streams.items():
-                stream_begin, stream_end = slots_ranges[0][0], slots_ranges[-1][1]
-                assert stream_begin < stream_end
-                print(stream_info[0])
-                interviewer = User.objects.get(email__iexact=stream_info[0])
-                section = get_section(stream_info[1])
-                campaign, venue, interview_format = get_filial_info(stream_info[3])
-                stream = InterviewStream(
-                    campaign=campaign,
-                    venue=venue,
-                    format=format,
-                    interview_format=interview_format,
-                    section=section,
-                    with_assignments=with_assignments,
-                    date=stream_info[2],
-                    start_at=stream_begin,
-                    end_at=stream_end,
-                    duration=slot_size.seconds // 60
-                )
-                stream.save()
-                stream.interviewers.add(interviewer)
-                slots_that_should_be = {
-                    start_at
-                    for period in slots_ranges
-                    for start_at, end_at in slot_range(period[0], period[1], slot_size)
-                }
-                all_slots = {start_at for start_at, end_at in slot_range(stream_begin, stream_end, slot_size)}
-                slots_for_delete = all_slots.difference(slots_that_should_be)
-                InterviewSlot.objects.filter(
-                    stream=stream,
-                    start_at__in=slots_for_delete
-                ).delete()
-            if not commit:
-                raise Exception("Please set value commit = True")
+            begin, end = self.get_stream_begin_end(headers[slot_columns[0]])
+            slot_size = (datetime.timedelta(hours=end.hour, minutes=end.minute) -
+                         datetime.timedelta(hours=begin.hour, minutes=begin.minute))
+            with transaction.atomic():
+                for stream_info, slots_ranges in streams.items():
+                    stream_begin, stream_end = slots_ranges[0][0], slots_ranges[-1][1]
+                    assert stream_begin < stream_end
+                    print(stream_info[0])
+                    interviewer = User.objects.get(email__iexact=stream_info[0])
+                    section = self.get_section(stream_info[1])
+                    campaign, venue, interview_format = self.get_filial_info(stream_info[3])
+                    stream = InterviewStream(
+                        campaign=campaign,
+                        venue=venue,
+                        format=self.format,
+                        interview_format=interview_format,
+                        section=section,
+                        with_assignments=self.with_assignments,
+                        date=stream_info[2],
+                        start_at=stream_begin,
+                        end_at=stream_end,
+                        duration=slot_size.seconds // 60
+                    )
+                    stream.save()
+                    stream.interviewers.add(interviewer)
+                    slots_that_should_be = {
+                        start_at
+                        for period in slots_ranges
+                        for start_at, end_at in slot_range(period[0], period[1], slot_size)
+                    }
+                    print(slots_that_should_be)
+                    all_slots = {start_at for start_at, end_at in slot_range(stream_begin, stream_end, slot_size)}
+                    print(all_slots)
+                    slots_for_delete = all_slots.difference(slots_that_should_be)
+                    InterviewSlot.objects.filter(
+                        stream=stream,
+                        start_at__in=slots_for_delete
+                    ).delete()
+                if not self.commit:
+                    raise Exception("Please set value commit = True")
 
-
-main()
--- apps/admission/management/commands/import_notes.py
+++ apps/admission/management/commands/import_notes.py
@@ -1,24 +1,48 @@
 import csv
 
 from django.db import transaction
-
+from django.core.management import BaseCommand
 from admission.models import Applicant
 
+from ._utils import CurrentCampaignMixin
+class Command(CurrentCampaignMixin, BaseCommand):
+    help = """Import applicant notes from csv"""
 
-def main():
-    with open('notes.csv') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=';')
-        prefix = '/admission/applicants/'
-        commit = True
-        with transaction.atomic():
-            for row in reader:
-                note, applicant_url = row['Заметки'], row['ID']
-                assert applicant_url.startswith(prefix)
-                assert applicant_url[-1] == '/'
-                applicant_id = int(applicant_url[len(prefix):-1])
-                assert Applicant.objects.filter(pk=applicant_id).update(admin_note=note)
-                print(applicant_id, note)
-            if not commit:
-                raise Exception("Please set value commit = True")
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            "--filename",
+            type=str,
+            default='notes.csv',
+            help="csv file name",
+        )
+        parser.add_argument(
+            "--delimiter",
+            type=str,
+            default=',',
+            help="csv delimiter",
+        )
 
-main()
+    def handle(self, *args, **options):
+        campaigns = self.get_current_campaigns(options, confirm=False)
+        delimiter = options["delimiter"]
+        filename = options["filename"]
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=delimiter)
+            prefix = '/admission/applicants/'
+            with transaction.atomic():
+                for row in reader:
+                    note, applicant_url, email = row['Заметки'], row['ID'], row['Email']
+                    first_name, last_name, patronymic = row['Имя'], row['Фамилия'], row['Отчество']
+                    assert applicant_url.startswith(prefix)
+                    assert applicant_url[-1] == '/'
+                    applicant_id = int(applicant_url[len(prefix):-1])
+                    applicant = Applicant.objects.get(pk=applicant_id)
+                    assert(applicant.campaign in campaigns)
+                    assert(applicant.first_name == first_name)
+                    assert(applicant.last_name == last_name)
+                    assert(applicant.patronymic == patronymic)
+                    assert(applicant.email == email)
+                    applicant.admin_note = note
+                    applicant.save()
+                    print(f"{last_name} {first_name} {patronymic}, {applicant.campaign}: {note}")
