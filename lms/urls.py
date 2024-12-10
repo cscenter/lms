9eb135f3ea9a1e4c1e33561020f5094d19b470d3
Дмитрий Че <39742182+Dmi4er4@users.noreply.github.com>
2024-12-10 19:12:37 +0300

    Add swagger (#918)

--- lms/urls.py
+++ lms/urls.py
@@ -74,4 +74,26 @@ if settings.DEBUG:
         urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     if apps.is_installed('rosetta'):
         urlpatterns += [path('rosetta/', include('rosetta.urls'))]
+    if apps.is_installed('drf_yasg'):
+        from rest_framework import permissions
+        from drf_yasg.views import get_schema_view
+        from drf_yasg import openapi
+        schema_view = get_schema_view(
+            openapi.Info(
+                title="LMS API",
+                default_version='v1',
+                description="Test description",
+                terms_of_service="https://www.google.com/policies/terms/",
+                contact=openapi.Contact(email="contact@snippets.local"),
+                license=openapi.License(name="BSD License"),
+            ),
+            public=True,
+            permission_classes=(permissions.AllowAny,),
+        )
+
+        urlpatterns += [
+        path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+        ]
 
