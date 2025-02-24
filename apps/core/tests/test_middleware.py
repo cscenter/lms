46ccd1c82657a835c997449080d62b3bab510585
Sofya Anokhovskaya <cinnabun@yandex-team.ru>
2025-02-24 17:36:20 +0300

    feat: tests

--- apps/core/tests/test_middleware.py
+++ apps/core/tests/test_middleware.py
@@ -1,20 +1,6 @@
 import pytest
-from apps.core.middleware import UserStatusCheckMiddleware
 from apps.learning.settings import StudentStatuses
 from apps.users.tests.factories import StudentFactory
-from compscicenter_ru.apps.application.views import SESSION_LOGIN_KEY
-
-@pytest.fixture
-def middleware():
-    return UserStatusCheckMiddleware(get_response=lambda req: 'response')
-
-@pytest.fixture
-def request_with_user():
-    class Request:
-        def __init__(self):
-            self.user = StudentFactory(email='bounce@simulator.amazonses.com')
-            self.session = {}
-    return Request()
 
 
 @pytest.mark.django_db
@@ -23,16 +9,16 @@ def request_with_user():
     StudentStatuses.ACADEMIC_LEAVE,
     StudentStatuses.ACADEMIC_LEAVE_SECOND
 ])
-def test_call_with_inactive_student(middleware, request_with_user, settings, status):
-
-    request_with_user.session[SESSION_LOGIN_KEY] = 'some_value'
-    student_profile = request_with_user.user.get_student_profile(settings.SITE_ID)
+def test_call_with_inactive_student(client, settings, status):
+    student = StudentFactory(email='bounce@simulator.amazonses.com')
+    student_profile = student.get_student_profile(settings.SITE_ID)
     student_profile.status = status
+    student_profile.save()
+    client.login(student)
 
-    response = middleware(request_with_user)
+    response = client.get('/')
+    assert not response.wsgi_request.user.is_authenticated
 
-    assert SESSION_LOGIN_KEY not in request_with_user.session
-    assert response == 'response'
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("status", [
@@ -40,26 +26,25 @@ def test_call_with_inactive_student(middleware, request_with_user, settings, sta
     StudentStatuses.WILL_GRADUATE,
     StudentStatuses.GRADUATE
 ])
-def test_call_with_active_student(middleware, request_with_user, settings, status):
+def test_call_with_active_student(client, settings, status):
 
-    request_with_user.session[SESSION_LOGIN_KEY] = 'some_value'
-    student_profile = request_with_user.user.get_student_profile(settings.SITE_ID)
+    student = StudentFactory(email='bounce@simulator.amazonses.com')
+    student_profile = student.get_student_profile(settings.SITE_ID)
     student_profile.status = status
+    student_profile.save()
+    client.login(student)
 
-    response = middleware(request_with_user)
-
-    assert SESSION_LOGIN_KEY in request_with_user.session
-    assert response == 'response'
+    response = client.get('/')
+    assert response.wsgi_request.user.is_authenticated
 
 
 @pytest.mark.django_db
-def test_call_without_student_profile(middleware, request_with_user, mocker):
+def test_call_without_student_profile(client, mocker):
 
     mock_get_student = mocker.patch("users.services.get_student_profile")
+    student = StudentFactory(email='bounce@simulator.amazonses.com')
     mock_get_student.return_value = None
-    request_with_user.session[SESSION_LOGIN_KEY] = 'some_value'
+    client.login(student)
 
-    response = middleware(request_with_user)
-
-    assert SESSION_LOGIN_KEY in request_with_user.session
-    assert response == 'response'
+    response = client.get('/')
+    assert response.status_code
