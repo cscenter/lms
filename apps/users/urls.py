b40557271c0ca2d119adc7a8cf209f338e793f9b
dmi4er4 <dmi4er4@gmail.com>
2025-02-25 23:18:01 +0300

    remove reduntant str

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -23,9 +23,9 @@ urlpatterns = [
     # iCalendar
     path("events/<int:pk>/", EventDetailView.as_view(), name="non_course_event_detail"),
     re_path(r'^events.ics', ICalEventsView.as_view(), name='ical_events'),
-    path('users/<str:encoded_pk>/classes.ics', ICalClassesView.as_view(), name='user_ical_classes'),
-    path('users/<str:encoded_pk>/assignments.ics', ICalAssignmentsView.as_view(), name='user_ical_assignments'),
-    path('users/<str:encoded_pk>/interviews.ics', ICalInterviewsView.as_view(), name='user_ical_interviews'),
+    path('users/<encoded_pk>/classes.ics', ICalClassesView.as_view(), name='user_ical_classes'),
+    path('users/<encoded_pk>/assignments.ics', ICalAssignmentsView.as_view(), name='user_ical_assignments'),
+    path('users/<encoded_pk>/interviews.ics', ICalInterviewsView.as_view(), name='user_ical_interviews'),
 
     path('api/v1/', include(([
         path('', include((user_api_patterns, 'api'))),
