73251d3282abb0087dc7c2f728975b7a0100e187
Sofya Anokhovskaya <cinnabun@yandex-team.ru>
2025-04-03 11:25:27 +0300

    fix: edit form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -29,8 +29,7 @@ class UserProfileForm(forms.ModelForm):
     )
     yandex_login = forms.CharField(
         max_length=64,
-        label="Логин из Яндекс.Контеста",
-        help_text="обновлено будет только в YandexUserData",
+        label="Логин из Яндекс",
         required=False
     )
 
@@ -44,14 +43,15 @@ class UserProfileForm(forms.ModelForm):
         self.helper = FormHelper()
         # Create a new list for show_fields
         show_fields = list(UserProfileForm.Meta.fields)
-        show_fields.append('yandex_login')
+        # show_fields.append('yandex_login')
         
         self.fields['birth_date'].disabled = True
         self.fields['yandex_login'].disabled = True
         self.fields['yandex_login'].initial = self.initial.get("yandex_login", "")
-        # Update yandex_login help_text with the correct URL
-        yandex_url = reverse('auth:users:yandex_begin')
-        self.fields['yandex_login'].help_text = f'<a href="{yandex_url}">[Изменить]</a>'
+        # Update yandex_login help_text with the correct URL only if the user is viewing their own profile
+        if self.editor == self.student:
+            yandex_url = reverse('auth:users:yandex_begin')
+            self.fields['yandex_login'].help_text = f'<a href="{yandex_url}">[Изменить]</a>'
 
         if is_club_site():
             show_fields.extend(['first_name', 'last_name', 'patronymic'])
@@ -64,7 +64,7 @@ class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('birth_date', 'phone', 'workplace', 'bio', 'time_zone',
+        fields = ('birth_date', 'phone', 'workplace', 'bio', 'time_zone', 'yandex_login',
                   'telegram_username', 'github_login', 'stepic_id', 'codeforces_login',
                   'private_contacts', 'is_notification_allowed', 'tshirt_size')
         widgets = {
