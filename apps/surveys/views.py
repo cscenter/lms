4d7b748d8b42fda53b29926e19f92fcf29d96946
Sofya Anokhovskaya <cinnabun@yandex-team.ru>
2025-04-14 20:23:25 +0300

    feat: views for reports

--- apps/surveys/views.py
+++ apps/surveys/views.py
@@ -1,16 +1,23 @@
+import logging
 import pytz
 from vanilla import TemplateView
+
 
 from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.views import generic
 from django.views.generic import FormView
+from django.shortcuts import redirect
 
 from core.exceptions import Redirect
 from core.urls import reverse
 from courses.views.mixins import CourseURLParamsMixin
 from surveys.forms import FormBuilder
 from surveys.models import CourseSurvey
+from users.mixins import CuratorOnlyMixin
+
+logger = logging.getLogger(__name__)
 
 
 class CourseSurveyDetailView(CourseURLParamsMixin, FormView):
@@ -57,3 +64,21 @@ class CourseSurveyFormSuccessView(CourseURLParamsMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         return {"course": self.course}
+
+
+class ReportBugView(CuratorOnlyMixin, generic.base.View):
+    def get(self, request, *args, **kwargs):
+        url = "https://forms.yandex.ru/surveys/13739605.ea35e390cf310d138e5e32315ecb2c07f1813e89/"
+        # TODO Create log
+        # TODO Set params to Form?
+        logger.info(f"Got bug report from {request.user} on page {request.META['HTTP_REFERER']}")
+        return redirect(url)
+    
+
+class ReportIdeaView(CuratorOnlyMixin, generic.base.View):
+    def get(self, request, *args, **kwargs):
+        url = "https://forms.yandex.ru/surveys/13739606.3c14ec2d9997b34e4b254c8abddf1636af04f78f/"
+        # TODO Create log
+        # TODO Set params to Form?
+        logger.info(f"Got idea report from {request.user} on page {request.META['HTTP_REFERER']}")
+        return redirect(url)
